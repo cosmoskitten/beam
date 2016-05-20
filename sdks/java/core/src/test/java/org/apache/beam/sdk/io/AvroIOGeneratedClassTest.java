c78b721f1270e9fd3b549ec9f5140b0a70900139
Scott Wegner <swegner@google.com>
2016-05-20 11:10:55 -0700

    fixup! revert changes to AvroIOGeneratedClassTest

--- sdks/java/core/src/test/java/org/apache/beam/sdk/io/AvroIOGeneratedClassTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/io/AvroIOGeneratedClassTest.java
@@ -56,7 +56,7 @@ import java.util.List;
 @RunWith(JUnit4.class)
 public class AvroIOGeneratedClassTest {
   @Rule
-  public TemporaryFolder tmpFolder = new TemporaryFoldorg.apache.beamer();
+  public TemporaryFolder tmpFolder = new TemporaryFolder();
   private File avroFile;
 
   @Before
