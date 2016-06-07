05287c4ddcc38378e4e8cd921b6f6394e43eed75
Maximilian Michels <mxm@apache.org>
2016-06-07 13:57:33 +0200

    [BEAM-196] fix NPE in ParDoWrapper

--- runners/flink/runner/src/test/java/org/apache/beam/runners/flink/PipelineOptionsTest.java
+++ runners/flink/runner/src/test/java/org/apache/beam/runners/flink/PipelineOptionsTest.java
@@ -18,14 +18,29 @@
 package org.apache.beam.runners.flink;
 
 import org.apache.beam.runners.flink.translation.utils.SerializedPipelineOptions;
+import org.apache.beam.runners.flink.translation.wrappers.streaming.FlinkAbstractParDoWrapper;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.windowing.GlobalWindow;
+import org.apache.beam.sdk.transforms.windowing.PaneInfo;
+import org.apache.beam.sdk.util.WindowedValue;
+import org.apache.beam.sdk.util.WindowingInternals;
+import org.apache.beam.sdk.util.WindowingStrategy;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.commons.lang.SerializationUtils;
+import org.apache.flink.util.Collector;
+import org.joda.time.Instant;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests the serialization and deserialization of PipelineOptions.
@@ -58,11 +73,95 @@ public class PipelineOptionsTest {
 
   @Test
   public void testCaching() {
-    MyOptions deserializedOptions = serializedOptions.getPipelineOptions().as(MyOptions.class);
+    PipelineOptions deserializedOptions = serializedOptions.getPipelineOptions().as(PipelineOptions.class);
     assertNotNull(deserializedOptions);
-    assertEquals(deserializedOptions, serializedOptions.getPipelineOptions());
-    assertEquals(deserializedOptions, serializedOptions.getPipelineOptions());
-    assertEquals(deserializedOptions, serializedOptions.getPipelineOptions());
+    assertTrue(deserializedOptions == serializedOptions.getPipelineOptions());
+    assertTrue(deserializedOptions == serializedOptions.getPipelineOptions());
+    assertTrue(deserializedOptions == serializedOptions.getPipelineOptions());
   }
+
+  @Test
+  public void testNonNull() {
+    try {
+      new SerializedPipelineOptions(null);
+      Assert.fail();
+    } catch (Exception e) {
+      // ok
+    }
+  }
+
+  @Test
+  public void ParDoBaseClassPipelineOptionsNullTest() {
+    try {
+      new TestParDoBase(null, WindowingStrategy.globalDefault(), new TestDoFn());
+      Assert.fail();
+    } catch (Exception e) {
+      // correct
+    }
+  }
+
+  /**
+   * Tests that PipelineOptions are present after serialization
+   */
+  @Test
+  public void ParDoBaseClassPipelineOptionsSerializationTest() throws Exception {
+    TestParDoBase wrapper =
+        new TestParDoBase(options, WindowingStrategy.globalDefault(), new TestDoFn());
+
+    final byte[] serialized = SerializationUtils.serialize(wrapper);
+    TestParDoBase deserialize = (TestParDoBase) SerializationUtils.deserialize(serialized);
+
+    // execute once to access options
+    deserialize.flatMap(
+        WindowedValue.of(
+            new Object(),
+            Instant.now(),
+            GlobalWindow.INSTANCE,
+            PaneInfo.NO_FIRING),
+        Mockito.mock(Collector.class));
+
+  }
+
+
+  private static class TestDoFn extends DoFn<Object, Object> {
+
+    @Override
+    public void processElement(ProcessContext c) throws Exception {
+      Assert.assertNotNull(c.getPipelineOptions());
+      Assert.assertEquals(
+          options.getTestOption(),
+          c.getPipelineOptions().as(MyOptions.class).getTestOption());
+    }
+  }
+
+  private static class TestParDoBase extends FlinkAbstractParDoWrapper {
+    public TestParDoBase(PipelineOptions options, WindowingStrategy windowingStrategy, DoFn doFn) {
+      super(options, windowingStrategy, doFn);
+    }
+
+
+    @Override
+    public WindowingInternals windowingInternalsHelper(
+        WindowedValue inElement,
+        Collector outCollector) {
+      return null;
+    }
+
+    @Override
+    public void sideOutputWithTimestampHelper(
+        WindowedValue inElement,
+        Object output,
+        Instant timestamp,
+        Collector outCollector,
+        TupleTag tag) {}
+
+    @Override
+    public void outputWithTimestampHelper(
+        WindowedValue inElement,
+        Object output,
+        Instant timestamp,
+        Collector outCollector) {}
+  }
+
 
 }
