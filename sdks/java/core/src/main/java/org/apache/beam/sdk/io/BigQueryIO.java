05698595e793f4bf52aa8e64484c5f545283ee54
Lucas Amorim <lucasamorim@protonmail.com>
2016-06-28 14:28:17 -0700

    [BEAM-142] - BigQueryIO: don't unnecessarily initialize an ExecutorService to validate parameters
    By default, BigQueryIO initializes a ExecutorContext, however AppEngine
    doesn't allow modification of threads to be daemon
    threads. Using GcsOptions.ExecutorContext to create the
    executorContext.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/io/BigQueryIO.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/io/BigQueryIO.java
@@ -1594,7 +1594,7 @@ public class BigQueryIO {
           TableReference table) {
         try {
           Bigquery client = Transport.newBigQueryClient(options).build();
-          BigQueryTableInserter inserter = new BigQueryTableInserter(client);
+          BigQueryTableInserter inserter = new BigQueryTableInserter(client, options);
           if (!inserter.isEmpty(table)) {
             throw new IllegalArgumentException(
                 "BigQuery table is not empty: " + BigQueryIO.toTableSpec(table));
@@ -2109,7 +2109,7 @@ public class BigQueryIO {
           if (!createdTables.contains(tableSpec)) {
             TableSchema tableSchema = JSON_FACTORY.fromString(jsonTableSchema, TableSchema.class);
             Bigquery client = Transport.newBigQueryClient(options).build();
-            BigQueryTableInserter inserter = new BigQueryTableInserter(client);
+            BigQueryTableInserter inserter = new BigQueryTableInserter(client, options);
             inserter.getOrCreateTable(tableReference, WriteDisposition.WRITE_APPEND,
                 CreateDisposition.CREATE_IF_NEEDED, tableSchema);
             createdTables.add(tableSpec);
