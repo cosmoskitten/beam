60ae867ad5b4e9c085ff53a218179add69c01260
Thomas Groh <tgroh@google.com>
2016-07-14 14:51:02 -0700

    Add DoFn#setup and DoFn#teardown
    These methods are called to do expensive setup work, and to clean up a
    DoFn before it is discarded.

--- runners/direct-java/src/main/java/org/apache/beam/runners/direct/ParDoMultiEvaluatorFactory.java
+++ runners/direct-java/src/main/java/org/apache/beam/runners/direct/ParDoMultiEvaluatorFactory.java
@@ -71,9 +71,7 @@ class ParDoMultiEvaluatorFactory implements TransformEvaluatorFactory {
 
   @Override
   public void cleanup() throws Exception {
-    for (DoFnLifecycleManager lifecycleManager : fnClones.asMap().values()) {
-      lifecycleManager.removeAll();
-    }
+    DoFnLifecycleManagers.removeAllFromManagers(fnClones.asMap().values());
   }
 
   private <InT, OuT> TransformEvaluator<InT> createMultiEvaluator(
@@ -82,8 +80,7 @@ class ParDoMultiEvaluatorFactory implements TransformEvaluatorFactory {
       EvaluationContext evaluationContext) throws Exception {
     Map<TupleTag<?>, PCollection<?>> outputs = application.getOutput().getAll();
 
-    DoFnLifecycleManager fnLocal =
-        fnClones.getUnchecked((AppliedPTransform) application);
+    DoFnLifecycleManager fnLocal = fnClones.getUnchecked((AppliedPTransform) application);
     String stepName = evaluationContext.getStepName(application);
     DirectStepContext stepContext =
         evaluationContext.getExecutionContext(application, inputBundle.getKey())
