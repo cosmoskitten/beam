960e8db24aa96be812d81dc86c75ededddcc9a0d
Dan Halperin <dhalperi@google.com>
2016-08-05 08:51:15 -0700

    TextIO: test compression with larger files
    It is important for correctness that we test with large files
    because otherwise the compressed file may be larger than the
    uncompressed file, which could mask bugs

--- sdks/java/core/src/test/java/org/apache/beam/sdk/io/TextIOTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/io/TextIOTest.java
@@ -638,6 +638,33 @@ public class TextIOTest {
     p.run();
   }
 
+
+  @Test
+  @Category(NeedsRunner.class)
+  public void testGZIPReadWhenCompressed() throws Exception {
+    final int numLines = 1000;
+    File tmpFile = tmpFolder.newFile();
+    String filename = tmpFile.getPath();
+
+    List<String> expected = new ArrayList<>();
+    try (PrintStream writer = new PrintStream(
+        new GZIPOutputStream(new FileOutputStream(tmpFile)))) {
+      for (int i = 0; i < numLines; ++i) {
+        String line = "word" + i;
+        writer.println(line);
+        expected.add(line);
+      }
+    }
+
+    Pipeline p = TestPipeline.create();
+    TextIO.Read.Bound<String> read =
+        TextIO.Read.from(filename).withCompressionType(CompressionType.GZIP);
+    PCollection<String> output = p.apply(read);
+
+    PAssert.that(output).containsInAnyOrder(expected);
+    p.run();
+  }
+
   @Test
   public void testTextIOGetName() {
     assertEquals("TextIO.Read", TextIO.Read.from("somefile").getName());
