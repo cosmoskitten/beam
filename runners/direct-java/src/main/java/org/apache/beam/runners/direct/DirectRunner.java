e5b071f91ca39fb2b468e3da389f4d19dce9f447
Eugene Kirpichov <kirpichov@google.com>
2016-08-11 17:13:53 -0700

    [BEAM-65] SplittableDoFn prototype.
    Work in progress. Currently only runs in direct runner,
    and not ready for any use by real users.

--- runners/direct-java/src/main/java/org/apache/beam/runners/direct/DirectRunner.java
+++ runners/direct-java/src/main/java/org/apache/beam/runners/direct/DirectRunner.java
@@ -28,6 +28,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.apache.beam.runners.core.GBKIntoKeyedWorkItems;
 import org.apache.beam.runners.direct.DirectGroupByKey.DirectGroupByKeyOnly;
 import org.apache.beam.runners.direct.DirectRunner.DirectPipelineResult;
 import org.apache.beam.runners.direct.TestStreamEvaluatorFactory.DirectTestStreamFactory;
@@ -81,6 +82,10 @@ public class DirectRunner
               .put(GroupByKey.class, new DirectGroupByKeyOverrideFactory())
               .put(TestStream.class, new DirectTestStreamFactory())
               .put(Write.Bound.class, new WriteWithShardingFactory())
+              .put(ParDo.Bound.class, new ParDoOverrideFactory())
+              .put(
+                  GBKIntoKeyedWorkItems.class,
+                  new DirectGBKIntoKeyedWorkItemsOverrideFactory())
               .build();
 
   /**
