06c28bbdec6537d672e2bd27ca71e483bd16a72c
Pei He <peihe@google.com>
2016-10-05 15:52:57 -0700

    test travis

---
+++ .travis/test_archetype.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mvn archetype:generate \
+  -DarchetypeArtifactId=beam-sdks-java-maven-archetypes-examples \
+  -DarchetypeGroupId=org.apache.beam \
+  -DarchetypeVersion=0.3.0-incubating-SNAPSHOT \
+  -DgroupId=com.example \
+  -DartifactId=test-beam-archetypes \
+  -Dversion="0.3.0-incubating-SNAPSHOT" \
+  -DinteractiveMode=false \
+  -Dpackage=org.apache.beam.examples
+
+cd test-beam-archetypes
+
+mvn clean install
