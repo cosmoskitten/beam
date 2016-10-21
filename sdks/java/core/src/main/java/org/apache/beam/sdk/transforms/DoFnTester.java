88d6b528d2bf98c1130191b6be11d5c9119fdf6c
Kenneth Knowles <klk@google.com>
2016-10-21 14:17:40 -0700

    fixup! DoFnTester unused var

--- sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/DoFnTester.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/DoFnTester.java
@@ -785,8 +785,6 @@ public class DoFnTester<InputT, OutputT> implements AutoCloseable {
 
   private Map<String, Object> accumulators;
 
-  private Map<String, Aggregator> aggregators;
-
   /** The output tags used by the {@link DoFn} under test. */
   private TupleTag<OutputT> mainOutputTag = new TupleTag<>();
 
