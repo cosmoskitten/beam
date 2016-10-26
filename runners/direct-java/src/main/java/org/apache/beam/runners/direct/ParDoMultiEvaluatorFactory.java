1a33f760a9b18b4da79bf3825b64c01718676df9
Eugene Kirpichov <kirpichov@google.com>
2016-10-26 16:34:47 -0700

    Deduplicates ParDo{Single,Multi}EvaluatorFactory
    This is in preparation for adding a third one for a future ParDo-like primitive
    transform to be introduced inside SplittableParDo.

--- runners/direct-java/src/main/java/org/apache/beam/runners/direct/ParDoMultiEvaluatorFactory.java
+++ runners/direct-java/src/main/java/org/apache/beam/runners/direct/ParDoMultiEvaluatorFactory.java
@@ -1,5 +1,5 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one
+ * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
@@ -17,91 +17,46 @@
  */
 package org.apache.beam.runners.direct;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import java.util.Map;
 import org.apache.beam.runners.direct.DirectExecutionContext.DirectStepContext;
-import org.apache.beam.runners.direct.DirectRunner.CommittedBundle;
 import org.apache.beam.sdk.transforms.AppliedPTransform;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
-import org.apache.beam.sdk.transforms.ParDo.BoundMulti;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionTuple;
-import org.apache.beam.sdk.values.TupleTag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * The {@link DirectRunner} {@link TransformEvaluatorFactory} for the
- * {@link BoundMulti} primitive {@link PTransform}.
+ * The {@link DirectRunner} {@link TransformEvaluatorFactory} for the {@link ParDo.BoundMulti}
+ * primitive {@link PTransform}.
  */
-class ParDoMultiEvaluatorFactory implements TransformEvaluatorFactory {
-  private static final Logger LOG = LoggerFactory.getLogger(ParDoMultiEvaluatorFactory.class);
-  private final LoadingCache<AppliedPTransform<?, ?, ?>, DoFnLifecycleManager> fnClones;
-  private final EvaluationContext evaluationContext;
-
+class ParDoMultiEvaluatorFactory<InputT, OutputT>
+    extends ParDoEvaluatorFactoryBase<
+        InputT, OutputT, PCollectionTuple, ParDo.BoundMulti<InputT, OutputT>> {
   public ParDoMultiEvaluatorFactory(EvaluationContext evaluationContext) {
-    this.evaluationContext = evaluationContext;
-    fnClones = CacheBuilder.newBuilder()
-        .build(new CacheLoader<AppliedPTransform<?, ?, ?>, DoFnLifecycleManager>() {
-          @Override
-          public DoFnLifecycleManager load(AppliedPTransform<?, ?, ?> key)
-              throws Exception {
-            BoundMulti<?, ?> bound = (BoundMulti<?, ?>) key.getTransform();
-            return DoFnLifecycleManager.of(bound.getNewFn());
-          }
-        });
+    super(evaluationContext);
   }
 
   @Override
-  public <T> TransformEvaluator<T> forApplication(
-      AppliedPTransform<?, ?, ?> application, CommittedBundle<?> inputBundle) throws Exception {
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    TransformEvaluator<T> evaluator =
-        createMultiEvaluator((AppliedPTransform) application, inputBundle);
-    return evaluator;
+  DoFn<InputT, OutputT> getDoFn(
+      ParDo.BoundMulti<InputT, OutputT> transform) {
+    return transform.getNewFn();
   }
 
   @Override
-  public void cleanup() throws Exception {
-    DoFnLifecycleManagers.removeAllFromManagers(fnClones.asMap().values());
-  }
-
-  private <InT, OuT> TransformEvaluator<InT> createMultiEvaluator(
-      AppliedPTransform<PCollection<InT>, PCollectionTuple, BoundMulti<InT, OuT>> application,
-      CommittedBundle<InT> inputBundle) throws Exception {
-    Map<TupleTag<?>, PCollection<?>> outputs = application.getOutput().getAll();
-
-    DoFnLifecycleManager fnLocal = fnClones.getUnchecked((AppliedPTransform) application);
-    String stepName = evaluationContext.getStepName(application);
-    DirectStepContext stepContext =
-        evaluationContext.getExecutionContext(application, inputBundle.getKey())
-            .getOrCreateStepContext(stepName, stepName);
-    try {
-      @SuppressWarnings({"unchecked", "rawtypes"})
-      TransformEvaluator<InT> parDoEvaluator =
-          ParDoEvaluator.create(
-              evaluationContext,
-              stepContext,
-              inputBundle,
-              application,
-              (DoFn) fnLocal.get(),
-              application.getTransform().getSideInputs(),
-              application.getTransform().getMainOutputTag(),
-              application.getTransform().getSideOutputTags().getAll(),
-              outputs);
-      return DoFnLifecycleManagerRemovingTransformEvaluator.wrapping(parDoEvaluator, fnLocal);
-    } catch (Exception e) {
-      try {
-        fnLocal.remove();
-      } catch (Exception removalException) {
-        LOG.error("Exception encountered while cleaning up in ParDo evaluator construction",
-            removalException);
-        e.addSuppressed(removalException);
-      }
-      throw e;
-    }
+  ParDoEvaluator<InputT, OutputT> createParDoEvaluator(
+      AppliedPTransform<PCollection<InputT>, PCollectionTuple, ParDo.BoundMulti<InputT, OutputT>>
+          application,
+      DirectStepContext stepContext,
+      DoFn<InputT, OutputT> fnLocal) {
+    ParDo.BoundMulti<InputT, OutputT> transform = application.getTransform();
+    return ParDoEvaluator.create(
+        evaluationContext,
+        stepContext,
+        application,
+        fnLocal,
+        transform.getSideInputs(),
+        transform.getMainOutputTag(),
+        transform.getSideOutputTags().getAll(),
+        application.getOutput().getAll());
   }
 }
