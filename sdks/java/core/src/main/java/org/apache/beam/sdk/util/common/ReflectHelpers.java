684c5e0bca723749d1c4133f599a9e1b0835293d
Pei He <peihe@google.com>
2016-11-01 11:43:32 -0700

    fixup! address comments-3

--- sdks/java/core/src/main/java/org/apache/beam/sdk/util/common/ReflectHelpers.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/util/common/ReflectHelpers.java
@@ -21,11 +21,13 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static java.util.Arrays.asList;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Queues;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
@@ -44,7 +46,9 @@ public class ReflectHelpers {
 
   private static final Joiner COMMA_SEPARATOR = Joiner.on(", ");
 
-  /** A {@link Function} that turns a method into a simple method signature. */
+  /**
+   * A {@link Function} that turns a method into a simple method signature.
+   */
   public static final Function<Method, String> METHOD_FORMATTER = new Function<Method, String>() {
     @Override
     public String apply(@Nonnull Method input) {
@@ -57,98 +61,106 @@ public class ReflectHelpers {
     }
   };
 
-  /** A {@link Function} that turns a method into the declaring class + method signature. */
+  /**
+   * A {@link Function} that turns a method into the declaring class + method signature.
+   */
   public static final Function<Method, String> CLASS_AND_METHOD_FORMATTER =
       new Function<Method, String>() {
-    @Override
-    public String apply(@Nonnull Method input) {
-      return String.format("%s#%s",
-          CLASS_NAME.apply(input.getDeclaringClass()),
-          METHOD_FORMATTER.apply(input));
-    }
-  };
+        @Override
+        public String apply(@Nonnull Method input) {
+          return String.format("%s#%s",
+              CLASS_NAME.apply(input.getDeclaringClass()),
+              METHOD_FORMATTER.apply(input));
+        }
+      };
 
-  /** A {@link Function} with returns the classes name. */
+  /**
+   * A {@link Function} with returns the classes name.
+   */
   public static final Function<Class<?>, String> CLASS_NAME =
       new Function<Class<?>, String>() {
-    @Override
-    public String apply(@Nonnull Class<?> input) {
-      return input.getName();
-    }
-  };
+        @Override
+        public String apply(@Nonnull Class<?> input) {
+          return input.getName();
+        }
+      };
 
-  /** A {@link Function} with returns the classes name. */
+  /**
+   * A {@link Function} with returns the classes name.
+   */
   public static final Function<Class<?>, String> CLASS_SIMPLE_NAME =
       new Function<Class<?>, String>() {
-    @Override
-    public String apply(@Nonnull Class<?> input) {
-      return input.getSimpleName();
-    }
-  };
+        @Override
+        public String apply(@Nonnull Class<?> input) {
+          return input.getSimpleName();
+        }
+      };
 
-  /** A {@link Function} that formats types. */
+  /**
+   * A {@link Function} that formats types.
+   */
   public static final Function<Type, String> TYPE_SIMPLE_DESCRIPTION =
       new Function<Type, String>() {
-    @Override
-    @Nullable
-    public String apply(@Nonnull Type input) {
-      StringBuilder builder = new StringBuilder();
-      format(builder, input);
-      return builder.toString();
-    }
-
-    private void format(StringBuilder builder, Type t) {
-      if (t instanceof Class) {
-        formatClass(builder, (Class<?>) t);
-      } else if (t instanceof TypeVariable) {
-        formatTypeVariable(builder, (TypeVariable<?>) t);
-      } else if (t instanceof WildcardType) {
-        formatWildcardType(builder, (WildcardType) t);
-      } else if (t instanceof ParameterizedType) {
-        formatParameterizedType(builder, (ParameterizedType) t);
-      } else if (t instanceof GenericArrayType) {
-        formatGenericArrayType(builder, (GenericArrayType) t);
-      } else {
-        builder.append(t.toString());
-      }
-    }
-
-    private void formatClass(StringBuilder builder, Class<?> clazz) {
-      builder.append(clazz.getSimpleName());
-    }
-
-    private void formatTypeVariable(StringBuilder builder, TypeVariable<?> t) {
-      builder.append(t.getName());
-    }
-
-    private void formatWildcardType(StringBuilder builder, WildcardType t) {
-      builder.append("?");
-      for (Type lowerBound : t.getLowerBounds()) {
-        builder.append(" super ");
-        format(builder, lowerBound);
-      }
-      for (Type upperBound : t.getUpperBounds()) {
-        if (!Object.class.equals(upperBound)) {
-          builder.append(" extends ");
-          format(builder, upperBound);
+        @Override
+        @Nullable
+        public String apply(@Nonnull Type input) {
+          StringBuilder builder = new StringBuilder();
+          format(builder, input);
+          return builder.toString();
         }
-      }
-    }
 
-    private void formatParameterizedType(StringBuilder builder, ParameterizedType t) {
-      format(builder, t.getRawType());
-      builder.append('<');
-      COMMA_SEPARATOR.appendTo(builder,
-          FluentIterable.from(asList(t.getActualTypeArguments()))
-          .transform(TYPE_SIMPLE_DESCRIPTION));
-      builder.append('>');
-    }
+        private void format(StringBuilder builder, Type t) {
+          if (t instanceof Class) {
+            formatClass(builder, (Class<?>) t);
+          } else if (t instanceof TypeVariable) {
+            formatTypeVariable(builder, (TypeVariable<?>) t);
+          } else if (t instanceof WildcardType) {
+            formatWildcardType(builder, (WildcardType) t);
+          } else if (t instanceof ParameterizedType) {
+            formatParameterizedType(builder, (ParameterizedType) t);
+          } else if (t instanceof GenericArrayType) {
+            formatGenericArrayType(builder, (GenericArrayType) t);
+          } else {
+            builder.append(t.toString());
+          }
+        }
 
-    private void formatGenericArrayType(StringBuilder builder, GenericArrayType t) {
-      format(builder, t.getGenericComponentType());
-      builder.append("[]");
-    }
-  };
+        private void formatClass(StringBuilder builder, Class<?> clazz) {
+          builder.append(clazz.getSimpleName());
+        }
+
+        private void formatTypeVariable(StringBuilder builder, TypeVariable<?> t) {
+          builder.append(t.getName());
+        }
+
+        private void formatWildcardType(StringBuilder builder, WildcardType t) {
+          builder.append("?");
+          for (Type lowerBound : t.getLowerBounds()) {
+            builder.append(" super ");
+            format(builder, lowerBound);
+          }
+          for (Type upperBound : t.getUpperBounds()) {
+            if (!Object.class.equals(upperBound)) {
+              builder.append(" extends ");
+              format(builder, upperBound);
+            }
+          }
+        }
+
+        private void formatParameterizedType(StringBuilder builder, ParameterizedType t) {
+          format(builder, t.getRawType());
+          builder.append('<');
+          COMMA_SEPARATOR.appendTo(builder,
+              FluentIterable.from(asList(t.getActualTypeArguments()))
+                  .transform(TYPE_SIMPLE_DESCRIPTION));
+          builder.append('>');
+        }
+
+        private void formatGenericArrayType(StringBuilder builder, GenericArrayType t) {
+          format(builder, t.getGenericComponentType());
+          builder.append("[]");
+        }
+      };
 
   /**
    * Returns all the methods visible from the provided interfaces.
@@ -164,7 +176,7 @@ public class ReflectHelpers {
           public Iterable<Method> apply(@Nonnull Class<?> input) {
             return getClosureOfMethodsOnInterface(input);
           }
-    });
+        });
   }
 
   /**
@@ -185,5 +197,20 @@ public class ReflectHelpers {
       interfacesToProcess.addAll(Arrays.asList(current.getInterfaces()));
     }
     return builder.build();
+  }
+
+  /**
+   * Returns a concise string for printing a {@link Annotation}.
+   *
+   * @param annotation The {@link Annotation} to print.
+   * @return a concise string representation that doesn't contain the package name.
+   */
+  public static String toStringForPrint(Annotation annotation) {
+    String annotationName = annotation.annotationType().getName();
+    String annotationNameWithoutPackage =
+        annotationName.substring(annotationName.lastIndexOf('.') + 1).replace('$', '.');
+    String annotationToString = annotation.toString();
+    String values = annotationToString.substring(annotationToString.indexOf('('));
+    return String.format("%s%s", annotationNameWithoutPackage, values);
   }
 }
