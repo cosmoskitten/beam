c51da5ec7e576efed13aed4d77287f5f25984ddd
Kenneth Knowles <klk@google.com>
2016-11-03 14:50:02 -0700

    Revise DebuggingWordCount to be more portable

--- examples/java/src/test/java/org/apache/beam/examples/DebuggingWordCountTest.java
+++ examples/java/src/test/java/org/apache/beam/examples/DebuggingWordCountTest.java
@@ -20,6 +20,11 @@ package org.apache.beam.examples;
 import com.google.common.io.Files;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import org.apache.beam.examples.DebuggingWordCount.WordCountOptions;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.testing.TestPipelineOptions;
+import org.apache.beam.sdk.util.IOChannelUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -35,9 +40,16 @@ public class DebuggingWordCountTest {
 
   @Test
   public void testDebuggingWordCount() throws Exception {
-    File file = tmpFolder.newFile();
-    Files.write("stomach secret Flourish message Flourish here Flourish", file,
+    File inputFile = tmpFolder.newFile();
+    File outputFile = tmpFolder.newFile();
+    Files.write(
+        "stomach secret Flourish message Flourish here Flourish",
+        inputFile,
         StandardCharsets.UTF_8);
-    DebuggingWordCount.main(new String[]{"--inputFile=" + file.getAbsolutePath()});
+    WordCountOptions options =
+        TestPipeline.testingPipelineOptions().as(WordCountOptions.class);
+    options.setInputFile(inputFile.getAbsolutePath());
+    options.setOutput(outputFile.getAbsolutePath());
+    DebuggingWordCount.main(TestPipeline.convertToArgs(options));
   }
 }
