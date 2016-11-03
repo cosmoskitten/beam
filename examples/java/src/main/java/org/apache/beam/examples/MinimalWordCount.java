aa6bdd87b7d53a8b6f9b3b2088225c4cbbcf2e76
Kenneth Knowles <klk@google.com>
2016-11-03 14:18:43 -0700

    Hardcode MinimalWordCount to the DirectRunner
    This makes it easy to immediately run, and removes various
    non-portable instructions and others that aren't the easiest
    for a "Getting Started" scenario.

--- examples/java/src/main/java/org/apache/beam/examples/MinimalWordCount.java
+++ examples/java/src/main/java/org/apache/beam/examples/MinimalWordCount.java
@@ -17,6 +17,7 @@
  */
 package org.apache.beam.examples;
 
+import org.apache.beam.runners.direct.DirectRunner;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
@@ -45,37 +46,29 @@ import org.apache.beam.sdk.values.KV;
  * <pre>
  *   1. Reading data from text files
  *   2. Specifying 'inline' transforms
- *   3. Counting a PCollection
- *   4. Writing data to Cloud Storage as text files
+ *   3. Counting items in a PCollection
+ *   4. Writing data to text files
  * </pre>
  *
- * <p>To execute this pipeline, first edit the code to set your project ID, the temp
- * location, and the output location. The specified GCS bucket(s) must already exist.
+ * <p>To execute this pipeline, first edit the output file to a location on your local disk
+ * (look for the string 'YOUR_OUTPUT_FILE' near the bottom of the file).
+ * This example uses the {@link DirectRunner}, so writing to local disk is OK. When running
+ * on a distributed service, you would use an appropriate file service.
  *
  * <p>Then, run the pipeline as described in the README. It will be deployed and run with the
  * selected runner. No args are required to run the pipeline. You can see the results in your
- * output bucket in the GCS browser.
+ * output file.
  */
 public class MinimalWordCount {
 
   public static void main(String[] args) {
     // Create a PipelineOptions object. This object lets us set various execution
-    // options for our pipeline, such as the associated Cloud Platform project and the location
-    // in Google Cloud Storage to stage files.
+    // options for our pipeline, such as the runner you wish to use.
     PipelineOptions options = PipelineOptionsFactory.create();
 
-    // In order to run your pipeline, you need to make following runner specific changes:
-    //
-    // CHANGE 1/3: Select a Beam runner, such as DataflowRunner or FlinkRunner.
-    // CHANGE 2/3: Specify runner-required options.
-    // For DataflowRunner, set project and temp location as follows:
-    //   DataflowPipelineOptions dataflowOptions = options.as(DataflowPipelineOptions.class);
-    //   dataflowOptions.setRunner(DataflowRunner.class);
-    //   dataflowOptions.setProject("SET_YOUR_PROJECT_ID_HERE");
-    //   dataflowOptions.setTempLocation("gs://SET_YOUR_BUCKET_NAME_HERE/AND_TEMP_DIRECTORY");
-    // For FlinkRunner, set the runner as follows. See {@code FlinkPipelineOptions}
-    // for more details.
-    //   options.setRunner(FlinkRunner.class);
+    // For this example, we hardcode the DirectRunner. In subsequent examples we will demonstrate
+    // how to to choose a runner on the command line.
+    options.setRunner(DirectRunner.class);
 
     // Create the Pipeline object with the options we defined above.
     Pipeline p = Pipeline.create(options);
@@ -85,7 +78,10 @@ public class MinimalWordCount {
     // Concept #1: Apply a root transform to the pipeline; in this case, TextIO.Read to read a set
     // of input text files. TextIO.Read returns a PCollection where each element is one line from
     // the input text (a set of Shakespeare's texts).
+
+    // This example reads a public data set consisting of the complete works of Shakespeare.
     p.apply(TextIO.Read.from("gs://apache-beam-samples/shakespeare/*"))
+
      // Concept #2: Apply a ParDo transform to our PCollection of text lines. This ParDo invokes a
      // DoFn (defined in-line) on each element that tokenizes the text line into individual words.
      // The ParDo returns a PCollection<String>, where each element is an individual word in
@@ -100,10 +96,12 @@ public class MinimalWordCount {
                          }
                        }
                      }))
+
      // Concept #3: Apply the Count transform to our PCollection of individual words. The Count
      // transform returns a new PCollection of key/value pairs, where each key represents a unique
      // word in the text. The associated value is the occurrence count for that word.
      .apply(Count.<String>perElement())
+
      // Apply a MapElements transform that formats our PCollection of word counts into a printable
      // string, suitable for writing to an output file.
      .apply("FormatResults", MapElements.via(new SimpleFunction<KV<String, Long>, String>() {
@@ -112,11 +110,13 @@ public class MinimalWordCount {
                          return input.getKey() + ": " + input.getValue();
                        }
                      }))
+
      // Concept #4: Apply a write transform, TextIO.Write, at the end of the pipeline.
      // TextIO.Write writes the contents of a PCollection (in this case, our PCollection of
-     // formatted strings) to a series of text files in Google Cloud Storage.
-     // CHANGE 3/3: The Google Cloud Storage path is required for outputting the results to.
-     .apply(TextIO.Write.to("gs://YOUR_OUTPUT_BUCKET/AND_OUTPUT_PREFIX"));
+     // formatted strings) to a series of text files.
+     //
+     // Replace the string 'YOUR_OUTPUT_FILE' with an actual location on your local disk.
+     .apply(TextIO.Write.to("YOUR_OUTPUT_FILE"));
 
     // Run the pipeline.
     p.run().waitUntilFinish();
