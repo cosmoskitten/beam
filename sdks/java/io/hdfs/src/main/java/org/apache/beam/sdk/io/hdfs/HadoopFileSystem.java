2a3c795a800b130195af3438e59992d386f5c2c2
Pei He <peihe@google.com>
2016-12-14 17:09:06 -0800

    fixup! address comments.

--- sdks/java/io/hdfs/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopFileSystem.java
+++ sdks/java/io/hdfs/src/main/java/org/apache/beam/sdk/io/hdfs/HadoopFileSystem.java
@@ -22,7 +22,7 @@ import org.apache.beam.sdk.io.FileSystem;
 /**
  * Adapts {@link org.apache.hadoop.fs.FileSystem} connectors to be used as Apache Beam {@link FileSystem FileSystems}.
  */
-class HadoopFileSystem implements FileSystem {
+class HadoopFileSystem extends FileSystem {
 
   HadoopFileSystem() {}
 }
