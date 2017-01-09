66985d96a8fa44898ad3391afe142927ba94ec7b
Jason Kuster <jasonkuster@google.com>
2017-01-09 13:38:31 -0800

    Export to BigQuery and mark run as official.
    Signed-off-by: Jason Kuster <jasonkuster@google.com>

--- .jenkins/job_beam_PerformanceTests_Dataflow.groovy
+++ .jenkins/job_beam_PerformanceTests_Dataflow.groovy
@@ -5,7 +5,7 @@ job('beam_PerformanceTests_Dataflow'){
     steps {
         shell('git clone -b apache --single-branch https://github.com/jasonkuster/PerfKitBenchmarker.git')
         python{
-            command('PerfKitBenchmarker/pkb.py --project=google.com:clouddfe --benchmarks=dpb_wordcount_benchmark --dpb_dataflow_staging_location=gs://temp-storage-for-perf-tests/staging --dpb_dataflow_jar=./ --dpb_wordcount_input=dataflow-samples/shakespeare/kinglear.txt --dpb_log_level=INFO --config_override=dpb_wordcount_benchmark.dpb_service.service_type=dataflow')
+            command('PerfKitBenchmarker/pkb.py --project=google.com:clouddfe --benchmarks=dpb_wordcount_benchmark --dpb_dataflow_staging_location=gs://temp-storage-for-perf-tests/staging --dpb_dataflow_jar=./ --dpb_wordcount_input=dataflow-samples/shakespeare/kinglear.txt --dpb_log_level=INFO --config_override=dpb_wordcount_benchmark.dpb_service.service_type=dataflow --bigquery_table=beam_performance.pkb_results --official=true')
         }
     }
 }
--- .jenkins/job_beam_PerformanceTests_Spark.groovy
+++ .jenkins/job_beam_PerformanceTests_Spark.groovy
@@ -5,7 +5,7 @@ job('beam_PerformanceTests_Spark'){
     steps {
         shell('git clone -b apache --single-branch https://github.com/jasonkuster/PerfKitBenchmarker.git')
         python{
-            command('PerfKitBenchmarker/pkb.py --project=google.com:clouddfe --benchmarks=dpb_wordcount_benchmark --dpb_wordcount_input=/etc/hosts --dpb_log_level=INFO --config_override=dpb_wordcount_benchmark.dpb_service.service_type=dataproc')
+            command('PerfKitBenchmarker/pkb.py --project=google.com:clouddfe --benchmarks=dpb_wordcount_benchmark --dpb_wordcount_input=/etc/hosts --dpb_log_level=INFO --config_override=dpb_wordcount_benchmark.dpb_service.service_type=dataproc --bigquery_table=beam_performance.pkb_results --official=true')
         }
     }
 }
