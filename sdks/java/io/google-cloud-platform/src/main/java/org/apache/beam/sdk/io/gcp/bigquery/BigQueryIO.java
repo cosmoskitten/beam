1394767b4692a9148fb572117308698484094591
Pei He <peihe@google.com>
2017-01-23 14:52:30 -0800

    [BEAM-1258] Improve logging in BigQueryIO.verifyTableEmpty().

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryIO.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryIO.java
@@ -1863,25 +1863,60 @@ public class BigQueryIO {
             writeDisposition, validate, testServices);
       }
 
-      private static void verifyTableEmpty(
+      private static void verifyTableNotExistOrEmpty(
           DatasetService datasetService,
-          TableReference table) {
+          TableReference tableRef) {
         try {
-          boolean isEmpty = datasetService.isTableEmpty(
-              table.getProjectId(), table.getDatasetId(), table.getTableId());
-          if (!isEmpty) {
-            throw new IllegalArgumentException(
-                "BigQuery table is not empty: " + BigQueryIO.toTableSpec(table));
+          if (datasetService.getTable(
+              tableRef.getProjectId(),
+              tableRef.getDatasetId(),
+              tableRef.getTableId()) == null) {
+            return;
+          }
+          checkState(
+              datasetService.isTableEmpty(
+                  tableRef.getProjectId(), tableRef.getDatasetId(), tableRef.getTableId()),
+              "BigQuery table is not empty: " + BigQueryIO.toTableSpec(tableRef));
+        } catch (IOException | InterruptedException e) {
+          if (e instanceof  InterruptedException) {
+            Thread.currentThread().interrupt();
+          }
+          throw new RuntimeException(
+              "unable to confirm BigQuery table emptiness for table "
+                  + BigQueryIO.toTableSpec(tableRef), e);
+        }
+      }
+
+
+      /**
+       * Create the table if it does not exist.
+       *
+       * @return true if a new table was created.
+       */
+      private boolean createTableIfNeeded(
+          DatasetService datasetService,
+          TableReference tableRef,
+          String jsonTableSchema) {
+        try {
+          if (datasetService.getTable(
+              tableRef.getProjectId(),
+              tableRef.getDatasetId(),
+              tableRef.getTableId()) == null) {
+            TableSchema tableSchema = JSON_FACTORY.fromString(
+                jsonTableSchema, TableSchema.class);
+            datasetService.createTable(
+                new Table().setTableReference(tableRef).setSchema(tableSchema));
+            return true;
+          } else {
+            return false;
           }
         } catch (IOException | InterruptedException e) {
-          ApiErrorExtractor errorExtractor = new ApiErrorExtractor();
-          if (e instanceof IOException && errorExtractor.itemNotFound((IOException) e)) {
-            // Nothing to do. If the table does not exist, it is considered empty.
-          } else {
-            throw new RuntimeException(
-                "unable to confirm BigQuery table emptiness for table "
-                    + BigQueryIO.toTableSpec(table), e);
+          if (e instanceof  InterruptedException) {
+            Thread.currentThread().interrupt();
           }
+          throw new RuntimeException(
+              "unable to get or create BigQuery table: " + BigQueryIO.toTableSpec(tableRef),
+              e);
         }
       }
 
@@ -1913,11 +1948,13 @@ public class BigQueryIO {
           // stage of the pipeline. For these cases the #withoutValidation method can be used to
           // disable the check.
           verifyDatasetPresence(datasetService, table);
-          if (getCreateDisposition() == BigQueryIO.Write.CreateDisposition.CREATE_NEVER) {
+          if (getCreateDisposition() == CreateDisposition.CREATE_NEVER) {
             verifyTablePresence(datasetService, table);
+          } else if (getCreateDisposition() == CreateDisposition.CREATE_IF_NEEDED) {
+            createTableIfNeeded(datasetService, table, jsonSchema.get());
           }
           if (getWriteDisposition() == BigQueryIO.Write.WriteDisposition.WRITE_EMPTY) {
-            verifyTableEmpty(datasetService, table);
+            verifyTableNotExistOrEmpty(datasetService, table);
           }
         }
 
