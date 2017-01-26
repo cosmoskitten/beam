77006ba21ce8733ff2c9826d2e8f8d3b4913555a
Ismaël Mejía <iemejia@gmail.com>
2017-01-26 11:08:42 +0100

    [BEAM-1297] Use client JVM and simple JIT profiling for the build run

--- .jenkins/common_job_properties.groovy
+++ .jenkins/common_job_properties.groovy
@@ -138,8 +138,12 @@ class common_job_properties {
   // Sets common config for Maven jobs.
   static def setMavenConfig(def context) {
     context.mavenInstallation('Maven 3.3.3')
+    context.mavenOpts('-client')
     context.mavenOpts('-Dorg.slf4j.simpleLogger.showDateTime=true')
     context.mavenOpts('-Dorg.slf4j.simpleLogger.dateTimeFormat=yyyy-MM-dd\\\'T\\\'HH:mm:ss.SSS')
+    context.mavenOpts('-XX:+TieredCompilation')
+    context.mavenOpts('-XX:TieredStopAtLevel=1')
+    context.mavenOpts('-XX:+BytecodeVerificationLocal')
     context.rootPOM('pom.xml')
     // Use a repository local to the workspace for better isolation of jobs.
     context.localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
