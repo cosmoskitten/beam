bb517fd0aceee032de37c0f53be67aef209cd0a8
Kenneth Knowles <klk@google.com>
2017-01-26 21:17:18 -0800

    Move TimerInternals to runners/core-java

--- runners/core-java/src/test/java/org/apache/beam/runners/core/SplittableParDoTest.java
+++ runners/core-java/src/test/java/org/apache/beam/runners/core/SplittableParDoTest.java
@@ -47,7 +47,6 @@ import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
 import org.apache.beam.sdk.transforms.windowing.GlobalWindow;
 import org.apache.beam.sdk.transforms.windowing.IntervalWindow;
 import org.apache.beam.sdk.transforms.windowing.PaneInfo;
-import org.apache.beam.sdk.util.TimerInternals;
 import org.apache.beam.sdk.util.WindowedValue;
 import org.apache.beam.sdk.util.state.StateInternals;
 import org.apache.beam.sdk.values.PCollection;
