b00ecb5d89e86c19a14075285b6ce33cf187f0aa
Radhika S Kulkarni <radhika_kulkarni1@persistent.co.in>
2017-02-06 20:46:35 +0530

    Added synchronization for currentReader in start() method

--- sdks/java/io/hadoop-input-format/src/main/java/org/apache/beam/sdk/io/hadoop/inputformat/HadoopInputFormatIO.java
+++ sdks/java/io/hadoop-input-format/src/main/java/org/apache/beam/sdk/io/hadoop/inputformat/HadoopInputFormatIO.java
@@ -582,10 +582,10 @@ public class HadoopInputFormatIO {
       return toBuilder().setValueTranslationFunction(function)
           .setValueClass((TypeDescriptor<V>) function.getOutputTypeDescriptor()).build();
     }
-    
+
     @Override
     public PCollection<KV<K, V>> expand(PBegin input) {
-      // Get the key and value coders based on the key and value classes.      
+      // Get the key and value coders based on the key and value classes.
       CoderRegistry coderRegistry = input.getPipeline().getCoderRegistry();
       Coder<K> keyCoder = getDefaultCoder(getKeyClass(), coderRegistry);
       Coder<V> valueCoder = getDefaultCoder(getValueClass(), coderRegistry);
@@ -630,7 +630,7 @@ public class HadoopInputFormatIO {
       checkNotNull(configuration.get("value.class"),
           HadoopInputFormatIOConstants.MISSING_INPUTFORMAT_VALUE_CLASS_ERROR_MSG);
     }
-    
+
     /**
      * Validates inputs provided by the pipeline user before reading the data.
      */
@@ -760,7 +760,7 @@ public class HadoopInputFormatIO {
     private TaskAttemptContextImpl taskAttemptContext;
     private transient Class<?> expectedKeyClass;
     private transient Class<?> expectedValueClass;
-    
+
     HadoopInputFormatBoundedSource(
         SerializableConfiguration conf,
         Coder<K> keyCoder,
@@ -1064,7 +1064,7 @@ public class HadoopInputFormatIO {
          * coder.
          */
         return validateClassUsingCoder(property, coder);
-      }      
+      }
       /*
        * Validates key/value class with InputFormat's parameterized type.
        */
@@ -1126,7 +1126,7 @@ public class HadoopInputFormatIO {
       }
       return true;
     }
-   
+
     /**
      * Returns parameterized type of the InputFormat class.
      */
@@ -1159,7 +1159,7 @@ public class HadoopInputFormatIO {
       reader.nextKeyValue();
       return reader;
     }
-  
+
     @VisibleForTesting
     InputFormat<?, ?> getInputFormat(){
       return inputFormatObj;
@@ -1174,7 +1174,7 @@ public class HadoopInputFormatIO {
     public Coder<KV<K, V>> getDefaultOutputCoder() {
       return KvCoder.of(keyCoder, valueCoder);
     }
-    
+
     private Class<?> getExpectedKeyClass() {
       return expectedKeyClass;
     }
@@ -1229,7 +1229,7 @@ public class HadoopInputFormatIO {
 
     /**
      * BoundedReader for Hadoop InputFormat source.
-     * 
+     *
      * @param <K> Type of keys RecordReader emits.
      * @param <V> Type of values RecordReader emits.
      */
@@ -1308,16 +1308,21 @@ public class HadoopInputFormatIO {
 >>>>>>> Resolved most of the code review comments.
           currentReader =
               (RecordReader<K1, V1>) inputFormatObj.createRecordReader(split, taskAttemptContext);
-          if (currentReader != null) {
-            currentReader.initialize(split, taskAttemptContext);
-            if (currentReader.nextKeyValue()) {
-              recordsReturned++;
-              return true;
+          // currentReader object could be accessed concurrently by multiple sources. Hence to be on
+          // safer side, it has been added in synchronized block
+          synchronized (currentReader) {
+            if (currentReader != null) {
+              currentReader.initialize(split, taskAttemptContext);
+              if (currentReader.nextKeyValue()) {
+                recordsReturned++;
+                return true;
+              }
+            } else {
+              throw new IOException(String.format(
+                  HadoopInputFormatIOConstants.NULL_CREATE_RECORDREADER_ERROR_MSG,
+                  inputFormatObj.getClass()));
             }
-          } else {
-            throw new IOException(
-                String.format(HadoopInputFormatIOConstants.NULL_CREATE_RECORDREADER_ERROR_MSG,
-                    inputFormatObj.getClass()));
+            currentReader = null;
           }
         } catch (InterruptedException e) {
 <<<<<<< HEAD
@@ -1336,7 +1341,6 @@ public class HadoopInputFormatIO {
           throw new IOException("Unable to read data: ", e);
 >>>>>>> Changes for spaces, Constants file name and comments as per Stephens code review comments
         }
-        currentReader = null;
         doneReading = true;
         return false;
       }
@@ -1405,11 +1409,11 @@ public class HadoopInputFormatIO {
 >>>>>>> Resolved most of the code review comments.
         return KV.of(key, value);
       }
-      
+
       /**
-       * Returns the serialized output of transformed key or value object. 
-       * @throws ClassCastException 
-       * @throws CoderException 
+       * Returns the serialized output of transformed key or value object.
+       * @throws ClassCastException
+       * @throws CoderException
        */
       private <T, T1> T1 transformKeyOrValue(T input,
           @Nullable SimpleFunction<T, T1> simpleFunction, Coder<T1> coder) throws CoderException,
@@ -1477,15 +1481,15 @@ public class HadoopInputFormatIO {
       private boolean isKnownImmutable(Object o) {
         Set<Class<?>> immutableTypes = new HashSet<Class<?>>(
             Arrays.asList(
-                String.class, 
-                Byte.class, 
-                Short.class, 
-                Integer.class, 
+                String.class,
+                Byte.class,
+                Short.class,
+                Integer.class,
                 Long.class,
-                Float.class, 
-                Double.class, 
-                Boolean.class, 
-                BigInteger.class, 
+                Float.class,
+                Double.class,
+                Boolean.class,
+                BigInteger.class,
                 BigDecimal.class));
         return immutableTypes.contains(o.getClass());
       }
