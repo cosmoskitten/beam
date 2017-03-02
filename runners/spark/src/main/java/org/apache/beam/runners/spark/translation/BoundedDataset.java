dc8c3948251018afa3165316f8efb741b74d4640
Jean-Baptiste Onofré <jbonofre@apache.org>
2017-03-02 17:28:50 +0100

    [BEAM-649] Analyse DAG to determine if RDD/DStream has to be cached or not

--- runners/spark/src/main/java/org/apache/beam/runners/spark/translation/BoundedDataset.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/translation/BoundedDataset.java
@@ -100,6 +100,8 @@ public class BoundedDataset<T> implements Dataset {
 
   @Override
   public void cache(String storageLevel) {
+    // populate the rdd if needed
+    getRDD();
     rdd.persist(StorageLevel.fromString(storageLevel));
   }
 
