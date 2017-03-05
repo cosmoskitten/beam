4173a435d6def65b81e8276a753d02974b8a9692
Aviem Zur <aviemzur@gmail.com>
2017-03-05 21:37:05 +0200

    [BEAM-1397] Introduce IO metrics

--- sdks/java/core/src/test/java/org/apache/beam/sdk/metrics/MetricsTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/metrics/MetricsTest.java
@@ -27,8 +27,19 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import javax.annotation.Nullable;
 import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.coders.SerializableCoder;
+import org.apache.beam.sdk.coders.VarIntCoder;
+import org.apache.beam.sdk.io.Read;
+import org.apache.beam.sdk.io.UnboundedSource;
+import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.testing.RunnableOnService;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.testing.UsesAttemptedMetrics;
@@ -39,6 +50,7 @@ import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.TupleTag;
 import org.apache.beam.sdk.values.TupleTagList;
 import org.hamcrest.CoreMatchers;
+import org.joda.time.Instant;
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
@@ -59,6 +71,101 @@ public class MetricsTest implements Serializable {
   @After
   public void tearDown() {
     MetricsEnvironment.setCurrentContainer(null);
+  }
+
+  private static class TestUnboundedSource extends UnboundedSource<Integer, TestCheckpointMark> {
+    private final Integer numElements;
+
+    public TestUnboundedSource(Integer numElements) {
+      this.numElements = numElements;
+    }
+
+    @Override
+    public List<? extends UnboundedSource<Integer, TestCheckpointMark>> generateInitialSplits(int
+        desiredNumSplits, PipelineOptions options) throws Exception {
+      TestUnboundedSource[] splits = new TestUnboundedSource[desiredNumSplits];
+      for (int i = 0; i < splits.length; i++) {
+        splits[i] = new TestUnboundedSource(numElements / desiredNumSplits);
+      }
+      return Arrays.asList(splits);
+    }
+
+    @Override
+    public UnboundedReader<Integer> createReader(PipelineOptions options, @Nullable
+        TestCheckpointMark checkpointMark) throws IOException {
+      return new TestReader(this, numElements);
+    }
+
+    @Nullable
+    @Override
+    public Coder<TestCheckpointMark> getCheckpointMarkCoder() {
+      return SerializableCoder.of(TestCheckpointMark.class);
+    }
+
+    @Override
+    public void validate() {}
+
+    @Override
+    public Coder<Integer> getDefaultOutputCoder() {
+      return VarIntCoder.of();
+    }
+
+    private static class TestReader extends UnboundedReader<Integer> {
+      private final TestUnboundedSource source;
+      private final Integer numElements;
+      private int current;
+
+      public TestReader(TestUnboundedSource source, Integer numElements) {
+        this.source = source;
+        this.numElements = numElements;
+        this.current = 0;
+      }
+
+      @Override
+      public boolean start() throws IOException {
+        return numElements > current;
+      }
+
+      @Override
+      public boolean advance() throws IOException {
+        current++;
+        return start();
+      }
+
+      @Override
+      public Integer getCurrent() throws NoSuchElementException {
+        return current;
+      }
+
+      @Override
+      public Instant getCurrentTimestamp() throws NoSuchElementException {
+        return Instant.now();
+      }
+
+      @Override
+      public void close() throws IOException {}
+
+      @Override
+      public Instant getWatermark() {
+        return Instant.now();
+      }
+
+      @Override
+      public CheckpointMark getCheckpointMark() {
+        return new TestCheckpointMark();
+      }
+
+      @Override
+      public UnboundedSource<Integer, ?> getCurrentSource() {
+        return source;
+      }
+    }
+  }
+
+  private static class TestCheckpointMark implements UnboundedSource.CheckpointMark, Serializable {
+    @Override
+    public void finalizeCheckpoint() throws IOException {
+    }
   }
 
   @Test
@@ -120,7 +227,7 @@ public class MetricsTest implements Serializable {
   @Category({RunnableOnService.class, UsesCommittedMetrics.class})
   @Test
   public void committedMetricsReportToQuery() {
-    PipelineResult result = runPipelineWithMetrics();
+    PipelineResult result = runBoundedPipelineWithMetrics();
 
     MetricQueryResults metrics = result.metrics().queryMetrics(MetricsFilter.builder()
         .addNameFilter(MetricNameFilter.inNamespace(MetricsTest.class))
@@ -142,11 +249,10 @@ public class MetricsTest implements Serializable {
         distributionCommittedMinMax(MetricsTest.class.getName(), "bundle", "MyStep1", 10L, 40L)));
   }
 
-
   @Category({RunnableOnService.class, UsesAttemptedMetrics.class})
   @Test
   public void attemptedMetricsReportToQuery() {
-    PipelineResult result = runPipelineWithMetrics();
+    PipelineResult result = runBoundedPipelineWithMetrics();
 
     MetricQueryResults metrics = result.metrics().queryMetrics(MetricsFilter.builder()
         .addNameFilter(MetricNameFilter.inNamespace(MetricsTest.class))
@@ -169,7 +275,33 @@ public class MetricsTest implements Serializable {
         distributionAttemptedMinMax(MetricsTest.class.getName(), "bundle", "MyStep1", 10L, 40L)));
   }
 
-  private PipelineResult runPipelineWithMetrics() {
+  @Category({RunnableOnService.class, UsesAttemptedMetrics.class})
+  @Test
+  public void unboundedSourceAttemptedMetricsReportToQuery() {
+    PipelineResult result = runUnboundedPipelineWithMetrics();
+
+    MetricQueryResults metrics = result.metrics().queryMetrics(MetricsFilter.builder()
+        .addNameFilter(MetricNameFilter.inNamespace(MetricsTest.class))
+        .build());
+
+    // TODO: BEAM-1169: Metrics shouldn't verify the physical values tightly.
+    assertThat(metrics.counters(), hasItem(
+        attemptedMetricsResult(MetricsTest.class.getName(), "count", "MyStep1", 3L)));
+    assertThat(metrics.distributions(), hasItem(
+        attemptedMetricsResult(MetricsTest.class.getName(), "input", "MyStep1",
+            DistributionResult.create(26L, 3L, 5L, 13L))));
+
+    assertThat(metrics.counters(), hasItem(
+        attemptedMetricsResult(MetricsTest.class.getName(), "count", "MyStep2", 6L)));
+    assertThat(metrics.distributions(), hasItem(
+        attemptedMetricsResult(MetricsTest.class.getName(), "input", "MyStep2",
+            DistributionResult.create(52L, 6L, 5L, 13L))));
+
+    assertThat(metrics.distributions(), hasItem(
+        distributionAttemptedMinMax(MetricsTest.class.getName(), "bundle", "MyStep1", 10L, 40L)));
+  }
+
+  private PipelineResult runBoundedPipelineWithMetrics() {
     final Counter count = Metrics.counter(MetricsTest.class, "count");
     final TupleTag<Integer> output1 = new TupleTag<Integer>(){};
     final TupleTag<Integer> output2 = new TupleTag<Integer>(){};
@@ -214,6 +346,54 @@ public class MetricsTest implements Serializable {
     PipelineResult result = pipeline.run();
 
     result.waitUntilFinish();
+    return result;
+  }
+
+  private PipelineResult runUnboundedPipelineWithMetrics() {
+    final Counter count = Metrics.counter(MetricsTest.class, "count");
+    final TupleTag<Integer> output1 = new TupleTag<Integer>(){};
+    final TupleTag<Integer> output2 = new TupleTag<Integer>(){};
+    pipeline
+        .apply(Read.from(new TestUnboundedSource(10)))
+        .apply("MyStep1", ParDo.of(new DoFn<Integer, Integer>() {
+          Distribution bundleDist = Metrics.distribution(MetricsTest.class, "bundle");
+
+          @StartBundle
+          public void startBundle(Context c) {
+            bundleDist.update(10L);
+          }
+
+          @SuppressWarnings("unused")
+          @ProcessElement
+          public void processElement(ProcessContext c) {
+            Distribution values = Metrics.distribution(MetricsTest.class, "input");
+            count.inc();
+            values.update(c.element());
+
+            c.output(c.element());
+            c.output(c.element());
+          }
+
+          @DoFn.FinishBundle
+          public void finishBundle(Context c) {
+            bundleDist.update(40L);
+          }
+        }))
+        .apply("MyStep2", ParDo.withOutputTags(output1, TupleTagList.of(output2))
+            .of(new DoFn<Integer, Integer>() {
+              @SuppressWarnings("unused")
+              @ProcessElement
+              public void processElement(ProcessContext c) {
+                Distribution values = Metrics.distribution(MetricsTest.class, "input");
+                count.inc();
+                values.update(c.element());
+                c.output(c.element());
+                c.sideOutput(output2, c.element());
+              }
+            }));
+    PipelineResult result = pipeline.run();
+
+   // result.waitUntilFinish(Duration.standardSeconds(5));
     return result;
   }
 }
