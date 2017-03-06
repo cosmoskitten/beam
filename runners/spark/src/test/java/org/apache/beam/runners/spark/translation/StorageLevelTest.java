cd632838a17cf00c947755b0e633dcc0408d6b02
Jean-Baptiste Onofré <jbonofre@apache.org>
2017-03-06 07:35:38 +0100

    [BEAM-649] Back on the original storage level test thanks to fix on master

--- runners/spark/src/test/java/org/apache/beam/runners/spark/translation/StorageLevelTest.java
+++ runners/spark/src/test/java/org/apache/beam/runners/spark/translation/StorageLevelTest.java
@@ -17,14 +17,13 @@
  */
 package org.apache.beam.runners.spark.translation;
 
-import org.apache.beam.runners.spark.SparkPipelineOptions;
-import org.apache.beam.runners.spark.SparkRunner;
+import org.apache.beam.runners.spark.PipelineRule;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.values.PCollection;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -32,12 +31,13 @@ import org.junit.Test;
  */
 public class StorageLevelTest {
 
+  @Rule
+  public final transient PipelineRule pipelineRule = PipelineRule.batch();
+
   @Test
   public void test() throws Exception {
-    SparkPipelineOptions options = PipelineOptionsFactory.create().as(SparkPipelineOptions.class);
-    options.setRunner(SparkRunner.class);
-    options.setStorageLevel("DISK_ONLY");
-    Pipeline pipeline = Pipeline.create(options);
+    pipelineRule.getOptions().setStorageLevel("DISK_ONLY");
+    Pipeline pipeline = pipelineRule.createPipeline();
 
     PCollection<String> pCollection = pipeline.apply(Create.of("foo"));
 
