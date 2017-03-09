1094257a9d8fc940d995716765b0aead05faa91d
Kenneth Knowles <klk@google.com>
2017-03-08 17:02:05 -0800

    Use stable naming strategy for ByteBuddy invokers
    This helps to coalesce related failures across multiple
    JVM instances, over time, across machines, etc.

--- sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/reflect/ByteBuddyOnTimerInvokerFactory.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/transforms/reflect/ByteBuddyOnTimerInvokerFactory.java
@@ -26,7 +26,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.NamingStrategy;
 import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.modifier.FieldManifestation;
 import net.bytebuddy.description.modifier.Visibility;
@@ -150,20 +149,18 @@ class ByteBuddyOnTimerInvokerFactory implements OnTimerInvokerFactory {
 
     final TypeDescription clazzDescription = new TypeDescription.ForLoadedType(fnClass);
 
-    final String className =
-        "auxiliary_OnTimer_" + CharMatcher.JAVA_LETTER_OR_DIGIT.retainFrom(timerId);
+    final String suffix = String.format("%s$%s$%s",
+        OnTimerInvoker.class.getSimpleName(),
+        CharMatcher.javaLetterOrDigit().retainFrom(timerId),
+        timerId.hashCode());
 
     DynamicType.Builder<?> builder =
         new ByteBuddy()
             // Create subclasses inside the target class, to have access to
             // private and package-private bits
-            .with(
-                new NamingStrategy.SuffixingRandom(className) {
-                  @Override
-                  public String subclass(TypeDescription.Generic superClass) {
-                    return super.name(clazzDescription);
-                  }
-                })
+            .with(StableInvokerNamingStrategy.forDoFnClass(fnClass)
+                .withSuffix(suffix))
+
             // class <invoker class> implements OnTimerInvoker {
             .subclass(OnTimerInvoker.class, ConstructorStrategy.Default.NO_CONSTRUCTORS)
 
