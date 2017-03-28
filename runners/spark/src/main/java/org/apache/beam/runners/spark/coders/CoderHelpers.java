8dc41f21fba214cbc1bb6afbd352b45f10565e2e
Amit Sela <amitsela33@gmail.com>
2017-03-28 10:23:32 +0300

    fixup! codestyle.

--- runners/spark/src/main/java/org/apache/beam/runners/spark/coders/CoderHelpers.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/coders/CoderHelpers.java
@@ -37,12 +37,9 @@ import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
-/**
- * Serialization utility class.
- */
+/** Serialization utility class. */
 public final class CoderHelpers {
-  private CoderHelpers() {
-  }
+  private CoderHelpers() {}
 
   /**
    * Utility method for serializing an object using the specified coder.
@@ -66,7 +63,7 @@ public final class CoderHelpers {
    * Utility method for serializing a Iterable of values using the specified coder.
    *
    * @param values Values to serialize.
-   * @param coder  Coder to serialize with.
+   * @param coder Coder to serialize with.
    * @param <T> type of value that is serialized
    * @return List of bytes representing serialized objects.
    */
@@ -82,8 +79,8 @@ public final class CoderHelpers {
    * Utility method for deserializing a byte array using the specified coder.
    *
    * @param serialized bytearray to be deserialized.
-   * @param coder      Coder to deserialize with.
-   * @param <T>        Type of object to be returned.
+   * @param coder Coder to deserialize with.
+   * @param <T> Type of object to be returned.
    * @return Deserialized object.
    */
   public static <T> T fromByteArray(byte[] serialized, Coder<T> coder) {
@@ -99,25 +96,27 @@ public final class CoderHelpers {
    * Utility method for deserializing a Iterable of byte arrays using the specified coder.
    *
    * @param serialized bytearrays to be deserialized.
-   * @param coder      Coder to deserialize with.
-   * @param <T>        Type of object to be returned.
+   * @param coder Coder to deserialize with.
+   * @param <T> Type of object to be returned.
    * @return Iterable of deserialized objects.
    */
   public static <T> Iterable<T> fromByteArrays(
       Collection<byte[]> serialized, final Coder<T> coder) {
-    return Iterables.transform(serialized, new com.google.common.base.Function<byte[], T>() {
-      @Override
-      public T apply(@Nonnull byte[] bytes) {
-        return fromByteArray(checkNotNull(bytes, "Cannot decode null values."), coder);
-      }
-    });
+    return Iterables.transform(
+        serialized,
+        new com.google.common.base.Function<byte[], T>() {
+          @Override
+          public T apply(@Nonnull byte[] bytes) {
+            return fromByteArray(checkNotNull(bytes, "Cannot decode null values."), coder);
+          }
+        });
   }
 
   /**
    * A function wrapper for converting an object to a bytearray.
    *
    * @param coder Coder to serialize with.
-   * @param <T>   The type of the object being serialized.
+   * @param <T> The type of the object being serialized.
    * @return A function that accepts an object and returns its coder-serialized form.
    */
   public static <T> Function<T, byte[]> toByteFunction(final Coder<T> coder) {
@@ -133,7 +132,7 @@ public final class CoderHelpers {
    * A function wrapper for converting a byte array to an object.
    *
    * @param coder Coder to deserialize with.
-   * @param <T>   The type of the object being deserialized.
+   * @param <T> The type of the object being deserialized.
    * @return A function that accepts a byte array and returns its corresponding object.
    */
   public static <T> Function<byte[], T> fromByteFunction(final Coder<T> coder) {
@@ -150,8 +149,8 @@ public final class CoderHelpers {
    *
    * @param keyCoder Coder to serialize keys.
    * @param valueCoder Coder to serialize values.
-   * @param <K>   The type of the key being serialized.
-   * @param <V>   The type of the value being serialized.
+   * @param <K> The type of the key being serialized.
+   * @param <V> The type of the value being serialized.
    * @return A function that accepts a key-value pair and returns a pair of byte arrays.
    */
   public static <K, V> PairFunction<Tuple2<K, V>, ByteArray, byte[]> toByteFunction(
@@ -159,8 +158,8 @@ public final class CoderHelpers {
     return new PairFunction<Tuple2<K, V>, ByteArray, byte[]>() {
       @Override
       public Tuple2<ByteArray, byte[]> call(Tuple2<K, V> kv) {
-        return new Tuple2<>(new ByteArray(toByteArray(kv._1(), keyCoder)), toByteArray(kv._2(),
-            valueCoder));
+        return new Tuple2<>(
+            new ByteArray(toByteArray(kv._1(), keyCoder)), toByteArray(kv._2(), valueCoder));
       }
     };
   }
@@ -170,8 +169,8 @@ public final class CoderHelpers {
    *
    * @param keyCoder Coder to deserialize keys.
    * @param valueCoder Coder to deserialize values.
-   * @param <K>   The type of the key being deserialized.
-   * @param <V>   The type of the value being deserialized.
+   * @param <K> The type of the key being deserialized.
+   * @param <V> The type of the value being deserialized.
    * @return A function that accepts a pair of byte arrays and returns a key-value pair.
    */
   public static <K, V> PairFunction<Tuple2<ByteArray, byte[]>, K, V> fromByteFunction(
@@ -179,27 +178,27 @@ public final class CoderHelpers {
     return new PairFunction<Tuple2<ByteArray, byte[]>, K, V>() {
       @Override
       public Tuple2<K, V> call(Tuple2<ByteArray, byte[]> tuple) {
-        return new Tuple2<>(fromByteArray(tuple._1().getValue(), keyCoder),
-            fromByteArray(tuple._2(), valueCoder));
+        return new Tuple2<>(
+            fromByteArray(tuple._1().getValue(), keyCoder), fromByteArray(tuple._2(), valueCoder));
       }
     };
   }
 
   /**
-   * A function wrapper for converting a byte array pair to a key-value pair, where
-   * values are {@link Iterable}.
+   * A function wrapper for converting a byte array pair to a key-value pair, where values are
+   * {@link Iterable}.
    *
    * @param keyCoder Coder to deserialize keys.
    * @param valueCoder Coder to deserialize values.
-   * @param <K>   The type of the key being deserialized.
-   * @param <V>   The type of the value being deserialized.
+   * @param <K> The type of the key being deserialized.
+   * @param <V> The type of the value being deserialized.
    * @return A function that accepts a pair of byte arrays and returns a key-value pair.
    */
-  public static <K, V> PairFlatMapFunction<Iterator<Tuple2<ByteArray, Iterable<byte[]>>>,
-      K, Iterable<V>> fromByteFunctionIterable(
-          final Coder<K> keyCoder, final Coder<V> valueCoder) {
-    return new PairFlatMapFunction<Iterator<Tuple2<ByteArray, Iterable<byte[]>>>,
-        K, Iterable<V>>() {
+  public static <K, V>
+      PairFlatMapFunction<Iterator<Tuple2<ByteArray, Iterable<byte[]>>>, K, Iterable<V>>
+          fromByteFunctionIterable(final Coder<K> keyCoder, final Coder<V> valueCoder) {
+    return new PairFlatMapFunction<
+        Iterator<Tuple2<ByteArray, Iterable<byte[]>>>, K, Iterable<V>>() {
       @Override
       public Iterable<Tuple2<K, Iterable<V>>> call(
           Iterator<Tuple2<ByteArray, Iterable<byte[]>>> itr) {
@@ -208,21 +207,21 @@ public final class CoderHelpers {
                 itr,
                 new com.google.common.base.Function<
                     Tuple2<ByteArray, Iterable<byte[]>>, Tuple2<K, Iterable<V>>>() {
-                      @Override
-                      public Tuple2<K, Iterable<V>> apply(Tuple2<ByteArray, Iterable<byte[]>> t2) {
-                        K k = fromByteArray(t2._1().getValue(), keyCoder);
-                        return new Tuple2<>(
-                            k,
-                            Iterables.transform(
-                                t2._2(),
-                                new com.google.common.base.Function<byte[], V>() {
-                                  @Override
-                                  public V apply(byte[] bytes) {
-                                    return fromByteArray(bytes, valueCoder);
-                                  }
-                                }));
-                      }
-                    });
+                  @Override
+                  public Tuple2<K, Iterable<V>> apply(Tuple2<ByteArray, Iterable<byte[]>> t2) {
+                    K k = fromByteArray(t2._1().getValue(), keyCoder);
+                    return new Tuple2<>(
+                        k,
+                        Iterables.transform(
+                            t2._2(),
+                            new com.google.common.base.Function<byte[], V>() {
+                              @Override
+                              public V apply(byte[] bytes) {
+                                return fromByteArray(bytes, valueCoder);
+                              }
+                            }));
+                  }
+                });
         return new Iterable<Tuple2<K, Iterable<V>>>() {
           @Override
           public Iterator<Tuple2<K, Iterable<V>>> iterator() {
