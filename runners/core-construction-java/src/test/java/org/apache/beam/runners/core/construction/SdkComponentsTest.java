ca61a6dfe1ac94be823d6e7cb2beddca54c6eaf5
Thomas Groh <tgroh@google.com>
2017-04-07 13:41:29 -0700

    Use SdkComponents in WindowingStrategy.toProto

--- runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/SdkComponentsTest.java
+++ runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/SdkComponentsTest.java
@@ -132,11 +132,13 @@ public class SdkComponentsTest {
   }
 
   @Test
-  public void registerWindowingStrategy() {
+  public void registerWindowingStrategy() throws IOException {
     WindowingStrategy<?, ?> strategy =
         WindowingStrategy.globalDefault().withMode(AccumulationMode.ACCUMULATING_FIRED_PANES);
     String name = components.registerWindowingStrategy(strategy);
     assertThat(name, not(isEmptyOrNullString()));
+
+    components.toComponents().getWindowingStrategiesOrThrow(name);
   }
 
   @Test
