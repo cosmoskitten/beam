b371783932ff78abab1b0c8dcaf4f26f88c36796
Thomas Groh <tgroh@google.com>
2017-04-07 09:17:19 -0700

    Add SdkComponents
    This takes SDK objects and assigns IDs to them. It is effectively a
    ComponentsBuilder context where a component is referred to by the
    Java object which is being translated, rather than by an opaque
    string or protocol buffer.
    
    The Builder for SdkComponents exposes mutation methods to add
    components. The actual SdkComponents implementation is deeply immutable,
    to signal that during actual conversion to/from protos should assume
    that all elements are already recorded as a component.

---
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/util/SdkComponents.java
@@ -0,0 +1,162 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.beam.sdk.util;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import java.io.IOException;
+import java.util.Set;
+import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.common.runner.v1.RunnerApi;
+import org.apache.beam.sdk.common.runner.v1.RunnerApi.Components;
+import org.apache.beam.sdk.transforms.AppliedPTransform;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.PCollection;
+
+/** SDK objects that will be represented at some later point within a {@link Components} object. */
+public class SdkComponents {
+  private final BiMap<AppliedPTransform<?, ?, ?>, String> transformIds;
+  private final BiMap<PCollection<?>, String> pCollectionIds;
+  private final BiMap<WindowingStrategy<?, ?>, String> windowingStrategyIds;
+  private final BiMap<Coder<?>, String> coderIds;
+  // TODO: Specify environments
+
+  public static SdkComponents create() {
+    return new SdkComponents();
+  }
+
+  private SdkComponents() {
+    this.transformIds = HashBiMap.create();
+    this.pCollectionIds = HashBiMap.create();
+    this.windowingStrategyIds = HashBiMap.create();
+    this.coderIds = HashBiMap.create();
+  }
+
+  /**
+   * Puts the provided {@link AppliedPTransform} into this {@link SdkComponents}.
+   *
+   * <p>Uses the full name of the transform as the id.
+   */
+  public String getTransformId(AppliedPTransform<?, ?, ?> pTransform) {
+    String existing = transformIds.get(pTransform);
+    if (existing != null) {
+      return existing;
+    }
+    String name = pTransform.getFullName();
+    if (name.isEmpty()) {
+      name = uniqify("unnamed_ptransform", transformIds.values());
+    }
+    transformIds.put(pTransform, name);
+    return name;
+  }
+
+  /**
+   * Puts the provided {@link PCollection} into this {@link SdkComponents} if it is not already
+   * present, returning the id of the provided {@link PCollection} in this {@link SdkComponents}.
+   *
+   * <p>Uses a unique name based on the result of {@link PCollection#getName()}.
+   */
+  public String getPCollectionId(PCollection<?> pCollection) {
+    String existing = pCollectionIds.get(pCollection);
+    if (existing != null) {
+      return existing;
+    }
+    String uniqueName = uniqify(pCollection.getName(), pCollectionIds.values());
+    pCollectionIds.put(pCollection, uniqueName);
+    return uniqueName;
+  }
+
+  /**
+   * Puts the provided {@link WindowingStrategy} into this {@link SdkComponents} if it is not
+   * already present, returning the id of the {@link WindowingStrategy} in this {@link
+   * SdkComponents}.
+   */
+  public String getWindowingStrategyId(WindowingStrategy<?, ?> windowingStrategy) {
+    String existing = windowingStrategyIds.get(windowingStrategy);
+    if (existing != null) {
+      return existing;
+    }
+    String baseName = NameUtils.approximateSimpleName(windowingStrategy);
+    String name = uniqify(baseName, windowingStrategyIds.values());
+    windowingStrategyIds.put(windowingStrategy, name);
+    return name;
+  }
+
+  /**
+   * Puts the provided {@link Coder} into this {@link SdkComponents} if it is not already present,
+   * returning the id of the {@link Coder} in this {@link SdkComponents}.
+   */
+  public String getCoderId(Coder<?> coder) {
+    String existing = coderIds.get(coder);
+    if (existing != null) {
+      return existing;
+    }
+    String baseName = NameUtils.approximateSimpleName(coder);
+    String name = uniqify(baseName, coderIds.values());
+    coderIds.put(coder, name);
+    return name;
+  }
+
+  private String uniqify(String baseName, Set<String> existing) {
+    String name = baseName;
+    int increment = 1;
+    while (existing.contains(name)) {
+      name = baseName + Integer.toString(increment);
+      increment++;
+    }
+    return name;
+  }
+
+  /**
+   * Convert this {@link SdkComponents} into a {@link RunnerApi.Components}, including all of the
+   * contained {@link Coder coders}, {@link WindowingStrategy windowing strategies}, {@link
+   * PCollection PCollections}, and {@link PTransform PTransforms}.
+   */
+  public RunnerApi.Components toComponents() throws IOException {
+    RunnerApi.Components.Builder componentsBuilder = RunnerApi.Components.newBuilder();
+    // TODO: Implement in Coders
+    //    for (Map.Entry<Coder<?>, String> coderEntry : coderIds.entrySet()) {
+    //      RunnerApi.Coder coderProto = Coders.toProto(coderEntry.getKey(), this);
+    //      componentsBuilder.putCoders(coderEntry.getValue(), coderProto);
+    //    }
+    // TODO: Implement in WindowingStrategies
+    //    for (Map.Entry<WindowingStrategy<?, ?>, String> strategyEntry :
+    //        windowingStrategyIds.entrySet()) {
+    //      RunnerApi.WindowingStrategy strategyProto =
+    //          WindowingStrategies.toProto(strategyEntry.getKey(), this);
+    //      componentsBuilder.putWindowingStrategies(strategyEntry.getValue(), strategyProto);
+    //    }
+    // TODO: implement
+    //    for (Map.Entry<PCollection<?>, String> collectionEntry : collectionIds.entrySet()) {
+    //      RunnerApi.PCollection pCollectionProto =
+    //          PCollections.toProto(collectionEntry.getKey(), this);
+    //      componentsBuilder.putPcollections(collectionEntry.getValue(), pCollectionProto);
+    //    }
+    // TODO: implement
+    //    for (Map.Entry<AppliedPTransform<?, ?, ?>, String> transformEntry
+    //                  : transformIds.entrySet()) {
+    //      RunnerApi.PTransform ptransformProto =
+    //          PTransforms.toProto(transformEntry.getKey(), this);
+    //      componentsBuilder.putTransforms(transformEntry.getValue(), ptransformProto);
+    //    }
+    // TODO: Add environments.
+    //    return componentsBuilder.build();
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+}
