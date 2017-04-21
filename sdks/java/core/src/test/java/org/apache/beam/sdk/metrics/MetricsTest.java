efa1bbd0d8265bf4e8f2fa354f3b336cc53b0b90
Dan Halperin <dhalperi@google.com>
2017-04-21 10:03:50 -0700

    Make it possible to test runners that don't support all metrics
    Runners may only partially support metrics. This partial support runs along axes:
    - attempted & committed
    - counters & distributions & gauges & ...
    
    Prior to this PR, we have categories for the first axis, but not the second.
    This means that a runner that supports only part of the second axis has to blacklist tests
    on the first axis. Adding categories for both axes lets runners build a matrix of supported
    features.
    
    I also renamed the exiting categories so that they are grouped alphabetically. Happy to
    undo if this is not a good change.
    
    This also lets the DataflowRunner run more tests by accurately identifying its test
    matrix.

--- sdks/java/core/src/test/java/org/apache/beam/sdk/metrics/MetricsTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/metrics/MetricsTest.java
@@ -33,6 +33,9 @@ import org.apache.beam.sdk.io.GenerateSequence;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.testing.UsesAttemptedMetrics;
 import org.apache.beam.sdk.testing.UsesCommittedMetrics;
+import org.apache.beam.sdk.testing.UsesCounterMetrics;
+import org.apache.beam.sdk.testing.UsesDistributionMetrics;
+import org.apache.beam.sdk.testing.UsesGaugeMetrics;
 import org.apache.beam.sdk.testing.ValidatesRunner;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
@@ -121,7 +124,8 @@ public class MetricsTest implements Serializable {
     assertThat(cell.getCumulative(), CoreMatchers.equalTo(42L));
   }
 
-  @Category({ValidatesRunner.class, UsesCommittedMetrics.class})
+  @Category({ValidatesRunner.class, UsesCommittedMetrics.class, UsesCounterMetrics.class,
+      UsesDistributionMetrics.class, UsesGaugeMetrics.class})
   @Test
   public void committedMetricsReportToQuery() {
     PipelineResult result = runPipelineWithMetrics();
@@ -150,7 +154,8 @@ public class MetricsTest implements Serializable {
   }
 
 
-  @Category({ValidatesRunner.class, UsesAttemptedMetrics.class})
+  @Category({ValidatesRunner.class, UsesAttemptedMetrics.class, UsesCounterMetrics.class,
+      UsesDistributionMetrics.class, UsesGaugeMetrics.class})
   @Test
   public void attemptedMetricsReportToQuery() {
     PipelineResult result = runPipelineWithMetrics();
@@ -232,7 +237,7 @@ public class MetricsTest implements Serializable {
   }
 
   @Test
-  @Category({ValidatesRunner.class, UsesAttemptedMetrics.class})
+  @Category({ValidatesRunner.class, UsesAttemptedMetrics.class, UsesCounterMetrics.class})
   public void testBoundedSourceMetrics() {
     long numElements = 1000;
 
@@ -253,7 +258,7 @@ public class MetricsTest implements Serializable {
   }
 
   @Test
-  @Category({ValidatesRunner.class, UsesAttemptedMetrics.class})
+  @Category({ValidatesRunner.class, UsesAttemptedMetrics.class, UsesCounterMetrics.class})
   public void testUnboundedSourceMetrics() {
     long numElements = 1000;
 
