aac04a6cd612cb2a9e7380db31d0729d1187dd39
Reuven Lax <relax@google.com>
2017-04-21 19:46:25 -0700

    Remove accidental change

--- sdks/java/core/src/main/java/org/apache/beam/sdk/io/TFRecordIO.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/io/TFRecordIO.java
@@ -345,7 +345,6 @@ public class TFRecordIO {
         throw new IllegalStateException(
             "need to set the filename prefix of a TFRecordIO.Write transform");
      }
-
       org.apache.beam.sdk.io.WriteFiles<byte[]> write =
           org.apache.beam.sdk.io.WriteFiles.to(
               new TFRecordSink(
