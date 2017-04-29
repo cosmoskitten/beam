a3e137080a72df28c3727bd18bf5e29ce8eb820a
Vikas Kedigehalli <vikasrk@google.com>
2017-04-28 22:40:39 -0700

    Staging location as a new directory

--- sdks/java/core/src/main/java/org/apache/beam/sdk/io/FileSystems.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/io/FileSystems.java
@@ -32,6 +32,8 @@ import com.google.common.collect.Multimap;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
 import com.google.common.collect.TreeMultimap;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
@@ -117,6 +119,10 @@ public class FileSystems {
   public static Metadata matchSingleFileSpec(String spec) throws IOException {
     List<MatchResult> matches = FileSystems.match(Collections.singletonList(spec));
     MatchResult matchResult = Iterables.getOnlyElement(matches);
+    if (matchResult.status() == Status.NOT_FOUND) {
+      throw new FileNotFoundException(String.format("File spec %s not found", spec));
+    }
+
     if (matchResult.status() != Status.OK) {
       throw new IOException(
           String.format("Error matching file spec %s: status %s", spec, matchResult.status()));
