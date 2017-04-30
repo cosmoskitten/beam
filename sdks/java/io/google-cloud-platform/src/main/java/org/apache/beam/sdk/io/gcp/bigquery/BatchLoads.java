75cd2628148ae55107fa2f32b475124c4424275a
Reuven Lax <relax@google.com>
2017-04-30 14:08:58 -0700

    Fix tests

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BatchLoads.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BatchLoads.java
@@ -63,19 +63,16 @@ class BatchLoads<DestinationT>
   private BigQueryServices bigQueryServices;
   private final WriteDisposition writeDisposition;
   private final CreateDisposition createDisposition;
-  private final ValueProvider<String> singletonJsonTableRef;
-  private final String singletonTableDescription;
+  private final boolean singletonTable;
   private final DynamicDestinations<?, DestinationT> dynamicDestinations;
 
   BatchLoads(WriteDisposition writeDisposition, CreateDisposition createDisposition,
-             ValueProvider<String> singletonJsonTableRef,
-             String singletonTableDescription,
+             boolean singletonTable,
              DynamicDestinations<?, DestinationT> dynamicDestinations) {
     bigQueryServices = new BigQueryServicesImpl();
     this.writeDisposition = writeDisposition;
     this.createDisposition = createDisposition;
-    this.singletonJsonTableRef  = singletonJsonTableRef;
-    this.singletonTableDescription = singletonTableDescription;
+    this.singletonTable = singletonTable;
     this.dynamicDestinations = dynamicDestinations;
   }
 
@@ -182,8 +179,7 @@ class BatchLoads<DestinationT>
             "WritePartition",
             ParDo.of(
                     new WritePartition<>(
-                        singletonJsonTableRef,
-                        singletonTableDescription,
+                        singletonTable,
                         resultsView,
                         multiPartitionsTag,
                         singlePartitionTag))
