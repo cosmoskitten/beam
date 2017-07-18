778221be43d0b9e55d3d6007e9495b3ea22410cf
James Xu <xumingmingv@gmail.com>
2017-07-18 10:46:04 +0800

    R/recordType -> R/rowType

--- dsls/sql/src/main/java/org/apache/beam/dsls/sql/BeamSqlEnv.java
+++ dsls/sql/src/main/java/org/apache/beam/dsls/sql/BeamSqlEnv.java
@@ -72,7 +72,7 @@ public class BeamSqlEnv implements Serializable{
    *
    */
   public void registerTable(String tableName, BaseBeamTable table) {
-    schema.add(tableName, new BeamCalciteTable(table.getRecordType()));
+    schema.add(tableName, new BeamCalciteTable(table.getRowType()));
     planner.getSourceTables().put(tableName, table);
   }
 
@@ -90,7 +90,7 @@ public class BeamSqlEnv implements Serializable{
     }
     @Override
     public RelDataType getRowType(RelDataTypeFactory typeFactory) {
-      return CalciteUtils.toCalciteRecordType(this.beamSqlRowType)
+      return CalciteUtils.toCalciteRowType(this.beamSqlRowType)
           .apply(BeamQueryPlanner.TYPE_FACTORY);
     }
 
