db148e7a691321a4f9abc44b48eff44c21bfe81c
Kenneth Knowles <klk@google.com>
2017-07-17 22:03:09 -0700

    Uniquify application nodes in TextIOReadTest (HACK)
    These nodes have never had stable unique names, but we have always mutated the
    pipeline before running it so they got lucky. But we are no longer mutating the
    original pipeline, so we need actual unique application names.
    This commit adds a quick hack to fix the test.

--- sdks/java/core/src/test/java/org/apache/beam/sdk/io/TextIOReadTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/io/TextIOReadTest.java
@@ -56,6 +56,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.zip.GZIPOutputStream;
 import java.util.zip.ZipEntry;
@@ -94,6 +95,7 @@ public class TextIOReadTest {
   private static final List<String> TINY =
       Arrays.asList("Irritable eagle", "Optimistic jay", "Fanciful hawk");
   private static final List<String> LARGE = makeLines(1000);
+  private static int uniquifier = 0;
 
   private static Path tempFolder;
   private static File emptyTxt;
@@ -296,15 +298,18 @@ public class TextIOReadTest {
   private void assertReadingCompressedFileMatchesExpected(
       File file, CompressionType compressionType, List<String> expected) {
 
+    int thisUniquifier = ++uniquifier;
+
     TextIO.Read read = TextIO.read().from(file.getPath()).withCompressionType(compressionType);
-    PAssert.that(p.apply("Read_" + file + "_" + compressionType.toString(), read))
+    PAssert.that(
+            p.apply("Read_" + file + "_" + compressionType.toString() + "_" + thisUniquifier, read))
         .containsInAnyOrder(expected);
 
     TextIO.ReadAll readAll =
         TextIO.readAll().withCompressionType(compressionType).withDesiredBundleSizeBytes(10);
     PAssert.that(
-            p.apply("Create_" + file, Create.of(file.getPath()))
-                .apply("Read_" + compressionType.toString(), readAll))
+            p.apply("Create_" + file + "_" + thisUniquifier, Create.of(file.getPath()))
+                .apply("Read_" + compressionType.toString() + "_" + thisUniquifier, readAll))
         .containsInAnyOrder(expected);
     p.run();
   }
