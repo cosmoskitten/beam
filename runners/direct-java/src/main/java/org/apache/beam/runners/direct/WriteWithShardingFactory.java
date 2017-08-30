71be905387d223dad3284eb228e2dd03f089e76e
Reuven Lax <relax@relax-macbookpro2.roam.corp.google.com>
2017-08-30 11:52:33 -0700

    Use helper method

--- runners/direct-java/src/main/java/org/apache/beam/runners/direct/WriteWithShardingFactory.java
+++ runners/direct-java/src/main/java/org/apache/beam/runners/direct/WriteWithShardingFactory.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import org.apache.beam.runners.core.construction.PTransformReplacements;
+import org.apache.beam.runners.core.construction.ReplacementOutputs;
 import org.apache.beam.runners.core.construction.WriteFilesTranslation;
 import org.apache.beam.sdk.io.FileBasedSink;
 import org.apache.beam.sdk.io.WriteFiles;
@@ -89,6 +90,8 @@ class WriteWithShardingFactory<InputT>
       Map<TupleTag<?>, PValue> outputs, WriteFilesResult newOutput) {
     // We must connect the new output from WriteFilesResult to the outputs provided by the original
     // transform.
+    return ReplacementOutputs.tagged(outputs, newOutput);
+    /*
     Map.Entry<TupleTag<?>, PValue> original = Iterables.getOnlyElement(outputs.entrySet());
     Map.Entry<TupleTag<?>, PValue> replacement =
         Iterables.getOnlyElement(newOutput.expand().entrySet());
@@ -97,6 +100,7 @@ class WriteWithShardingFactory<InputT>
         ReplacementOutput.of(
             TaggedPValue.of(original.getKey(), original.getValue()),
             TaggedPValue.of(replacement.getKey(), replacement.getValue())));
+            */
   }
 
   private static class LogElementShardsWithDrift<T>
