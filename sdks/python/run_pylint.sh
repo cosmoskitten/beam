7c7c315fc2bdb27669ad4e15e15a345496fc4455
Holden Karau <holden@us.ibm.com>
2017-09-01 21:38:47 -0700

    Fix remaining style issues from auto conversion, switch ref in pylint to stage 1 (stages in futurize are apparently 1 indexed)

--- sdks/python/run_pylint.sh
+++ sdks/python/run_pylint.sh
@@ -82,12 +82,12 @@ done
 pushd $MODULE
 isort -p apache_beam -w 120 -y -c -ot -cs -sl ${SKIP_PARAM}
 popd
-echo "Checking for files requiring stage 0 refactoring from futurize"
+echo "Checking for files requiring stage 1 refactoring from futurize"
 futurize_results=$(futurize --stage1 apache_beam 2>&1 |grep Refactored)
 futurize_filtered=$(echo $futurize_results |grep -v pb2 |grep -v typehints.py)
 count=$(echo $futurize_results |wc -c)
 if [ $count == 1 ]; then
-  echo "Some of the changes require futurize stage 0 changes."
+  echo "Some of the changes require futurize stage 1 changes."
   echo $futurize_results
   exit 1
 fi
