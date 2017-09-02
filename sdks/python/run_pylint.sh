2ffee69fd31a1f08c90a7224c0297a4c3c73aec6
Holden Karau <holden@us.ibm.com>
2017-09-01 21:53:51 -0700

    Fix the pylint script checking futurize

--- sdks/python/run_pylint.sh
+++ sdks/python/run_pylint.sh
@@ -68,6 +68,7 @@ ISORT_EXCLUDED=(
   "apiclient.py"
   "avroio_test.py"
   "datastore_wordcount.py"
+  "datastoreio_test.py"
   "iobase_test.py"
   "fast_coders_test.py"
   "slow_coders_test.py"
@@ -85,8 +86,8 @@ popd
 echo "Checking for files requiring stage 1 refactoring from futurize"
 futurize_results=$(futurize --stage1 apache_beam 2>&1 |grep Refactored)
 futurize_filtered=$(echo $futurize_results |grep -v pb2 |grep -v typehints.py)
-count=$(echo $futurize_results |wc -c)
-if [ $count == 1 ]; then
+count=$(echo $futurize_filtered |wc -c)
+if [ "$count" != "1" ]; then
   echo "Some of the changes require futurize stage 1 changes."
   echo $futurize_results
   exit 1
