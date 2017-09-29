8abd195bd8f1420dc31ec4eca3c59d61eedd65e7
Kenneth Knowles <klk@google.com>
2017-09-29 10:48:11 -0700

    Add model/fn-execution to Maven structure and gen_protos.py

--- sdks/python/gen_protos.py
+++ sdks/python/gen_protos.py
@@ -34,6 +34,7 @@ GRPC_TOOLS = 'grpcio-tools>=1.3.5'
 BEAM_PROTO_PATHS = [
   os.path.join('..', '..', 'model', 'pipeline', 'src', 'main', 'proto'),
   os.path.join('..', '..', 'model', 'job-management', 'src', 'main', 'proto'),
+  os.path.join('..', '..', 'model', 'fn-execution', 'src', 'main', 'proto'),
   os.path.join('..', 'common', 'fn-api', 'src', 'main', 'proto')
 ]
 
