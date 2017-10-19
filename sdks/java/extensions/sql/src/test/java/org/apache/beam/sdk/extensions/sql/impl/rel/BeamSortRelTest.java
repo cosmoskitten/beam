d479b3e43c5bbc6fe6dec69a9753e510e94e3fb0
zangshayang1 <zangshayang1@hotmail.com>
2017-10-18 18:08:45 -0700

    This is to support Timestamp datatype in BeamRecordSqlType.java by introducing TimestampCoder. Previously Timestamp datatype is mapped to Date type.

--- sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/impl/rel/BeamSortRelTest.java
+++ sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/impl/rel/BeamSortRelTest.java
@@ -18,6 +18,7 @@
 
 package org.apache.beam.sdk.extensions.sql.impl.rel;
 
+import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.Date;
 import org.apache.beam.sdk.extensions.sql.TestUtils;
@@ -49,16 +50,16 @@ public class BeamSortRelTest extends BaseRelTest {
             Types.DOUBLE, "price",
             Types.TIMESTAMP, "order_time"
         ).addRows(
-            1L, 2, 1.0, new Date(0),
-            1L, 1, 2.0, new Date(1),
-            2L, 4, 3.0, new Date(2),
-            2L, 1, 4.0, new Date(3),
-            5L, 5, 5.0, new Date(4),
-            6L, 6, 6.0, new Date(5),
-            7L, 7, 7.0, new Date(6),
-            8L, 8888, 8.0, new Date(7),
-            8L, 999, 9.0, new Date(8),
-            10L, 100, 10.0, new Date(9)
+            1L, 2, 1.0, new Timestamp(0),
+            1L, 1, 2.0, new Timestamp(1),
+            2L, 4, 3.0, new Timestamp(2),
+            2L, 1, 4.0, new Timestamp(3),
+            5L, 5, 5.0, new Timestamp(4),
+            6L, 6, 6.0, new Timestamp(5),
+            7L, 7, 7.0, new Timestamp(6),
+            8L, 8888, 8.0, new Timestamp(7),
+            8L, 999, 9.0, new Timestamp(8),
+            10L, 100, 10.0, new Timestamp(9)
         )
     );
     sqlEnv.registerTable("SUB_ORDER_RAM",
@@ -104,10 +105,10 @@ public class BeamSortRelTest extends BaseRelTest {
         Types.DOUBLE, "price",
         Types.TIMESTAMP, "order_time"
     ).addRows(
-        7L, 7, 7.0, new Date(6),
-        8L, 8888, 8.0, new Date(7),
-        8L, 999, 9.0, new Date(8),
-        10L, 100, 10.0, new Date(9)
+        7L, 7, 7.0, new Timestamp(6),
+        8L, 8888, 8.0, new Timestamp(7),
+        8L, 999, 9.0, new Timestamp(8),
+        10L, 100, 10.0, new Timestamp(9)
     ).getRows());
     pipeline.run().waitUntilFinish();
   }
