444b639a631bb9cb8fede3065dcffc2bb91b2629
Udi Meiri <ehudm@google.com>
2017-10-25 15:45:39 -0700

    Add IdentityWindowFn.
    BEAM-1872 IdentityWindowFn is intended for internal use in a future
    implementation of Reshuffle.
    
    Add and pass current window to WindowFn.AssignContext, for
    IdentityWindowFn implementation.
    
    Add WindowedValueMatcher in testing/util.py, for use in IdentityWindowFn
    unit tests.

--- sdks/python/apache_beam/testing/util_test.py
+++ sdks/python/apache_beam/testing/util_test.py
@@ -21,9 +21,17 @@ import unittest
 
 from apache_beam import Create
 from apache_beam.testing.test_pipeline import TestPipeline
+from apache_beam.testing.util import BeamAssertException
+from apache_beam.testing.util import WindowedValueMatcher
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 from apache_beam.testing.util import is_empty
+from apache_beam.transforms.core import DoFn
+from apache_beam.transforms.core import ParDo
+from apache_beam.transforms.core import WindowInto
+from apache_beam.transforms.window import IntervalWindow
+from apache_beam.transforms.window import Sessions
+from apache_beam.transforms.window import TimestampedValue
 
 
 class UtilTest(unittest.TestCase):
@@ -46,6 +54,73 @@ class UtilTest(unittest.TestCase):
     with self.assertRaises(Exception):
       with TestPipeline() as p:
         assert_that(p | Create([1, 2, 3]), is_empty())
+
+  def test_windowed_value_matcher_pass(self):
+    class AddTimestampDoFn(DoFn):
+      def process(self, element):
+        yield TimestampedValue(element, element[1])
+
+    with TestPipeline() as p:
+      data = [(1, 1), (2, 1), (3, 1), (1, 2), (2, 2), (1, 4)]
+      expected_window_values = [
+          ((1, 1), 1.0, IntervalWindow(1.0, 3.0)),
+          ((2, 1), 1.0, IntervalWindow(1.0, 3.0)),
+          ((3, 1), 1.0, IntervalWindow(1.0, 3.0)),
+          ((1, 2), 2.0, IntervalWindow(2.0, 4.0)),
+          ((2, 2), 2.0, IntervalWindow(2.0, 4.0)),
+          ((1, 4), 4.0, IntervalWindow(4.0, 6.0))]
+      _ = (p
+           | 'start' >> Create(data)
+           | 'add_timestamps' >> ParDo(AddTimestampDoFn())
+           | 'window' >> WindowInto(Sessions(gap_size=2))
+           | 'assert_windowed_values' >> WindowedValueMatcher(
+               expected_window_values))
+
+  def test_windowed_value_matcher_unexpected_element(self):
+    class AddTimestampDoFn(DoFn):
+      def process(self, element):
+        yield TimestampedValue(element, element[1])
+
+    with self.assertRaisesRegexp(BeamAssertException, r'Unexpected.*(1, 4)'):
+      with TestPipeline() as p:
+        data = [(1, 1), (2, 1), (3, 1), (1, 2), (2, 2), (1, 4)]
+        # This list is missing element (1, 4).
+        expected_window_values = [
+            ((1, 1), 1.0, IntervalWindow(1.0, 3.0)),
+            ((2, 1), 1.0, IntervalWindow(1.0, 3.0)),
+            ((3, 1), 1.0, IntervalWindow(1.0, 3.0)),
+            ((1, 2), 2.0, IntervalWindow(2.0, 4.0)),
+            ((2, 2), 2.0, IntervalWindow(2.0, 4.0))]
+        _ = (p
+             | 'start' >> Create(data)
+             | 'add_timestamps' >> ParDo(AddTimestampDoFn())
+             | 'window' >> WindowInto(Sessions(gap_size=2))
+             | 'assert_windowed_values' >> WindowedValueMatcher(
+                 expected_window_values))
+
+  def test_windowed_value_matcher_extra_element(self):
+    class AddTimestampDoFn(DoFn):
+      def process(self, element):
+        yield TimestampedValue(element, element[1])
+
+    with self.assertRaisesRegexp(BeamAssertException,
+                                 r'Failed assert.*\[6\] == \[5\]'):
+      with TestPipeline() as p:
+        # This list is missing element (1, 4).
+        data = [(1, 1), (2, 1), (3, 1), (1, 2), (2, 2)]
+        expected_window_values = [
+            ((1, 1), 1.0, IntervalWindow(1.0, 3.0)),
+            ((2, 1), 1.0, IntervalWindow(1.0, 3.0)),
+            ((3, 1), 1.0, IntervalWindow(1.0, 3.0)),
+            ((1, 2), 2.0, IntervalWindow(2.0, 4.0)),
+            ((2, 2), 2.0, IntervalWindow(2.0, 4.0)),
+            ((1, 4), 4.0, IntervalWindow(4.0, 6.0))]
+        _ = (p
+             | 'start' >> Create(data)
+             | 'add_timestamps' >> ParDo(AddTimestampDoFn())
+             | 'window' >> WindowInto(Sessions(gap_size=2))
+             | 'assert_windowed_values' >> WindowedValueMatcher(
+                 expected_window_values))
 
 
 if __name__ == '__main__':
