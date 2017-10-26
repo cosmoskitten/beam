f623b595d16dac2e28705c6c3e08f15101dd1f0c
Luke Cwik <lcwik@google.com>
2017-10-26 12:41:50 -0700

    fixup

--- .test-infra/jenkins/common_job_properties.groovy
+++ .test-infra/jenkins/common_job_properties.groovy
@@ -163,7 +163,7 @@ class common_job_properties {
 
           // Use a github trigger extension which cancels builds when an
           // update to the PR makes a prior run irrelevant.
-          ghprbCancelBuildsOnUpdate {
+          cancelBuildsOnUpdate {
             overrideGlobal(true)
           }
         }
