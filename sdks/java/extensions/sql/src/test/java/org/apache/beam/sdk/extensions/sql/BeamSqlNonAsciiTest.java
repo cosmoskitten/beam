4c0d7f495fbce141a15bff3a12ce5f97aec27976
琨瑜 <yiyan.lyy@alibaba-inc.com>
2017-12-14 11:53:48 +0800

    Set utf-16 as SQL DSL default charset

---
+++ sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/BeamSqlNonAsciiTest.java
@@ -0,0 +1,24 @@
+package org.apache.beam.sdk.extensions.sql;
+
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.values.*;
+import org.junit.Test;
+
+/**
+ * Tests for non ascii char in sql.
+ */
+public class BeamSqlNonAsciiTest extends BeamSqlDslBase{
+
+    @Test
+    public void testNonAsciiLiteral() {
+        String sql = "SELECT * FROM TABLE_A WHERE f_string = '第四行'";
+
+        PCollection<BeamRecord> result =
+                PCollectionTuple.of(new TupleTag<BeamRecord>("TABLE_A"), boundedInput1)
+                        .apply("testCompositeFilter", BeamSql.queryMulti(sql));
+
+        PAssert.that(result).containsInAnyOrder(recordsInTableA.get(3));
+
+        pipeline.run().waitUntilFinish();
+    }
+}
