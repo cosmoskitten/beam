a208f768a2762297d68484fca4938924cdd9fdd7
Huygaa Batsaikhan <batbat@batbat-linuxworkstation.sea.corp.google.com>
2017-12-15 12:30:21 -0800

    Added an example pipeline that uses stateful processing to output team score every time it passes a new multiple of a threshold.

---
+++ examples/java8/src/main/java/org/apache/beam/examples/complete/game/utils/GameConstants.java
@@ -0,0 +1,18 @@
+package org.apache.beam.examples.complete.game.utils;
+
+import java.util.TimeZone;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+/**
+ * Shared constants between game series classes.
+ */
+public class GameConstants {
+
+  public static final String TIMESTAMP_ATTRIBUTE = "timestamp_ms";
+
+  public static DateTimeFormatter fmt =
+      DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS")
+          .withZone(DateTimeZone.forTimeZone(TimeZone.getTimeZone("PST")));
+}
