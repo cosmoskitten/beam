4263a98e891d116871e1d0d2b30bab6d44f94844
Maria Garcia Herrero <mariagh@google.com>
2017-12-21 00:13:38 -0800

    Add review comments

--- sdks/python/apache_beam/transforms/trigger.py
+++ sdks/python/apache_beam/transforms/trigger.py
@@ -378,8 +378,8 @@ class AfterCount(TriggerFn):
   COUNT_TAG = _CombiningValueStateTag('count', combiners.CountCombineFn())
 
   def __init__(self, count):
-    if count == 0:
-      raise ValueError("count (%s) must be a non-zero natural number." % count)
+    if not isinstance(count, int) or count < 1:
+      raise ValueError("count (%d) must be a non-zero natural number." % count)
     self.count = count
 
   def __repr__(self):
