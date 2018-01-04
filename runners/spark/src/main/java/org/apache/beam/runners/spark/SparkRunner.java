74c54ff92fd95bf7799a794cf82054fe89289d6c
Etienne Chauchot <echauchot@apache.org>
2018-01-04 17:05:12 +0100

    Stop MetricsPusher thread by observing pipeline state and improve the thread safety of MetricsPusher

--- runners/spark/src/main/java/org/apache/beam/runners/spark/SparkRunner.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/SparkRunner.java
@@ -239,6 +239,7 @@ public final class SparkRunner extends PipelineRunner<SparkPipelineResult> {
       registerMetricsSource(mOptions.getAppName());
     }
 
+    MetricsPusher.getInstance().setPipelineResult(result);
     return result;
   }
 
