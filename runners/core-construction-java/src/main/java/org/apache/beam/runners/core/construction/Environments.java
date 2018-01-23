ad783d924e0cb4fa428fe30d839f0dc78c661754
Thomas Groh <tgroh@google.com>
2018-01-22 17:08:58 -0800

    Retrieve Environments from PTransforms
    PTransforms associated with an environment (perhaps because they contain
    a UDF) must be able to vend that environment in order to run the
    environment-specific code.

--- runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/Environments.java
+++ runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/Environments.java
@@ -18,12 +18,41 @@
 
 package org.apache.beam.runners.core.construction;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.protobuf.ByteString;
+import java.io.IOException;
+import java.util.Map;
+import javax.annotation.Nullable;
+import org.apache.beam.model.pipeline.v1.RunnerApi.CombinePayload;
 import org.apache.beam.model.pipeline.v1.RunnerApi.Environment;
+import org.apache.beam.model.pipeline.v1.RunnerApi.PTransform;
+import org.apache.beam.model.pipeline.v1.RunnerApi.ParDoPayload;
+import org.apache.beam.model.pipeline.v1.RunnerApi.ReadPayload;
+import org.apache.beam.model.pipeline.v1.RunnerApi.SdkFunctionSpec;
+import org.apache.beam.model.pipeline.v1.RunnerApi.WindowIntoPayload;
 import org.apache.beam.sdk.util.ReleaseInfo;
 
-/** Utilities for interacting with {@link Environment Environments}. */
+/**
+ * Utilities for interacting with portability {@link Environment environments}.
+ */
 public class Environments {
-  // TODO: Make this a real URL
+  private static final Map<String, SdkFunctionSpecExtractor> KNOWN_URN_SPEC_EXTRACTORS =
+      ImmutableMap.<String, SdkFunctionSpecExtractor>builder()
+          .put(
+              PTransformTranslation.COMBINE_TRANSFORM_URN,
+              (transformSpec) -> CombinePayload.parseFrom(transformSpec).getCombineFn())
+          .put(
+              PTransformTranslation.PAR_DO_TRANSFORM_URN,
+              (transformSpec) -> ParDoPayload.parseFrom(transformSpec).getDoFn())
+          .put(
+              PTransformTranslation.READ_TRANSFORM_URN,
+              (transformSpec) -> ReadPayload.parseFrom(transformSpec).getSource())
+          .put(
+              PTransformTranslation.WINDOW_TRANSFORM_URN,
+              (transformPayload -> WindowIntoPayload.parseFrom(transformPayload).getWindowFn()))
+          .build();
+  private static final SdkFunctionSpecExtractor DEFAULT_SPEC_EXTRACTOR = transformPayload -> null;
+
   private static final String JAVA_SDK_HARNESS_CONTAINER_URL =
       String.format(
           "%s-%s",
@@ -32,4 +61,27 @@ public class Environments {
       Environment.newBuilder().setUrl(JAVA_SDK_HARNESS_CONTAINER_URL).build();
 
   private Environments() {}
+
+  @Nullable
+  public static Environment getEnvironment(
+      PTransform ptransform, RehydratedComponents components) throws IOException {
+    SdkFunctionSpecExtractor specExtractor =
+        KNOWN_URN_SPEC_EXTRACTORS.getOrDefault(
+            ptransform.getSpec().getUrn(), DEFAULT_SPEC_EXTRACTOR);
+    SdkFunctionSpec fnSpec = specExtractor.getSdkFunctionSpec(ptransform.getSpec().getPayload());
+    if (fnSpec != null) {
+      return components.getEnvironment(fnSpec.getEnvironmentId());
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * Extracts the {@link SdkFunctionSpec} for a {@link PTransform} payload, or {@code null} if there
+   * is no user fn within the payload.
+   */
+  private interface SdkFunctionSpecExtractor {
+    @Nullable
+    SdkFunctionSpec getSdkFunctionSpec(ByteString transformPayload) throws IOException;
+  }
 }
