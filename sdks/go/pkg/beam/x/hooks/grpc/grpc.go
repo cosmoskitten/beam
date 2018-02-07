364807e6bf0dc7bf2a1e4b58159232a946ff69be
Bill Neubauer <wcn@google.com>
2018-02-07 11:24:34 -0800

    Extending the registration concept
    Introducing phase hooks to allow hooks to be defined at init, request,
    and response handling time. This allows the system to be closed to the
    nature of the hooks themselves, by just having a contract that hooks
    will be run at key points in the harness lifecycle.

---
+++ sdks/go/pkg/beam/x/hooks/grpc/grpc.go
@@ -0,0 +1,83 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package grpc provides a hook for runners to tailor the behavior of the gRPC
+// link to the runner harness.
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/apache/beam/sdks/go/pkg/beam/core/runtime/harness"
+	"github.com/apache/beam/sdks/go/pkg/beam/util/grpcx"
+	"google.golang.org/grpc"
+)
+
+// Hook allow a runner to customize various aspects of gRPC
+// communication with the FnAPI harness. Each member of the struct
+// is optional; the default behavior will be used if a value is not
+// supplied.
+type Hook struct {
+	// Dialer allows the runner to customize the gRPC dialing behavior.
+	Dialer func(context.Context, string, time.Duration) (*grpc.ClientConn, error)
+	// TODO(wcn): expose other hooks here.
+}
+
+var hookRegistry = make(map[string]Hook)
+var enabledGrpcHook string
+
+// RegisterHook registers a Hook for the
+// supplied identifier. It panics if the same identifier is
+// registered twice.
+func RegisterHook(name string, c Hook) {
+	if _, exists := hookRegistry[name]; exists {
+		panic(fmt.Sprintf("grpc.Hook: %s registered twice", name))
+	}
+	hookRegistry[name] = c
+
+	hook := harness.Hook{
+		Init: func(_ context.Context) error {
+			grpcHook := hookRegistry[enabledGrpcHook]
+			if grpcHook.Dialer != nil {
+				grpcx.Dial = grpcHook.Dialer
+			}
+			return nil
+		},
+		Serialize: func() []string {
+			return []string{enabledGrpcHook}
+		},
+		Deserialize: func(opts ...string) {
+			enabledGrpcHook = opts[0]
+		},
+	}
+	harness.RegisterHook("grpc", hook)
+}
+
+// EnableHook is called to request the use of the gRPC
+// hook in a pipeline.
+func EnableHook(name string) {
+	_, exists := hookRegistry[name]
+	if !exists {
+		panic(fmt.Sprintf("EnableHook: %s not registered", name))
+	}
+	// Only one hook can be enabled. If the pipeline has two conflicting views about how to use gRPC
+	// that won't end well.
+	if enabledGrpcHook != "" && enabledGrpcHook != name {
+		panic(fmt.Sprintf("EnableHook: can't enable hook %s, hook %s already enabled", name, enabledGrpcHook))
+	}
+	enabledGrpcHook = name
+}
