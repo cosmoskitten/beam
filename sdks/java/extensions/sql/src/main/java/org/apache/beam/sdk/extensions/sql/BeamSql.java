fdf1b0658dc2b4d789526bfa41e765dc89b26723
Colm O hEigeartaigh <coheigea@apache.org>
2018-02-08 15:29:27 +0000

    Make sure there is a space between closing round bracket and opening curly bracket

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/BeamSql.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/BeamSql.java
@@ -90,4 +90,156 @@ public class BeamSql {
   public static QueryTransform query(String sqlQuery) {
     return QueryTransform.withQueryString(sqlQuery);
   }
+
+  /**
+   * Transforms a SQL query into a {@link PTransform} representing an equivalent execution plan.
+   *
+   * <p>This is a simplified form of {@link #queryMulti(String)} where the query must reference
+   * a single input table.
+   *
+   * <p>Make sure to query it from a static table name <em>PCOLLECTION</em>.
+   */
+  public static SimpleQueryTransform query(String sqlQuery) {
+    return new SimpleQueryTransform(sqlQuery);
+  }
+
+  /**
+   * A {@link PTransform} representing an execution plan for a SQL query.
+   *
+   * <p>The table names in the input {@code PCollectionTuple} are only valid during the current
+   * query.
+   */
+  public static class QueryTransform extends
+      PTransform<PCollectionTuple, PCollection<Row>> {
+    private BeamSqlEnv beamSqlEnv = new BeamSqlEnv();
+    private String sqlQuery;
+
+    public QueryTransform(String sqlQuery) {
+      this.sqlQuery = sqlQuery;
+    }
+
+    /**
+     * register a UDF function used in this query.
+     *
+     * <p>Refer to {@link BeamSqlUdf} for more about how to implement a UDF in BeamSql.
+     */
+     public QueryTransform withUdf(String functionName, Class<? extends BeamSqlUdf> clazz) {
+       beamSqlEnv.registerUdf(functionName, clazz);
+       return this;
+     }
+     /**
+      * register {@link SerializableFunction} as a UDF function used in this query.
+      * Note, {@link SerializableFunction} must have a constructor without arguments.
+      */
+      public QueryTransform withUdf(String functionName, SerializableFunction sfn) {
+        beamSqlEnv.registerUdf(functionName, sfn);
+        return this;
+      }
+
+     /**
+      * register a {@link CombineFn} as UDAF function used in this query.
+      */
+     public QueryTransform withUdaf(String functionName, CombineFn combineFn) {
+       beamSqlEnv.registerUdaf(functionName, combineFn);
+       return this;
+     }
+
+    @Override
+    public PCollection<Row> expand(PCollectionTuple input) {
+      registerTables(input);
+
+      BeamRelNode beamRelNode = null;
+      try {
+        beamRelNode = beamSqlEnv.getPlanner().convertToBeamRel(sqlQuery);
+      } catch (ValidationException | RelConversionException | SqlParseException e) {
+        throw new IllegalStateException(e);
+      }
+
+      try {
+        return beamRelNode.buildBeamPipeline(input, beamSqlEnv);
+      } catch (Exception e) {
+        throw new IllegalStateException(e);
+      }
+    }
+
+    //register tables, related with input PCollections.
+    private void registerTables(PCollectionTuple input) {
+      for (TupleTag<?> sourceTag : input.getAll().keySet()) {
+        PCollection<Row> sourceStream = (PCollection<Row>) input.get(sourceTag);
+        RowCoder sourceCoder = (RowCoder) sourceStream.getCoder();
+
+        beamSqlEnv.registerTable(sourceTag.getId(),
+            new BeamPCollectionTable(sourceStream, sourceCoder.getRowType()));
+      }
+    }
+  }
+
+  /**
+   * A {@link PTransform} representing an execution plan for a SQL query referencing
+   * a single table.
+   */
+  public static class SimpleQueryTransform
+      extends PTransform<PCollection<Row>, PCollection<Row>> {
+    private static final String PCOLLECTION_TABLE_NAME = "PCOLLECTION";
+    private QueryTransform delegate;
+
+    public SimpleQueryTransform(String sqlQuery) {
+      this.delegate = new QueryTransform(sqlQuery);
+    }
+
+    /**
+     * register a UDF function used in this query.
+     *
+     * <p>Refer to {@link BeamSqlUdf} for more about how to implement a UDAF in BeamSql.
+     */
+    public SimpleQueryTransform withUdf(String functionName, Class<? extends BeamSqlUdf> clazz) {
+      delegate.withUdf(functionName, clazz);
+      return this;
+    }
+
+    /**
+     * register {@link SerializableFunction} as a UDF function used in this query.
+     * Note, {@link SerializableFunction} must have a constructor without arguments.
+     */
+    public SimpleQueryTransform withUdf(String functionName, SerializableFunction sfn) {
+      delegate.withUdf(functionName, sfn);
+      return this;
+    }
+
+    /**
+     * register a {@link CombineFn} as UDAF function used in this query.
+     */
+    public SimpleQueryTransform withUdaf(String functionName, CombineFn combineFn) {
+      delegate.withUdaf(functionName, combineFn);
+      return this;
+    }
+
+    private void validateQuery() {
+      SqlNode sqlNode;
+      try {
+        sqlNode = delegate.beamSqlEnv.getPlanner().parseQuery(delegate.sqlQuery);
+        delegate.beamSqlEnv.getPlanner().getPlanner().close();
+      } catch (SqlParseException e) {
+        throw new IllegalStateException(e);
+      }
+
+      if (sqlNode instanceof SqlSelect) {
+        SqlSelect select = (SqlSelect) sqlNode;
+        String tableName = select.getFrom().toString();
+        if (!tableName.equalsIgnoreCase(PCOLLECTION_TABLE_NAME)) {
+          throw new IllegalStateException("Use fixed table name " + PCOLLECTION_TABLE_NAME);
+        }
+      } else {
+        throw new UnsupportedOperationException(
+            "Sql operation: " + sqlNode.toString() + " is not supported!");
+      }
+    }
+
+    @Override
+    public PCollection<Row> expand(PCollection<Row> input) {
+      validateQuery();
+      return PCollectionTuple.of(new TupleTag<>(PCOLLECTION_TABLE_NAME), input).apply(delegate);
+    }
+>>>>>>> Make sure there is a space between closing round bracket and opening curly bracket
+  }
 }
