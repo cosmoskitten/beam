3719c4f686f231a7c89bd2ef71839fa28197aeac
Eugene Kirpichov <kirpichov@google.com>
2018-02-12 12:49:17 -0800

    Moves standard URNs into .proto files

--- runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/ModelCoderRegistrar.java
+++ runners/core-construction-java/src/main/java/org/apache/beam/runners/core/construction/ModelCoderRegistrar.java
@@ -18,7 +18,15 @@
 
 package org.apache.beam.runners.core.construction;
 
-import static org.apache.beam.runners.core.construction.UrnUtils.validateCommonUrn;
+import static org.apache.beam.model.pipeline.v1.RunnerApi.StandardCoderUrns.Enum.BYTES;
+import static org.apache.beam.model.pipeline.v1.RunnerApi.StandardCoderUrns.Enum.GLOBAL_WINDOW;
+import static org.apache.beam.model.pipeline.v1.RunnerApi.StandardCoderUrns.Enum.INTERVAL_WINDOW;
+import static org.apache.beam.model.pipeline.v1.RunnerApi.StandardCoderUrns.Enum.ITERABLE;
+import static org.apache.beam.model.pipeline.v1.RunnerApi.StandardCoderUrns.Enum.KV;
+import static org.apache.beam.model.pipeline.v1.RunnerApi.StandardCoderUrns.Enum.LENGTH_PREFIX;
+import static org.apache.beam.model.pipeline.v1.RunnerApi.StandardCoderUrns.Enum.VARINT;
+import static org.apache.beam.model.pipeline.v1.RunnerApi.StandardCoderUrns.Enum.WINDOWED_VALUE;
+import static org.apache.beam.runners.core.construction.StandardUrns.getStandardUrn;
 
 import com.google.auto.service.AutoService;
 import com.google.common.annotations.VisibleForTesting;
@@ -26,6 +34,7 @@ import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableMap;
 import java.util.Map;
+import java.util.Set;
 import org.apache.beam.sdk.coders.ByteArrayCoder;
 import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.coders.IterableCoder;
@@ -39,26 +48,26 @@ import org.apache.beam.sdk.util.WindowedValue.FullWindowedValueCoder;
 /** The {@link CoderTranslatorRegistrar} for coders which are shared across languages. */
 @AutoService(CoderTranslatorRegistrar.class)
 public class ModelCoderRegistrar implements CoderTranslatorRegistrar {
+
   // The URNs for coders which are shared across languages
   @VisibleForTesting
   static final BiMap<Class<? extends Coder>, String> BEAM_MODEL_CODER_URNS =
       ImmutableBiMap.<Class<? extends Coder>, String>builder()
-          .put(ByteArrayCoder.class, validateCommonUrn("beam:coder:bytes:v1"))
-          .put(KvCoder.class, validateCommonUrn("beam:coder:kv:v1"))
-          .put(VarLongCoder.class, validateCommonUrn("beam:coder:varint:v1"))
-          .put(IntervalWindowCoder.class, validateCommonUrn("beam:coder:interval_window:v1"))
-          .put(IterableCoder.class, validateCommonUrn("beam:coder:iterable:v1"))
-          .put(LengthPrefixCoder.class, validateCommonUrn("beam:coder:length_prefix:v1"))
-          .put(GlobalWindow.Coder.class, validateCommonUrn("beam:coder:global_window:v1"))
-          .put(FullWindowedValueCoder.class, validateCommonUrn("beam:coder:windowed_value:v1"))
+          .put(ByteArrayCoder.class, getStandardUrn(BYTES))
+          .put(KvCoder.class, getStandardUrn(KV))
+          .put(VarLongCoder.class, getStandardUrn(VARINT))
+          .put(IntervalWindowCoder.class, getStandardUrn(INTERVAL_WINDOW))
+          .put(IterableCoder.class, getStandardUrn(ITERABLE))
+          .put(LengthPrefixCoder.class, getStandardUrn(LENGTH_PREFIX))
+          .put(GlobalWindow.Coder.class, getStandardUrn(GLOBAL_WINDOW))
+          .put(FullWindowedValueCoder.class, getStandardUrn(WINDOWED_VALUE))
           .build();
 
+  public static final Set<String> WELL_KNOWN_CODER_URNS = BEAM_MODEL_CODER_URNS.values();
+
   @VisibleForTesting
-  static final Map<Class<? extends Coder>, CoderTranslator<? extends Coder>>
-      BEAM_MODEL_CODERS =
-      ImmutableMap
-          .<Class<? extends Coder>, CoderTranslator<? extends Coder>>
-              builder()
+  static final Map<Class<? extends Coder>, CoderTranslator<? extends Coder>> BEAM_MODEL_CODERS =
+      ImmutableMap.<Class<? extends Coder>, CoderTranslator<? extends Coder>>builder()
           .put(ByteArrayCoder.class, CoderTranslators.atomic(ByteArrayCoder.class))
           .put(VarLongCoder.class, CoderTranslators.atomic(VarLongCoder.class))
           .put(IntervalWindowCoder.class, CoderTranslators.atomic(IntervalWindowCoder.class))
