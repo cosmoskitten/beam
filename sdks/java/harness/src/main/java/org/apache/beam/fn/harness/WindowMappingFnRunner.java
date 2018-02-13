fdd9f06301f812a96a3d78bfc5aa8fa4e6e2f64f
Luke Cwik <lcwik@google.com>
2018-02-12 18:35:40 -0800

    fixup! Address PR comments.

--- sdks/java/harness/src/main/java/org/apache/beam/fn/harness/WindowMappingFnRunner.java
+++ sdks/java/harness/src/main/java/org/apache/beam/fn/harness/WindowMappingFnRunner.java
@@ -15,43 +15,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.beam.fn.harness;
 
-import static com.google.common.collect.Iterables.getOnlyElement;
 import static org.apache.beam.runners.core.construction.UrnUtils.validateCommonUrn;
 
 import com.google.auto.service.AutoService;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Multimap;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Map;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-import org.apache.beam.fn.harness.data.BeamFnDataClient;
-import org.apache.beam.fn.harness.data.MultiplexingFnDataReceiver;
-import org.apache.beam.fn.harness.fn.ThrowingRunnable;
-import org.apache.beam.fn.harness.state.BeamFnStateClient;
-import org.apache.beam.model.pipeline.v1.RunnerApi;
-import org.apache.beam.model.pipeline.v1.RunnerApi.PCollection;
+import org.apache.beam.fn.harness.fn.ThrowingFunction;
 import org.apache.beam.model.pipeline.v1.RunnerApi.PTransform;
 import org.apache.beam.model.pipeline.v1.RunnerApi.SdkFunctionSpec;
 import org.apache.beam.runners.core.construction.PCollectionViewTranslation;
-import org.apache.beam.sdk.fn.data.FnDataReceiver;
-import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
 import org.apache.beam.sdk.transforms.windowing.WindowMappingFn;
-import org.apache.beam.sdk.util.WindowedValue;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.beam.sdk.values.KV;
 
 /**
- * Maps windows using a window mapping fn.
+ * Maps windows using a window mapping fn. The input is a window, the output must be a
+ * {@link KV} with the key being the original window and the value being the mapped window.
  */
-public class WindowMappingFnRunner<InputT extends BoundedWindow, OutputT extends BoundedWindow> {
-  private static final Logger LOG = LoggerFactory.getLogger(BeamFnDataReadRunner.class);
+public class WindowMappingFnRunner {
   static final String URN = validateCommonUrn("beam:transform:map_windows:v1");
 
   /**
@@ -67,59 +51,18 @@ public class WindowMappingFnRunner<InputT extends BoundedWindow, OutputT extends
     }
   }
 
-  /** A factory for {@link BeamFnDataReadRunner}s. */
   static class Factory<InputT extends BoundedWindow, OutputT extends BoundedWindow>
-      implements PTransformRunnerFactory<WindowMappingFnRunner<InputT, OutputT>> {
+      extends MapRunner.Factory<InputT, KV<InputT, OutputT>> {
 
     @Override
-    public WindowMappingFnRunner<InputT, OutputT> createRunnerForPTransform(
-        PipelineOptions pipelineOptions,
-        BeamFnDataClient beamFnDataClient,
-        BeamFnStateClient beamFnStateClient,
-        String pTransformId,
-        PTransform pTransform,
-        Supplier<String> processBundleInstructionId,
-        Map<String, PCollection> pCollections,
-        Map<String, RunnerApi.Coder> coders,
-        Map<String, RunnerApi.WindowingStrategy> windowingStrategies,
-        Multimap<String, FnDataReceiver<WindowedValue<?>>> pCollectionIdsToConsumers,
-        Consumer<ThrowingRunnable> addStartFunction,
-        Consumer<ThrowingRunnable> addFinishFunction) throws IOException {
-
-      Collection<FnDataReceiver<WindowedValue<OutputT>>> consumers =
-          (Collection) pCollectionIdsToConsumers.get(
-              getOnlyElement(pTransform.getOutputsMap().values()));
-
+    public ThrowingFunction<InputT, KV<InputT, OutputT>> createMapFunctionForPTransform(
+        String ptransformId, PTransform pTransform) throws IOException {
       SdkFunctionSpec windowMappingFnPayload =
           SdkFunctionSpec.parseFrom(pTransform.getSpec().getPayload());
       WindowMappingFn<OutputT> windowMappingFn =
           (WindowMappingFn<OutputT>) PCollectionViewTranslation.windowMappingFnFromProto(
               windowMappingFnPayload);
-
-      WindowMappingFnRunner<InputT, OutputT> runner =
-          new WindowMappingFnRunner<>(
-              windowMappingFn,
-              MultiplexingFnDataReceiver.forConsumers(consumers));
-      pCollectionIdsToConsumers.put(
-          Iterables.getOnlyElement(pTransform.getInputsMap().values()),
-          (FnDataReceiver) (FnDataReceiver<WindowedValue<InputT>>) runner::mapWindow);
-      return runner;
+      return (InputT input) -> KV.of(input, windowMappingFn.getSideInputWindow(input));
     }
-  }
-
-  private final WindowMappingFn<OutputT> windowMappingFn;
-  private final FnDataReceiver<WindowedValue<OutputT>> consumer;
-
-  public WindowMappingFnRunner(
-      WindowMappingFn<OutputT> windowMappingFn,
-      FnDataReceiver<WindowedValue<OutputT>> consumer) {
-    this.windowMappingFn = windowMappingFn;
-    this.consumer = consumer;
-  }
-
-  public void mapWindow(WindowedValue<InputT> element) throws Exception {
-    WindowedValue<OutputT> output =
-        element.withValue(windowMappingFn.getSideInputWindow(element.getValue()));
-    consumer.accept(output);
   }
 }
