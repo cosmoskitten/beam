5f7ffa0c3e82b61d79dd38a675e4458b48e0b520
Yifan Zou <yifanzou@rodete-desktop-imager.corp.google.com>
2018-02-23 13:28:46 -0800

    clean code

--- release/src/main/groovy/run_release_candidate_python_validation.sh
+++ release/src/main/groovy/run_release_candidate_python_validation.sh
@@ -118,7 +118,7 @@ verify_steaming_result() {
 
 # Python Release Candidate Configuration
 echo "SDK version: $(get_version)"
-VERSION="2.3.0"
+VERSION=$(get_version)
 CANDIDATE_URL="https://dist.apache.org/repos/dist/dev/beam/$VERSION/"
 SHA1_FILE_NAME="apache-beam-$VERSION-python.zip.sha1"
 MD5_FILE_NAME="apache-beam-$VERSION-python.zip.md5"
