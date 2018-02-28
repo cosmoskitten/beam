323f13c6066701fa592347081257d7c0b8b201a6
Holden Karau <holden@pigscanfly.ca>
2018-02-28 13:51:09 -0800

    Deprecate the comparators since I didn't find an active use for them and it encourages cmp like behaviour.

--- sdks/python/apache_beam/io/gcp/datastore/v1/helper.py
+++ sdks/python/apache_beam/io/gcp/datastore/v1/helper.py
@@ -29,6 +29,7 @@ from socket import error as SocketError
 # pylint: disable=ungrouped-imports
 from apache_beam.internal.gcp import auth
 from apache_beam.utils import retry
+from apache_beam.utils.annotations import deprecated
 
 # Protect against environments where datastore library is not available.
 # pylint: disable=wrong-import-order, wrong-import-position
@@ -48,6 +49,7 @@ except ImportError:
 # pylint: enable=ungrouped-imports
 
 
+@deprecated(since="v2.4")
 def key_comparator(k1, k2):
   """A comparator for Datastore keys.
 
@@ -76,6 +78,7 @@ def key_comparator(k1, k2):
   return 0
 
 
+@deprecated(since="v2.4")
 def compare_path(p1, p2):
   """A comparator for key path.
 
@@ -88,7 +91,7 @@ def compare_path(p1, p2):
   3. If no `id` is defined for both paths, then their `names` are compared.
   """
 
-  result = cmp(p1.kind, p2.kind)
+  result = (p1.kind > p2.kind) - (p1.kind < p2.kind)
   if result != 0:
     return result
 
@@ -96,12 +99,12 @@ def compare_path(p1, p2):
     if not p2.HasField('id'):
       return -1
 
-    return cmp(p1.id, p2.id)
+    return (p1.id > p2.id) - (p1.id < p2.id)
 
   if p2.HasField('id'):
     return 1
 
-  return cmp(p1.name, p2.name)
+  return (p1.name > p2.name) - (p1.name < p2.name)
 
 
 def get_datastore(project):
