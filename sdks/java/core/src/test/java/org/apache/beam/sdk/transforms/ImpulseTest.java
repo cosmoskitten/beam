9590590cd632f37801c8e1ce716345ef6781e631
Ben Sidhom <sidhom@google.com>
2018-03-01 15:36:04 -0800

    Add Impulse ValidatesRunner test

---
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/transforms/ImpulseTest.java
@@ -0,0 +1,31 @@
+package org.apache.beam.sdk.transforms;
+
+import java.util.Arrays;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.testing.UsesImpulse;
+import org.apache.beam.sdk.testing.ValidatesRunner;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ImpulseTest {
+  @Rule
+  public transient TestPipeline p = TestPipeline.create();
+
+  @Test
+  @Category({ValidatesRunner.class, UsesImpulse.class})
+  public void testImpulse() {
+    PCollection<Integer> result = p.apply(Impulse.create())
+        .apply(FlatMapElements.into(TypeDescriptors.integers())
+            .via(impulse -> Arrays.asList(1, 2, 3)));
+    PAssert.that(result).containsInAnyOrder(1, 2, 3);
+    p.run().waitUntilFinish();
+  }
+
+}
