200a03974e4e4d4391a1153eeb416c005287db19
Robert Bradshaw <robertwb@gmail.com>
2018-03-01 14:10:41 -0800

    [BEAM-2927] Python support for portable side inputs over Fn API

--- sdks/python/apache_beam/typehints/typehints.py
+++ sdks/python/apache_beam/typehints/typehints.py
@@ -1098,3 +1098,35 @@ def is_consistent_with(sub, base):
     # Nothing but object lives above any type constraints.
     return base == object
   return issubclass(sub, base)
+
+
+def coerce_to_kv_type(element_type, label=None):
+  """Attempts to coerce element_type to a compatible kv type.
+
+  Raises an error on failure.
+  """
+  # If input_type is not specified, then treat it as `Any`.
+  if not element_type:
+    return KV[Any, Any]
+  elif isinstance(element_type, TupleHint.TupleConstraint):
+    if len(element_type.tuple_types) == 2:
+      return element_type
+    else:
+      raise ValueError(
+          "Tuple input to %s must be have two components. "
+          "Found %s for %s" % (label, element_type))
+  elif isinstance(input_type, AnyTypeConstraint):
+    # `Any` type needs to be replaced with a KV[Any, Any] to
+    # satisfy the KV form.
+    return KV[Any, Any]
+  elif isinstance(element_type, UnionConstraint):
+    union_types = [
+        coerce_to_kv_type(t) for t in element_type.union_types]
+    return KV[
+        Union[tuple(t.tuple_types[0] for t in union_types)],
+        Union[tuple(t.tuple_types[1] for t in union_types)]]
+  else:
+    # TODO: Possibly handle other valid types.
+    raise ValueError(
+        "Input to %s must be of Tuple or Any type. "
+        "Found %s for %s" % (label, element_type))
