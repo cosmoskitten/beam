36a385742f8fe408cc14fe0bbe5450e427d35aa5
arkash <35550199+arkash@users.noreply.github.com>
2018-03-05 22:59:07 -0500

    BEAM-3246 Merge splits if they exceed 15K -- added checks to validate key ranges are adjacent before merging -- added logic to merge adjacent splits else continue without merge -- simplified merge code and loops

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigtable/BigtableIO.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigtable/BigtableIO.java
@@ -861,44 +861,29 @@ public class BigtableIO {
         return splits;
       }
       ImmutableList.Builder<BigtableSource> reducedSplits = ImmutableList.builder();
-      ImmutableList.Builder<ByteKeyRange> mergedRanges = ImmutableList.builder();
+      ImmutableList.Builder<ByteKeyRange> combinedRanges = ImmutableList.builder();
       int counter = 0;
       long size = 0;
       List<ByteKeyRange> previousSourceRanges = null;
       for (BigtableSource source : splits) {
         counter++;
-        if (checkRangeAdjacency(previousSourceRanges, source.getRanges())) {
-          mergedRanges.addAll(source.getRanges());
-          previousSourceRanges = source.getRanges();
-          size += source.getEstimatedSizeBytes(options);
-        } else {
-          if (size > 0) {
-            reducedSplits.add(
-                new BigtableSource(
-                    config,
-                    filter,
-                    ImmutableList.of(mergeRanges(mergedRanges.build())),
-                    size));
-          }
-          counter = 1;
+        if (counter == numberToCombine
+            || !checkRangeAdjacency(previousSourceRanges, source.getRanges())) {
+          reducedSplits.add(
+              new BigtableSource(
+                  config,
+                  filter,
+                  mergeRanges(combinedRanges.build()),
+                  size));
+          counter = 0;
           size = source.getEstimatedSizeBytes(options);
           previousSourceRanges = source.getRanges();
-          mergedRanges = ImmutableList.builder();
-          mergedRanges.addAll(previousSourceRanges);
-        }
-        if (counter == numberToCombine) {
-          if (size > 0) {
-            reducedSplits.add(
-                new BigtableSource(
-                    config,
-                    filter,
-                    ImmutableList.of(mergeRanges(mergedRanges.build())),
-                    size));
-          }
-          counter = 0;
-          size = 0;
-          previousSourceRanges = null;
-          mergedRanges = ImmutableList.builder();
+          combinedRanges = ImmutableList.builder();
+          combinedRanges.addAll(previousSourceRanges);
+        } else {
+          previousSourceRanges = mergeRanges(source.getRanges());
+          combinedRanges.addAll(previousSourceRanges);
+          size += source.getEstimatedSizeBytes(options);
         }
       }
       if (size > 0) {
@@ -906,7 +891,7 @@ public class BigtableIO {
             new BigtableSource(
                 config,
                 filter,
-                ImmutableList.of(mergeRanges(mergedRanges.build())),
+                mergeRanges(combinedRanges.build()),
                 size));
       }
       return reducedSplits.build();
@@ -915,7 +900,8 @@ public class BigtableIO {
     /** Helper to validate range Adjacency.
      * Ranges are considered adjacent if [1..100][100..200][200..300]
      **/
-    private boolean checkRangeAdjacency(List<ByteKeyRange> ranges, List<ByteKeyRange> otherRanges) {
+    private static boolean checkRangeAdjacency(List<ByteKeyRange> ranges,
+        List<ByteKeyRange> otherRanges) {
       checkArgument(ranges != null || otherRanges != null, "Both ranges cannot be null.");
       ImmutableList.Builder<ByteKeyRange> mergedRanges = ImmutableList.builder();
       if (ranges != null) {
@@ -930,7 +916,7 @@ public class BigtableIO {
     /** Helper to validate range Adjacency.
      * Ranges are considered adjacent if [1..100][100..200][200..300]
      **/
-    private boolean checkRangeAdjacency(List<ByteKeyRange> ranges) {
+    private static boolean checkRangeAdjacency(List<ByteKeyRange> ranges) {
       int index = 0;
       if (ranges.size() < 2) {
         return true;
@@ -951,13 +937,13 @@ public class BigtableIO {
      * ex. [1..100][100..200][200..300] will result in [1..300]
      * Note: this method will not check for adjacency see {@link #checkRangeAdjacency(List)}
      **/
-    private ByteKeyRange mergeRanges(List<ByteKeyRange> ranges) {
+    private static ImmutableList<ByteKeyRange> mergeRanges(List<ByteKeyRange> ranges) {
       if (ranges.size() < 2) {
-        return ranges.get(0);
+        return ImmutableList.of(ranges.get(0));
       }
-      return ByteKeyRange.of(
+      return ImmutableList.of(ByteKeyRange.of(
           ranges.get(0).getStartKey(),
-          ranges.get(ranges.size() - 1).getEndKey());
+          ranges.get(ranges.size() - 1).getEndKey()));
     }
 
     /** Helper that splits this source into bundles based on Cloud Bigtable sampled row keys. */
