09420dbebb407cde5c028fd4e508090546eb0c1e
Yifan Zou <yifanzou@rodete-desktop-imager.corp.google.com>
2018-03-28 20:24:23 -0700

    return stdout text from command execution and stop using var.last_text

--- release/src/main/groovy/quickstart-java-spark.groovy
+++ release/src/main/groovy/quickstart-java-spark.groovy
@@ -36,8 +36,8 @@ t.describe 'Run Apache Beam Java SDK Quickstart - Spark'
       --runner=SparkRunner" -Pspark-runner"""
 
     // Verify text from the pom.xml input file
-    t.run "grep Foundation counts*"
-    t.see "Foundation: 1"
+    String result = t.run "grep Foundation counts*"
+    t.see "Foundation: 1", result
 
     // Clean up
     t.done()
