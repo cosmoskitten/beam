b71d978026d38d17b5fd7b348daa0bf1c73f6c5e
Pablo <pabloem@google.com>
2018-04-17 09:45:16 -0700

    Quickfix of Python PostCommit

--- sdks/python/run_postcommit.sh
+++ sdks/python/run_postcommit.sh
@@ -31,20 +31,24 @@ set -v
 # pip install --user installation location.
 LOCAL_PATH=$HOME/.local/bin/
 
-# Remove any tox cache from previous workspace
-# TODO(udim): Remove this line and add '-r' to tox invocation instead.
-rm -rf sdks/python/target/.tox
-
 # INFRA does not install these packages
 pip install --user --upgrade virtualenv tox
 
-# Tox runs unit tests in a virtual environment
-${LOCAL_PATH}/tox -e ALL -c sdks/python/tox.ini
-
 # Virtualenv for the rest of the script to run setup & e2e tests
-${LOCAL_PATH}/virtualenv sdks/python
-. sdks/python/bin/activate
-cd sdks/python
+${LOCAL_PATH}/virtualenv .
+. bin/activate
+
+# TODO(pabloem): Move PostCommit to full Gradle, and avoid these workarounds.
+# Running setup.py install before pip install because pip installs within a tmp
+# directory, and makes gen_protos fail (as gen_protos relies on the code
+# structure of the git repo of Beam).
+python setup.py install
+
+# Workaround for protobuf, which has a known import issue, see:
+# https://github.com/tensorflow/tensorflow/issues/890
+pip uninstall -y protobuf
+pip install protobuf
+
 pip install -e .[gcp,test]
 
 # Run wordcount in the Direct Runner and validate output.
