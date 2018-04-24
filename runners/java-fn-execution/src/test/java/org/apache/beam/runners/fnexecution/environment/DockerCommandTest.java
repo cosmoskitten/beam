e1e01079aa0ad20917c95adf8eeb7bc3aabc37ad
Ben Sidhom <sidhom@google.com>
2018-04-23 17:12:34 -0700

    FIXUP: Capture stderr from Docker and test it

--- runners/java-fn-execution/src/test/java/org/apache/beam/runners/fnexecution/environment/DockerCommandTest.java
+++ runners/java-fn-execution/src/test/java/org/apache/beam/runners/fnexecution/environment/DockerCommandTest.java
@@ -17,12 +17,25 @@
  */
 package org.apache.beam.runners.fnexecution.environment;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 import org.apache.beam.runners.fnexecution.environment.testing.NeedsDocker;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -30,6 +43,8 @@ import org.junit.runners.JUnit4;
 @Category(NeedsDocker.class)
 @RunWith(JUnit4.class)
 public class DockerCommandTest {
+
+  @Rule public ExpectedException thrown = ExpectedException.none();
 
   @Test
   public void helloWorld() throws Exception {
@@ -42,10 +57,28 @@ public class DockerCommandTest {
   public void killContainer() throws Exception {
     DockerCommand docker = getWrapper();
     String container = docker.runImage("debian", Arrays.asList("/bin/bash", "-c", "sleep 60"));
+    Stopwatch stopwatch = Stopwatch.createStarted();
     docker.killContainer(container);
+    long elapsedSec = stopwatch.elapsed(TimeUnit.SECONDS);
+    assertThat(
+        "Container termination should complete before image self-exits",
+        elapsedSec,
+        is(lessThan(60L)));
+  }
+
+  @Test
+  public void capturesErrorOutput() throws Exception {
+    DockerCommand docker = getWrapper();
+    thrown.expect(instanceOf(IOException.class));
+    thrown.expectMessage(containsString("Error response from daemon"));
+    String badImageName = "this-image-should-hopefully-never-exist";
+    String container = docker.runImage(badImageName, ImmutableList.of());
+    // We should never reach this line, but clean up in case we do.
+    docker.killContainer(container);
+    Assert.fail(String.format("Container creation for %s should have failed", badImageName));
   }
 
   private static DockerCommand getWrapper() {
-    return DockerCommand.forExecutable("docker", Duration.ofMillis(10000));
+    return DockerCommand.forExecutable("docker", Duration.ofMillis(100_000));
   }
 }
