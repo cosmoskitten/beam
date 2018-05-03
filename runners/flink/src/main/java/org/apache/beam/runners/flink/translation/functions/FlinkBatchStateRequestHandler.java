52271282ebd69c21b94c99e364482c6aa758ea62
Ben Sidhom <sidhom@google.com>
2018-05-02 17:55:19 -0700

    WIP: Implement executable stage operator

---
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/functions/FlinkBatchStateRequestHandler.java
@@ -0,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.runners.flink.translation.functions;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.protobuf.ByteString;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import org.apache.beam.model.fnexecution.v1.BeamFnApi;
+import org.apache.beam.model.fnexecution.v1.BeamFnApi.StateResponse.Builder;
+import org.apache.beam.model.pipeline.v1.RunnerApi.Components;
+import org.apache.beam.model.pipeline.v1.RunnerApi.ExecutableStagePayload.SideInputId;
+import org.apache.beam.model.pipeline.v1.RunnerApi.MessageWithComponents;
+import org.apache.beam.model.pipeline.v1.RunnerApi.PCollection;
+import org.apache.beam.runners.core.construction.CoderTranslation;
+import org.apache.beam.runners.core.construction.RehydratedComponents;
+import org.apache.beam.runners.core.construction.graph.ExecutableStage;
+import org.apache.beam.runners.core.construction.graph.PipelineNode.PCollectionNode;
+import org.apache.beam.runners.core.construction.graph.SideInputReference;
+import org.apache.beam.runners.fnexecution.state.StateRequestHandler;
+import org.apache.beam.runners.fnexecution.wire.WireCoders;
+import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.coders.KvCoder;
+import org.apache.beam.sdk.util.WindowedValue;
+import org.apache.beam.sdk.util.WindowedValue.WindowedValueCoder;
+import org.apache.beam.sdk.values.KV;
+import org.apache.flink.api.common.functions.RuntimeContext;
+
+/**
+ * {@link StateRequestHandler} that uses a Flink {@link RuntimeContext} to access Flink broadcast
+ * variable that represent side inputs.
+ */
+class FlinkBatchStateRequestHandler implements StateRequestHandler {
+
+  // Map from side input id to global PCollection id.
+  private final Map<SideInputId, PCollectionNode> sideInputToCollection;
+  private final Components components;
+  private final RuntimeContext runtimeContext;
+
+  public static FlinkBatchStateRequestHandler forStage(
+      ExecutableStage stage, RuntimeContext runtimeContext) {
+    ImmutableMap.Builder<SideInputId, PCollectionNode> sideInputBuilder = ImmutableMap.builder();
+    for (SideInputReference sideInput : stage.getSideInputs()) {
+      sideInputBuilder.put(
+          SideInputId.newBuilder()
+              .setTransformId(sideInput.transform().getId())
+              .setLocalName(sideInput.localName())
+              .build(),
+          sideInput.collection());
+    }
+    Components components = stage.getComponents();
+    return new FlinkBatchStateRequestHandler(sideInputBuilder.build(), components, runtimeContext);
+  }
+
+  private FlinkBatchStateRequestHandler(
+      Map<SideInputId, PCollectionNode> sideInputToCollection,
+      Components components,
+      RuntimeContext runtimeContext) {
+    this.sideInputToCollection = sideInputToCollection;
+    this.components = components;
+    this.runtimeContext = runtimeContext;
+  }
+
+  @Override
+  public CompletionStage<Builder> handle(BeamFnApi.StateRequest request) throws Exception {
+    if (request.getStateKey().getTypeCase() != BeamFnApi.StateKey.TypeCase.MULTIMAP_SIDE_INPUT) {
+      throw new UnsupportedOperationException(
+          "This handler can only respond to MULTIMAP_SIDE_INPUT request.");
+    }
+    BeamFnApi.StateKey.MultimapSideInput multimapSideInput =
+        request.getStateKey().getMultimapSideInput();
+
+    String transformId = multimapSideInput.getPtransformId();
+    String localName = multimapSideInput.getSideInputId();
+
+    PCollectionNode collectionNode =
+        sideInputToCollection.get(
+            SideInputId.newBuilder().setTransformId(transformId).setLocalName(localName).build());
+    checkState(collectionNode != null, "No side input for %s/%s", transformId, localName);
+    List<Object> broadcastVariable = runtimeContext.getBroadcastVariable(collectionNode.getId());
+
+    PCollection pCollection = collectionNode.getPCollection();
+
+    String windowingStrategyId = pCollection.getWindowingStrategyId();
+    String windowCoderId =
+        components.getWindowingStrategiesOrThrow(windowingStrategyId).getWindowCoderId();
+
+    MessageWithComponents protoCoder =
+        WireCoders.createRunnerWireCoder(collectionNode, components, components::containsCoders);
+    // TODO: Does this need to use length-prefixed coders?
+    Coder javaCoder =
+        CoderTranslation.fromProto(
+            protoCoder.getCoder(), RehydratedComponents.forComponents(protoCoder.getComponents()));
+    // TODO: What is the protocol for state api coders? Can we _always_ use KV<byte[], byte[]>?
+
+    // TODO: we wouldn't have to do this if the harness didn't always
+    // expect a KV<Void, T> as input for a side input, currently the key field
+    // in the MultimapSideInput request is always Void
+    // we know the input coder is always a WindowedValueCoder<KvCoder<Void, SomeCoder>>
+    // and the harness expects a list of things encoded with SomeCoder
+    KvCoder kvCoder = (KvCoder) ((WindowedValueCoder) javaCoder).getValueCoder();
+    Coder someCoder = kvCoder.getValueCoder();
+
+    ByteString.Output output = ByteString.newOutput();
+    for (Object windowedValue : broadcastVariable) {
+      Object value = ((KV) ((WindowedValue) windowedValue).getValue()).getValue();
+      someCoder.encode(value, output);
+    }
+
+    CompletableFuture<Builder> response = new CompletableFuture<>();
+    Builder responseBuilder =
+        BeamFnApi.StateResponse.newBuilder()
+            .setGet(BeamFnApi.StateGetResponse.newBuilder().setData(output.toByteString()));
+    response.complete(responseBuilder);
+    return response;
+  }
+}
--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/functions/FlinkExecutableStageFunction.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/functions/FlinkExecutableStageFunction.java
@@ -17,12 +17,24 @@
  */
 package org.apache.beam.runners.flink.translation.functions;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
 import com.google.protobuf.Struct;
 import java.util.Map;
+import javax.annotation.concurrent.GuardedBy;
 import org.apache.beam.model.pipeline.v1.RunnerApi;
+import org.apache.beam.runners.core.construction.graph.ExecutableStage;
+import org.apache.beam.runners.fnexecution.control.JobBundleFactory;
+import org.apache.beam.runners.fnexecution.control.OutputReceiverFactory;
+import org.apache.beam.runners.fnexecution.control.RemoteBundle;
+import org.apache.beam.runners.fnexecution.control.StageBundleFactory;
+import org.apache.beam.runners.fnexecution.state.StateRequestHandler;
+import org.apache.beam.sdk.fn.data.FnDataReceiver;
 import org.apache.beam.sdk.transforms.join.RawUnionValue;
 import org.apache.beam.sdk.util.WindowedValue;
 import org.apache.flink.api.common.functions.RichMapPartitionFunction;
+import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.util.Collector;
 
@@ -45,6 +57,11 @@ public class FlinkExecutableStageFunction<InputT>
   // Map from PCollection id to the union tag used to represent this PCollection in the output.
   private final Map<String, Integer> outputMap;
 
+  // Worker-local fields. These should only be constructed and consumed on Flink TaskManagers.
+  private transient RuntimeContext runtimeContext;
+  private transient StateRequestHandler stateRequestHandler;
+  private transient StageBundleFactory stageBundleFactory;
+
   public FlinkExecutableStageFunction(
       RunnerApi.ExecutableStagePayload stagePayload,
       Struct pipelineOptions,
@@ -56,18 +73,71 @@ public class FlinkExecutableStageFunction<InputT>
 
   @Override
   public void open(Configuration parameters) throws Exception {
-    throw new UnsupportedOperationException();
+    ExecutableStage executableStage = ExecutableStage.fromPayload(stagePayload);
+    runtimeContext = getRuntimeContext();
+    // NOTE: It's safe to reuse the state handler between partitions because each partition uses the
+    // same backing runtime context and broadcast variables. We use checkState below to catch errors
+    // in backward-incompatible Flink changes.
+    stateRequestHandler = FlinkBatchStateRequestHandler.forStage(executableStage, runtimeContext);
+    // TODO: Implement and wire in JobBundleFactory.
+    JobBundleFactory jobBundleFactory = null;
+    stageBundleFactory = jobBundleFactory.forStage(executableStage);
   }
 
   @Override
   public void mapPartition(
       Iterable<WindowedValue<InputT>> iterable, Collector<RawUnionValue> collector)
       throws Exception {
-    throw new UnsupportedOperationException();
+    checkState(
+        runtimeContext == getRuntimeContext(),
+        "RuntimeContext changed from under us. State handler invalid.");
+    checkState(
+        stageBundleFactory != null, "%s not yet prepared", StageBundleFactory.class.getName());
+    checkState(
+        stateRequestHandler != null, "%s not yet prepared", StateRequestHandler.class.getName());
+
+    try (RemoteBundle<InputT> bundle =
+        stageBundleFactory.getBundle(
+            new ReceiverFactory(collector, outputMap), stateRequestHandler)) {
+      FnDataReceiver<WindowedValue<InputT>> receiver = bundle.getInputReceiver();
+      for (WindowedValue<InputT> input : iterable) {
+        receiver.accept(input);
+      }
+    }
+    // NOTE: RemoteBundle.close() blocks on completion of all data receivers. This is necessary to
+    // safely reference the partition-scoped Collector from receivers.
   }
 
   @Override
   public void close() throws Exception {
-    throw new UnsupportedOperationException();
+    stageBundleFactory.close();
+  }
+
+  private static class ReceiverFactory implements OutputReceiverFactory {
+
+    private final Object collectorLock = new Object();
+
+    @GuardedBy("collectorLock")
+    private final Collector<RawUnionValue> collector;
+
+    private final Map<String, Integer> outputMap;
+
+    ReceiverFactory(Collector<RawUnionValue> collector, Map<String, Integer> outputMap) {
+      this.collector = collector;
+      this.outputMap = outputMap;
+    }
+
+    @Override
+    public <OutputT> FnDataReceiver<OutputT> create(String collectionId) {
+      Integer unionTag = outputMap.get(collectionId);
+      checkArgument(unionTag != null, "Unknown PCollection id: %s", collectionId);
+      int tagInt = unionTag;
+      // TODO: Synchronize access to collector.
+      return (receivedElement) -> {
+        synchronized (collectorLock) {
+          collector.collect(new RawUnionValue(tagInt, receivedElement));
+        }
+      };
+    }
   }
 }
