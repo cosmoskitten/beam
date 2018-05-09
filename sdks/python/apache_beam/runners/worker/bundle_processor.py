41b1b1c49ff338fc1019eb86013daaa7c455bbeb
Luke Cwik <lcwik@google.com>
2018-05-09 16:44:41 -0700

    fixup! more lint

--- sdks/python/apache_beam/runners/worker/bundle_processor.py
+++ sdks/python/apache_beam/runners/worker/bundle_processor.py
@@ -669,11 +669,13 @@ def create(factory, transform_id, transform_proto, unused_parameter, consumers):
 def create(factory, transform_id, transform_proto, mapping_fn_spec, consumers):
   assert mapping_fn_spec.spec.urn == python_urns.PICKLED_WINDOW_MAPPING_FN
   window_mapping_fn = pickler.loads(mapping_fn_spec.spec.payload)
+
   class MapWindows(beam.DoFn):
 
     def process(self, element):
       key, window = element
       return [(key, window_mapping_fn(window))]
+
   return _create_simple_pardo_operation(
       factory, transform_id, transform_proto, consumers,
       MapWindows())
