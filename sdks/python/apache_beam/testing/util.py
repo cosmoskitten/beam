c1bc9fd939485303b05bc91353524f1faaea8889
Maria Garcia Herrero <mariagh@google.com>
2018-05-10 00:35:29 -0700

    Remove debugging printouts

--- sdks/python/apache_beam/testing/util.py
+++ sdks/python/apache_beam/testing/util.py
@@ -76,29 +76,21 @@ def contains_in_any_order(iterable):
 
 
 def equal_to_per_window(expected_dict_window_to_its_elements):
-  # print 'EQUAL'
   def matcher(elements):
     actual_elements_in_window, window = elements
-    print 'actual_elements_in_window, window', actual_elements_in_window, window
-    assert isinstance(expected_dict_window_to_its_elements, dict)
-    # print 'before IF'
     if window in expected_dict_window_to_its_elements:
       expected_elements_in_window = list(
           expected_dict_window_to_its_elements[window])
       sorted_expected = sorted(expected_elements_in_window)
       sorted_actual = sorted(actual_elements_in_window)
-      print 'EXPECTED:', sorted_expected
-      print 'ACTUAL:', sorted_actual
       if sorted_expected != sorted_actual:
-        print '!', all(elem in sorted_expected  for elem in sorted_actual)
-        # Sometimes the same actual window has subsets of the expected elements
-        # as results may come in early triggers.
+        # Results for the same window don't necessarily come all
+        # at once. Hence the same actual window may contain only
+        # subsets of the expected elements for the window.
+        # For example, in the presence of early triggers.
         if all(elem in sorted_expected  for elem in sorted_actual) is False:
           raise BeamAssertException(
               'Failed assert: %r == %r' % (sorted_expected, sorted_actual))
-    # else:
-    #   raise BeamAssertException('Failed assert: %r not in expected windows.' % window)
-
   return matcher
 
 
@@ -161,7 +153,6 @@ def assert_that(actual, matcher, windowing=None,
   class AddWindow(DoFn):
     def process(self, element, timestamp=DoFn.TimestampParam,
                 window=DoFn.WindowParam):
-      print 'element, window:', element, window
       yield element, window
 
   class AssertThat(PTransform):
