254d7d9e5c727f3bccdb7a32a90fc5c04bcb22ae
Mairbek Khadikov <mairbek@google.com>
2018-05-10 09:34:48 -0700

    Fix test
    Change-Id: I936efe226b23df4ecad6933d4c1382c573c356d7

--- sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/spanner/SpannerWriteIT.java
+++ sdks/java/io/google-cloud-platform/src/test/java/org/apache/beam/sdk/io/gcp/spanner/SpannerWriteIT.java
@@ -191,7 +191,7 @@ public class SpannerWriteIT {
 
   @Test
   public void testFailFast() throws Exception {
-    thrown.expect(new StackTraceContainsString("PipelineExecutionException"));
+    thrown.expect(new StackTraceContainsString("SpannerException"));
     thrown.expect(new StackTraceContainsString("Value must not be NULL in table users"));
     int numRecords = 100;
     p.apply(GenerateSequence.from(0).to(2 * numRecords))
