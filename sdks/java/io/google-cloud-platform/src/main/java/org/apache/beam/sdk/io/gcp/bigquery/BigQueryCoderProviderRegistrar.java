b54c9aac631168a80f12820910ca60ab4ac4fb67
Carlos Alonso <carlos.alonso@cabify.com>
2018-05-11 20:23:52 +0200

    Adds BigQueryInsertErrorCoder and tests

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryCoderProviderRegistrar.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/BigQueryCoderProviderRegistrar.java
@@ -35,6 +35,8 @@ public class BigQueryCoderProviderRegistrar implements CoderProviderRegistrar {
   public List<CoderProvider> getCoderProviders() {
     return ImmutableList.of(
         CoderProviders.forCoder(TypeDescriptor.of(TableRow.class), TableRowJsonCoder.of()),
-        CoderProviders.forCoder(TypeDescriptor.of(TableRowInfo.class), TableRowInfoCoder.of()));
+        CoderProviders.forCoder(TypeDescriptor.of(TableRowInfo.class), TableRowInfoCoder.of()),
+        CoderProviders.forCoder(
+            TypeDescriptor.of(BigQueryInsertError.class), BigQueryInsertErrorCoder.of()));
   }
 }
