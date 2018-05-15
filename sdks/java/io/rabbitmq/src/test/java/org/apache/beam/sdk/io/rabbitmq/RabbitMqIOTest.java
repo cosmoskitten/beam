308b06c8ad966eb00326729f31688c3392208dbe
Jean-Baptiste Onofré <jb@nanthrax.net>
2018-05-15 07:51:59 +0200

    [BEAM-1240] Minor style fixes

--- sdks/java/io/rabbitmq/src/test/java/org/apache/beam/sdk/io/rabbitmq/RabbitMqIOTest.java
+++ sdks/java/io/rabbitmq/src/test/java/org/apache/beam/sdk/io/rabbitmq/RabbitMqIOTest.java
@@ -30,8 +30,11 @@ import com.rabbitmq.client.Envelope;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.ServerSocket;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.transforms.Create;
@@ -40,65 +43,73 @@ import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.qpid.server.Broker;
 import org.apache.qpid.server.BrokerOptions;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * Test of {@link RabbitMqIO}.
- */
+/** Test of {@link RabbitMqIO}. */
+@RunWith(JUnit4.class)
 public class RabbitMqIOTest implements Serializable {
+  private static final Logger LOG = LoggerFactory.getLogger(RabbitMqIOTest.class);
 
-  public int port;
+  private static int port;
+  @ClassRule public static TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-  @Rule public transient TestPipeline pipeline = TestPipeline.create();
-  @Rule public transient TemporaryFolder temporaryFolder = new TemporaryFolder();
+  @Rule public transient TestPipeline p = TestPipeline.create();
 
-  private transient Broker broker;
+  private static transient Broker broker;
 
-  @Before
-  public void startBroker() throws Exception {
+  @BeforeClass
+  public static void startBroker() throws Exception {
     try (ServerSocket serverSocket = new ServerSocket(0)) {
       port = serverSocket.getLocalPort();
     }
 
+    System.setProperty("derby.stream.error.field", "MyApp.DEV_NULL");
     broker = new Broker();
     BrokerOptions options = new BrokerOptions();
-    options.setConfigProperty("qpid.amqp_port", String.valueOf(port));
-    options.setConfigProperty("qpid.work_dir", temporaryFolder.newFolder().toString());
-    options.setConfigProperty("qpid.home_dir", "src/test/qpid");
+    options.setConfigProperty(BrokerOptions.QPID_AMQP_PORT, String.valueOf(port));
+    options.setConfigProperty(BrokerOptions.QPID_WORK_DIR, temporaryFolder.newFolder().toString());
+    options.setConfigProperty(BrokerOptions.QPID_HOME_DIR, "src/test/qpid");
     broker.startup(options);
   }
 
-  @After
-  public void stopBroker() throws Exception {
+  @AfterClass
+  public static void stopBroker() {
     broker.shutdown();
   }
 
   @Test
   public void testReadQueue() throws Exception {
-    PCollection<RabbitMqMessage> raw = pipeline.apply(
-        RabbitMqIO.read().withUri("amqp://guest:guest@localhost:" + port).withQueue("READ")
-          .withMaxNumRecords(10));
+    final int maxNumRecords = 10;
+    PCollection<RabbitMqMessage> raw =
+        p.apply(
+            RabbitMqIO.read()
+                .withUri("amqp://guest:guest@localhost:" + port)
+                .withQueue("READ")
+                .withMaxNumRecords(maxNumRecords));
     PCollection<byte[]> output = raw.apply(ParDo.of(new ConverterFn()));
 
-    PAssert.that(output)
-        .containsInAnyOrder("Test 0".getBytes(), "Test 1".getBytes(), "Test 2".getBytes(),
-            "Test 3".getBytes(), "Test 4".getBytes(), "Test 5".getBytes(), "Test 6".getBytes(),
-            "Test 7".getBytes(), "Test 8".getBytes(), "Test 9".getBytes());
+    List<byte[]> records = generateRecords(maxNumRecords);
+    PAssert.that(output).containsInAnyOrder(records);
 
     ConnectionFactory connectionFactory = new ConnectionFactory();
     connectionFactory.setUri("amqp://guest:guest@localhost:" + port);
     Connection connection = connectionFactory.newConnection();
     Channel channel = connection.createChannel();
     channel.queueDeclare("READ", false, false, false, null);
-    for (int i = 0; i < 10; i++) {
-      channel.basicPublish("", "READ", null, ("Test " + i).getBytes());
+    for (byte[] record : records) {
+      channel.basicPublish("", "READ", null, record);
     }
 
-    pipeline.run();
+    p.run();
 
     channel.close();
     connection.close();
@@ -106,47 +117,42 @@ public class RabbitMqIOTest implements Serializable {
 
   @Test(timeout = 60 * 1000)
   public void testReadExchange() throws Exception {
-    PCollection<RabbitMqMessage> raw = pipeline.apply(
-        RabbitMqIO.read().withUri("amqp://guest:guest@localhost:" + port)
-            .withExchange("READ", "fanout", "test")
-            .withMaxNumRecords(10));
+    final int maxNumRecords = 10;
+    PCollection<RabbitMqMessage> raw =
+        p.apply(
+            RabbitMqIO.read()
+                .withUri("amqp://guest:guest@localhost:" + port)
+                .withExchange("READEXCHANGE", "fanout", "test")
+                .withMaxNumRecords(maxNumRecords));
     PCollection<byte[]> output = raw.apply(ParDo.of(new ConverterFn()));
-    PAssert.that(output).containsInAnyOrder(
-        "Test 0".getBytes(),
-        "Test 1".getBytes(),
-        "Test 2".getBytes(),
-        "Test 3".getBytes(),
-        "Test 4".getBytes(),
-        "Test 5".getBytes(),
-        "Test 6".getBytes(),
-        "Test 7".getBytes(),
-        "Test 8".getBytes(),
-        "Test 9".getBytes());
+
+    List<byte[]> records = generateRecords(maxNumRecords);
+    PAssert.that(output).containsInAnyOrder(records);
 
     ConnectionFactory connectionFactory = new ConnectionFactory();
     connectionFactory.setUri("amqp://guest:guest@localhost:" + port);
     Connection connection = connectionFactory.newConnection();
     final Channel channel = connection.createChannel();
-    channel.exchangeDeclare("READ", "fanout");
-    Thread publisher = new Thread() {
-      @Override
-      public void run() {
-        try {
-          Thread.sleep(5000);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-        for (int i = 0; i < 10; i++) {
-          try {
-            channel.basicPublish("READ", "test", null, ("Test " + i).getBytes());
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    };
+    channel.exchangeDeclare("READEXCHANGE", "fanout");
+    Thread publisher =
+        new Thread(
+            () -> {
+              try {
+                Thread.sleep(5000);
+              } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+              }
+              for (int i = 0; i < 10; i++) {
+                try {
+                  channel.basicPublish(
+                      "READEXCHANGE", "test", null, ("Test " + i).getBytes(StandardCharsets.UTF_8));
+                } catch (Exception e) {
+                  LOG.error(e.getMessage(), e);
+                }
+              }
+            });
     publisher.start();
-    pipeline.run();
+    p.run();
     publisher.join();
 
     channel.close();
@@ -155,15 +161,16 @@ public class RabbitMqIOTest implements Serializable {
 
   @Test
   public void testWriteExchange() throws Exception {
-    List<RabbitMqMessage> data = new ArrayList<>();
-    for (int i = 0; i < 1000; i++) {
-      RabbitMqMessage message = new RabbitMqMessage(("Test " + i).getBytes());
-      data.add(message);
-    }
-    pipeline.apply(Create.of(data))
-        .apply(RabbitMqIO.write()
-            .withUri("amqp://guest:guest@localhost:" + port)
-            .withExchange("WRITE", "fanout"));
+    final int maxNumRecords = 1000;
+    List<RabbitMqMessage> data =
+        IntStream.range(0, maxNumRecords)
+            .mapToObj(i -> new RabbitMqMessage(("Test " + i).getBytes(StandardCharsets.UTF_8)))
+            .collect(Collectors.toList());
+    p.apply(Create.of(data))
+        .apply(
+            RabbitMqIO.write()
+                .withUri("amqp://guest:guest@localhost:" + port)
+                .withExchange("WRITE", "fanout"));
 
     final List<String> received = new ArrayList<>();
     ConnectionFactory connectionFactory = new ConnectionFactory();
@@ -173,26 +180,26 @@ public class RabbitMqIOTest implements Serializable {
     channel.exchangeDeclare("WRITE", "fanout");
     String queueName = channel.queueDeclare().getQueue();
     channel.queueBind(queueName, "WRITE", "");
-    Consumer consumer = new DefaultConsumer(channel) {
-      @Override
-      public void handleDelivery(String consumerTag,
-          Envelope envelope,
-          AMQP.BasicProperties properties,
-          byte[] body) throws IOException {
-        String message = new String(body, "UTF-8");
-        received.add(message);
-      }
-    };
+    Consumer consumer =
+        new DefaultConsumer(channel) {
+          @Override
+          public void handleDelivery(
+              String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+              throws IOException {
+            String message = new String(body, "UTF-8");
+            received.add(message);
+          }
+        };
     channel.basicConsume(queueName, true, consumer);
 
-    pipeline.run();
+    p.run();
 
-    while (received.size() < 1000) {
+    while (received.size() < maxNumRecords) {
       Thread.sleep(500);
     }
 
-    assertEquals(1000, received.size());
-    for (int i = 0; i < 1000; i++) {
+    assertEquals(maxNumRecords, received.size());
+    for (int i = 0; i < maxNumRecords; i++) {
       assertTrue(received.contains("Test " + i));
     }
 
@@ -200,16 +207,19 @@ public class RabbitMqIOTest implements Serializable {
     connection.close();
   }
 
-  private class ConverterFn extends DoFn<RabbitMqMessage, byte[]> {
-
-    public ConverterFn() {}
+  private static class ConverterFn extends DoFn<RabbitMqMessage, byte[]> {
+    ConverterFn() {}
 
     @ProcessElement
-    public void processElement(ProcessContext processContext) {
-      RabbitMqMessage message = processContext.element();
-      processContext.output(message.getBody());
+    public void processElement(ProcessContext c) {
+      RabbitMqMessage message = c.element();
+      c.output(message.getBody());
     }
-
   }
 
+  private static List<byte[]> generateRecords(int maxNumRecords) {
+    return IntStream.range(0, maxNumRecords)
+        .mapToObj(i -> ("Test " + i).getBytes(StandardCharsets.UTF_8))
+        .collect(Collectors.toList());
+  }
 }
