016fcc3e3424452b09ea9af8e1ab8982dd296ce6
Colm O hEigeartaigh <coheigea@apache.org>
2018-05-15 17:03:56 +0100

    Avoid fully qualified class names, when there is an existing (static) import

--- runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/CoderTranslationTest.java
+++ runners/core-construction-java/src/test/java/org/apache/beam/runners/core/construction/CoderTranslationTest.java
@@ -144,7 +144,7 @@ public class CoderTranslationTest {
       Coder<?> decodedCoder =
           CoderTranslation.fromProto(
               coderProto, RehydratedComponents.forComponents(encodedComponents));
-      assertThat(decodedCoder, Matchers.equalTo(coder));
+      assertThat(decodedCoder, equalTo(coder));
 
       if (KNOWN_CODERS.contains(coder)) {
         for (RunnerApi.Coder encodedCoder : encodedComponents.getCodersMap().values()) {
