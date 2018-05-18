e2c45fadd672e7b11846f29ce7c3a867e66b7aee
Andrew Pilloud <apilloud@google.com>
2018-05-18 13:59:31 -0700

    [SQL] Split out JDBC IT

--- sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/impl/JdbcDriverTest.java
+++ sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/impl/JdbcDriverTest.java
@@ -22,10 +22,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import com.google.common.collect.ImmutableMap;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.Driver;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import org.apache.beam.sdk.extensions.sql.meta.provider.BeamSqlTableProvider;
@@ -38,56 +34,6 @@ import org.junit.Test;
  * Test for {@link JdbcDriver}.
  */
 public class JdbcDriverTest {
-
-  @Test
-  public void testDriverManager_getDriver() throws Exception {
-    Driver driver = DriverManager.getDriver(JdbcDriver.CONNECT_STRING_PREFIX);
-    assertTrue(driver instanceof JdbcDriver);
-  }
-
-  @Test
-  public void testDriverManager_simple() throws Exception {
-    Connection connection = DriverManager.getConnection(JdbcDriver.CONNECT_STRING_PREFIX);
-    Statement statement = connection.createStatement();
-    // SELECT 1 is a special case and does not reach the parser
-    assertTrue(statement.execute("SELECT 1"));
-  }
-
-  @Test
-  public void testDriverManager_parse() throws Exception {
-    Connection connection = DriverManager.getConnection(JdbcDriver.CONNECT_STRING_PREFIX);
-    Statement statement = connection.createStatement();
-    assertTrue(statement.execute("SELECT 'beam'"));
-  }
-
-  @Test
-  public void testDriverManager_ddl() throws Exception {
-    Connection connection = DriverManager.getConnection(JdbcDriver.CONNECT_STRING_PREFIX);
-
-    // Ensure no tables
-    final DatabaseMetaData metadata = connection.getMetaData();
-    ResultSet resultSet = metadata.getTables(null, null, null, new String[] {"TABLE"});
-    assertFalse(resultSet.next());
-
-    // Create tables
-    Statement statement = connection.createStatement();
-    assertEquals(0, statement.executeUpdate(
-        "CREATE TABLE test (id INTEGER) TYPE 'text'"));
-
-    // Ensure table test
-    resultSet = metadata.getTables(null, null, null, new String[] {"TABLE"});
-    assertTrue(resultSet.next());
-    assertEquals("test", resultSet.getString("TABLE_NAME"));
-    assertFalse(resultSet.next());
-
-    // Create tables
-    assertEquals(0, statement.executeUpdate(
-        "DROP TABLE test"));
-
-    // Ensure no tables
-    resultSet = metadata.getTables(null, null, null, new String[] {"TABLE"});
-    assertFalse(resultSet.next());
-  }
 
   @Test
   public void testInternalConnect_boundedTable() throws Exception {
