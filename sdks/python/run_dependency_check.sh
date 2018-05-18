df0966472e24b842275b60461f0990ebd53ebb07
Yifan Zou <yifanzou@rodete-desktop-imager.corp.google.com>
2018-05-17 18:22:01 -0700

    BEAM-4302, add jenkins job for python dependency check

---
+++ sdks/python/run_dependency_check.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#    Licensed to the Apache Software Foundation (ASF) under one or more
+#    contributor license agreements.  See the NOTICE file distributed with
+#    this work for additional information regarding copyright ownership.
+#    The ASF licenses this file to You under the Apache License, Version 2.0
+#    (the "License"); you may not use this file except in compliance with
+#    the License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+# This script will be run by Jenkins as a post commit test. In order to run
+# locally make the following changes:
+#
+# GCS_LOCATION -> Temporary location to use for service tests.
+# PROJECT      -> Project name to use for service jobs.
+#
+# Execute from the root of the repository: sdks/python/run_dependency_check.sh
+
+set -e
+set -v
+
+# Virtualenv for the rest of the script to run setup
+/usr/bin/virtualenv sdks/python
+. sdks/python/bin/activate
+cd sdks/python
+pip install -e .[gcp,test,docs]
+
+pip install yolk
+
+yolk -U
