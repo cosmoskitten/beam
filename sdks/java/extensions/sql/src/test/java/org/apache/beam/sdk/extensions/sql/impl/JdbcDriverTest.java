5619a2a4d1dc84779fe141b4a3bf54743b19139f
Kenn Knowles <kenn@kennknowles.com>
2018-05-26 20:37:13 -0700

    TestUtils.RowsBuilder to use FieldType instead of TypeName

--- sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/impl/JdbcDriverTest.java
+++ sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/impl/JdbcDriverTest.java
@@ -30,6 +30,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import org.apache.beam.sdk.extensions.sql.meta.provider.BeamSqlTableProvider;
 import org.apache.beam.sdk.extensions.sql.mock.MockedBoundedTable;
+import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.Schema.TypeName;
 import org.apache.calcite.jdbc.CalciteConnection;
 import org.junit.Before;
@@ -99,8 +100,8 @@ public class JdbcDriverTest {
             ImmutableMap.of(
                 "test",
                 MockedBoundedTable.of(
-                        TypeName.INT32, "id",
-                        TypeName.STRING, "name")
+                        Schema.FieldType.INT32, "id",
+                        Schema.FieldType.STRING, "name")
                     .addRows(1, "first")));
     CalciteConnection connection = JdbcDriver.connect(tableProvider);
     Statement statement = connection.createStatement();
