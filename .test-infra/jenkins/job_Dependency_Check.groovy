e62a5ca3ef86f1fb4c16c676ed2c180399735a24
Yifan Zou <yifanzou@rodete-desktop-imager.corp.google.com>
2018-05-30 15:59:30 -0700

    [BEAM-4302] add groovy scripts to extract check results

--- .test-infra/jenkins/job_Dependency_Check.groovy
+++ .test-infra/jenkins/job_Dependency_Check.groovy
@@ -17,6 +17,7 @@
  */
 
 import common_job_properties
+import dependency_check_utils
 
 job('beam_Dependency_Check') {
   description('Runs Beam dependency check.')
@@ -34,19 +35,24 @@ job('beam_Dependency_Check') {
   steps {
     // gradle task :dependencyUpdates will do Java dependency version check
     // ./gradlew :dependencyUpdates -Drevision=release -DreportfileName=javaDependencyReport
+//    gradle {
+//      rootBuildScriptDir(common_job_properties.checkoutDir)
+//      tasks(':dependencyUpdates')
+//      common_job_properties.setGradleSwitches(delegate)
+//      switches('-Drevision=release')
+//    }
+
     gradle {
       rootBuildScriptDir(common_job_properties.checkoutDir)
-      tasks(':dependencyUpdates')
+      tasks(':release:runBeamDependencyCheck')
       common_job_properties.setGradleSwitches(delegate)
-      switches('-Drevision=release')
     }
-
     // The shell script "run_dependency_check" will do version check for Python SDK.
-    shell('cd ' + common_job_properties.checkoutDir +
-      ' && bash sdks/python/run_dependency_check.sh')
+//    shell('cd ' + common_job_properties.checkoutDir +
+//      ' && bash sdks/python/run_dependency_check.sh')
 
-//    groovyScriptFile(common_job_properties.checkoutDir + '/.test-infra/jenkins/dependency_check_utils.groovy')
-    groovyCommand("src/.test-infra/jenkins/dependency_check_utils.generateDependencyReport()")
+    groovyScriptFile(common_job_properties.checkoutDir + '/.test-infra/jenkins/dependency_check_utils.groovy')
+//    groovyCommand("src/.test-infra/jenkins/dependency_check_utils.generateDependencyReport()")
   }
 
   publishers {
@@ -56,7 +62,8 @@ job('beam_Dependency_Check') {
           recipientList('yifanzou@google.com')
           contentType('text/plain')
           subject('Beam Dependency Check Report')
-          content(emailContents)
+//          content('''${SCRIPT, template="my-email.template"}''')
+          content('''${FILE, path="src/build/dependencyUpdates/dependency-check-report.txt"}''')
         }
       }
     }
