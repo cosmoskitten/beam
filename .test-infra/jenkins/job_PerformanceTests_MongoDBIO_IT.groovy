88cc0f10e2a74c4ee7fa1a57a96121ac99fe5381
Kasia <katarzyna.kucharczyk@polidea.com>
2018-06-04 14:16:33 +0200

    Changed a namespace to parsed job name with current job number.

--- .test-infra/jenkins/job_PerformanceTests_MongoDBIO_IT.groovy
+++ .test-infra/jenkins/job_PerformanceTests_MongoDBIO_IT.groovy
@@ -42,7 +42,7 @@ job('beam_PerformanceTests_MongoDBIO_IT') {
             numberOfRecords: '10000000'
     ]
 
-    String namespace = common_job_properties.getKubernetesNamespace('mongodbioit')
+    String namespace = common_job_properties.getKubernetesNamespace("beam_PerformanceTests_MongoDBIO_IT")
     String kubeconfig = common_job_properties.getKubeconfigLocationForNamespace(namespace)
 
     def testArgs = [
