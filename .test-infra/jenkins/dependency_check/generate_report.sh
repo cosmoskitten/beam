b178b13b36cbe67eced22aa22c156553b6616f8b
Yifan Zou <yifanzou@rodete-desktop-imager.corp.google.com>
2018-06-17 16:35:46 -0700

    [BEAM-4302] add unit tests and address PR feedbacks.

--- .test-infra/jenkins/dependency_check/generate_report.sh
+++ .test-infra/jenkins/dependency_check/generate_report.sh
@@ -25,32 +25,21 @@ DATASET_ID='beam_dependency_states'
 PYTHON_DEP_TABLE_ID='python_dependency_states'
 JAVA_DEP_TABLE_ID='java_dependency_states'
 REPORT_DESCRIPTION="
-<h4> In Beam, we make a best-effort attempt at keeping all dependencies up-to-date. </h4>
 <h4> A dependency update is high priority if it satisfies one of following criteria: </h4>
 <ul>
-<li> It has major versions update available;
-  <ul>
-    <li> e.g. org.assertj:assertj-core 2.5.0 -> 3.10.0 </li>
-  </ul>
-</li>
+<li> It has major versions update available, e.g. org.assertj:assertj-core 2.5.0 -> 3.10.0; </li>
 </ul>
 <ul>
-<li> It is over 3 minor versions behind the latest version;
-  <ul>
-    <li> e.g. org.tukaani:xz 1.5 -> 1.8 </li>
-  </ul>
-</li>
+<li> It is over 3 minor versions behind the latest version, e.g. org.tukaani:xz 1.5 -> 1.8; </li>
 </ul>
 <ul>
-<li> The current version is behind the later version for over 180 days.
-  <ul>
-    <li> e.g. com.google.auto.service:auto-service 2014-10-24 -> 2017-12-11 </li>
-  </ul>
-</li>
+<li> The current version is behind the later version for over 180 days, e.g. com.google.auto.service:auto-service 2014-10-24 -> 2017-12-11. </li>
 </ul>
-
-<h4> In the future, issues will be filed and tracked for these automatically, but in the meantime you can search for existing issues or open a new one. </h4>
-<h4> Read more about our dependency update policy: <a href=\"https://docs.google.com/document/d/15m1MziZ5TNd9rh_XN0YYBJfYkt0Oj-Ou9g0KFDPL2aA/edit#\"> Beam Dependency Update Policy </a></h4>"
+<h4> In Beam, we make a best-effort attempt at keeping all dependencies up-to-date.
+     In the future, issues will be filed and tracked for these automatically,
+     but in the meantime you can search for existing issues or open a new one.
+</h4>
+<h4> For more information: <a href=\"https://docs.google.com/document/d/15m1MziZ5TNd9rh_XN0YYBJfYkt0Oj-Ou9g0KFDPL2aA/edit#\"> Beam Dependency Update Policy </a></h4>"
 
 
 # Virtualenv for the rest of the script to run setup
@@ -58,22 +47,25 @@ REPORT_DESCRIPTION="
 . dependency/check/bin/activate
 pip install --upgrade google-cloud-bigquery
 
+# Run the unit tests of the report generator
+python $WORKSPACE/src/.test-infra/jenkins/dependency_check/dependency_check_report_generator_test.py \
+
 rm -f build/dependencyUpdates/beam-dependency-check-report.txt
 
-echo "<html><body>" > build/dependencyUpdates/beam-dependency-check-report.html
+echo "<html><body>" > $WORKSPACE/src/build/dependencyUpdates/beam-dependency-check-report.html
 
-python $WORKSPACE/src/.test-infra/jenkins/dependency_check/generate_dependency_check_report.py \
+python $WORKSPACE/src/.test-infra/jenkins/dependency_check/dependency_check_report_generator.py \
 build/dependencyUpdates/python_dependency_report.txt \
 Python \
 $PROJECT_ID \
 $DATASET_ID \
 $PYTHON_DEP_TABLE_ID
 
-python $WORKSPACE/src/.test-infra/jenkins/dependency_check/generate_dependency_check_report.py \
+python $WORKSPACE/src/.test-infra/jenkins/dependency_check/dependency_check_report_generator.py \
 build/dependencyUpdates/report.txt \
 Java \
 $PROJECT_ID \
 $DATASET_ID \
 $JAVA_DEP_TABLE_ID
 
-echo "$REPORT_DESCRIPTION </body></html>" >> build/dependencyUpdates/beam-dependency-check-report.html
+echo "$REPORT_DESCRIPTION </body></html>" >> $WORKSPACE/src/build/dependencyUpdates/beam-dependency-check-report.html
