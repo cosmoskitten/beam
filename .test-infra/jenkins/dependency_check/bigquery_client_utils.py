745780009ba952ed96080449e9a93eb935a74b09
Yifan Zou <yifanzou@rodete-desktop-imager.corp.google.com>
2018-06-17 16:35:46 -0700

    [BEAM-4302] add unit tests and address PR feedbacks.

--- .test-infra/jenkins/dependency_check/bigquery_client_utils.py
+++ .test-infra/jenkins/dependency_check/bigquery_client_utils.py
@@ -15,7 +15,11 @@
 # limitations under the License.
 #
 
+import datetime
+import logging
 from google.cloud import bigquery
+
+logger = logging.getLogger(__name__)
 
 class BigQueryClientUtils:
 
@@ -28,40 +32,117 @@ class BigQueryClientUtils:
     self.table = self.bigquery_client.get_table(self.table_ref)
 
 
-  def query_dep_release_date(self, dep, version):
+  def query_dep_info_by_version(self, dep, version):
     """
-    Query for release date of a specific version
+    Query for dependency information of a specific version
     Args:
       dep, version
     Return:
-      release_date
+      release_date, is_currently_used
     """
-    query = """SELECT release_date FROM `{0}.{1}.{2}` 
-    WHERE package_name=\'{3}\' and version=\'{4}\'""".format(self.project_id,
-                                                             self.dataset_id,
-                                                             self.table_id,
-                                                             dep.strip(),
-                                                             version.strip())
+    query = """SELECT release_date, is_currently_used 
+      FROM `{0}.{1}.{2}` 
+      WHERE package_name=\'{3}\' AND version=\'{4}\'""".format(self.project_id,
+                                                               self.dataset_id,
+                                                               self.table_id,
+                                                               dep.strip(),
+                                                               version.strip())
 
     query_job = self.bigquery_client.query(query)
     rows = list(query_job)
     if len(rows) == 0:
-      return None
+      logger.info("Did not find record of dependency {0} with version {1}.".format(dep, version))
+      return None, False
     assert len(rows) == 1
-    return rows[0]['release_date']
+    logger.info("""Found record of dependency {0} with version {1}: 
+    release date: {2}; is_currently_used: {3}.""".format(dep, version, rows[0]['release_date'], rows[0]['is_currently_used']))
+    return rows[0]['release_date'], rows[0]['is_currently_used']
 
 
-  def add_dep_to_table(self, dep, version, release_date, is_current_using=False):
+  def query_currently_used_dep_info_in_db(self, dep):
+    """
+    Query for the info of the currently used version of a specific dependency
+    Args:
+      dep
+    Return:
+      version, release_date
+    """
+    query = """SELECT version, release_date 
+      FROM `{0}.{1}.{2}` 
+      WHERE package_name=\'{3}\' AND is_currently_used=True""".format(self.project_id,
+                                                                     self.dataset_id,
+                                                                     self.table_id,
+                                                                     dep.strip())
+
+    query_job = self.bigquery_client.query(query)
+    rows = list(query_job)
+    if len(rows) == 0:
+      return None, None
+    assert len(rows) == 1
+    return rows[0]['version'], rows[0]['release_date']
+
+
+  def insert_dep_to_table(self, dep, version, release_date, is_currently_used=False):
     """
     Add a dependency with version and release date into bigquery table
     Args:
-      dep, version, is_current_using (default False)
-    Return:
-      release_date
+      dep, version, is_currently_used (default False)
     """
-    dep_to_insert = [
-      (dep, version, release_date, is_current_using)
-    ]
-    err = self.bigquery_client.insert_rows(self.table, dep_to_insert)
-    if len(err) != 0:
-      print "Found errors when inserting {1} to BQ table {2}:\n {3}".format(dep_to_insert[0], self.table_id, err)
+    query = """INSERT  
+    `{0}.{1}.{2}` (package_name, version, release_date, is_currently_used)
+    VALUES (\'{3}\', \'{4}\', \'{5}\', {6})""".format(self.project_id,
+                                                  self.dataset_id,
+                                                  self.table_id,
+                                                  dep.strip(),
+                                                  version.strip(),
+                                                  release_date,
+                                                  is_currently_used)
+    print "Inserting dep to table: \n {0}".format(query)
+    try:
+      query_job = self.bigquery_client.query(query)
+      if not query_job.done():
+        print query_job.result()
+    except:
+      raise
+
+
+  def delete_dep_from_table(self, dep, version):
+    """
+    Remove a dependency record from the table.
+    Args:
+      dep, version
+    """
+    query = """DELETE  
+      FROM `{0}.{1}.{2}` 
+      WHERE package_name=\'{3}\' AND version=\'{4}\'""".format(self.project_id,
+                                                               self.dataset_id,
+                                                               self.table_id,
+                                                               dep.strip(),
+                                                               version.strip())
+    print "Deleting dep from table: \n {0}".format(query)
+    try:
+      query_job = self.bigquery_client.query(query)
+      if not query_job.done():
+        print query_job.result()
+    except:
+      raise
+
+
+  def clean_stale_records_from_table(self):
+    """
+    Remove stale records from the table. A record is stale if it is not currently used and the release date is behind 3
+    years.
+    """
+    query = """DELETE  
+      FROM `{0}.{1}.{2}` 
+      WHERE release_date < '{3}'""".format(self.project_id,
+                                                self.dataset_id,
+                                                self.table_id,
+                                                datetime.datetime.today().date() - datetime.timedelta(3*365))
+    print "Clean Up Starts"
+    try:
+      query_job = self.bigquery_client.query(query)
+      if not query_job.done():
+        print query_job.result()
+    except:
+      raise
