34d7844e44cc199c8325c5410153d493627a9fab
Etienne Chauchot <echauchot@apache.org>
2018-06-20 19:00:29 +0200

    Add mandatory temp location

--- .test-infra/jenkins/nexmark_bigquery_properties.groovy
+++ .test-infra/jenkins/nexmark_bigquery_properties.groovy
@@ -23,5 +23,6 @@ class nexmark_bigquery_properties {
                                          '--bigQueryDataset=nexmark',
                                          '--project=apache-beam-testing',
                                          '--resourceNameMode=QUERY_RUNNER_AND_MODE',
-                                         '--exportSummaryToBigQuery=true'].join(' ')
+                                         '--exportSummaryToBigQuery=true',
+                                         '--tempLocation=nexmark-temp'].join(' ')
 }
