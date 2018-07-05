a478fb2d80e49d448726cbf4ad4f26cf9328f72e
Etienne Chauchot <echauchot@apache.org>
2018-07-05 11:58:29 +0200

    Move UsesMetricsPusher category from runner-core to sdk because not all the runners depend on runner-core

--- sdks/java/core/src/main/java/org/apache/beam/sdk/testing/UsesMetricsPusher.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/testing/UsesMetricsPusher.java
@@ -15,6 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Defines utilities for unit testing Runners.*/
+package org.apache.beam.sdk.testing;
 
-package org.apache.beam.runners.core.testing;
+/** Category tag for validation tests which utilize the metrics pusher feature. */
+
+public interface UsesMetricsPusher {}
