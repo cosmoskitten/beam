1eb684f6453b9f670aaad8c56b3d094292666e2b
Ben Sidhom <sidhom@google.com>
2018-07-09 10:51:40 -0700

    Remove outdated TODO

--- runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/environment/DockerEnvironmentFactory.java
+++ runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/environment/DockerEnvironmentFactory.java
@@ -131,8 +131,6 @@ public class DockerEnvironmentFactory implements EnvironmentFactory {
     Path workerPersistentDirectory = workerTempDir.resolve(pathSafeWorkerId);
     String semiPersistentDirectory = "/var/opt/apache/beam/semi_persistent_dir";
     String containerImage = environment.getUrl();
-    // TODO: https://issues.apache.org/jira/browse/BEAM-4148 The default service address will not
-    // work for Docker for Mac.
     String loggingEndpoint = loggingServiceServer.getApiServiceDescriptor().getUrl();
     String artifactEndpoint = retrievalServiceServer.getApiServiceDescriptor().getUrl();
     String provisionEndpoint = provisioningServiceServer.getApiServiceDescriptor().getUrl();
