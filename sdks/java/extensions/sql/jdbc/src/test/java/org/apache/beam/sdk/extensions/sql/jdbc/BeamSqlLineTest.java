46a227ffe0e71924c88f4994b35d353c07cdcacc
amaliujia <amaliujia@gmail.com>
2018-07-17 12:25:13 -0700

    [BEAM-4806] improve unit tests for SqlLineMain

--- sdks/java/extensions/sql/jdbc/src/test/java/org/apache/beam/sdk/extensions/sql/jdbc/BeamSqlLineTest.java
+++ sdks/java/extensions/sql/jdbc/src/test/java/org/apache/beam/sdk/extensions/sql/jdbc/BeamSqlLineTest.java
@@ -17,7 +17,11 @@
  */
 package org.apache.beam.sdk.extensions.sql.jdbc;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.PrintStream;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -29,6 +33,12 @@ import org.junit.rules.TemporaryFolder;
 public class BeamSqlLineTest {
 
   @Rule public TemporaryFolder folder = new TemporaryFolder();
+  public ByteArrayOutputStream byteArrayOutputStream;
+
+  @Before
+  public void setUp() {
+    byteArrayOutputStream = new ByteArrayOutputStream();
+  }
 
   @Test
   public void testSqlLine_emptyArgs() throws Exception {
@@ -65,13 +75,137 @@ public class BeamSqlLineTest {
     BeamSqlLine.main(
         new String[] {
           "-e",
-          "CREATE TABLE test (id INTEGER) TYPE 'text' LOCATION '"
+          "CREATE TABLE test_table (id INTEGER) TYPE 'text' LOCATION '"
               + simpleTable.getAbsolutePath()
               + "';",
           "-e",
-          "SELECT * FROM test;",
+          "SELECT * FROM test_table;",
           "-e",
-          "DROP TABLE test;"
+          "DROP TABLE test_table;"
         });
+  }
+
+  @Test
+  public void testSqlLine_select() throws Exception {
+    BeamSqlLine.testMain(
+        new String[] {"-e", "SELECT 3, 'hello', DATE '2018-05-28';"},
+        new PrintStream(byteArrayOutputStream));
+
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "3", "hello", "2018-05-28");
+    verifyStringOutputFalse(byteArrayOutputStream.toString(), "4", "hella", "2018-09-09");
+  }
+
+  @Test
+  public void testSqlLine_selectFromTable() throws Exception {
+    BeamSqlLine.testMain(
+        new String[] {
+          "-e",
+          "CREATE TABLE table_test (col_a VARCHAR, col_b VARCHAR, col_c VARCHAR, col_x TINYINT, col_y INT, col_z BIGINT) TYPE 'test';",
+          "-e",
+          "INSERT INTO table_test VALUES ('a', 'b', 'c', 1, 2, 3);",
+          "-e",
+          "SELECT * FROM table_test;"
+        },
+        new PrintStream(byteArrayOutputStream));
+
+    verifyStringOutputTrue(
+        byteArrayOutputStream.toString(), "col_a", "col_b", "col_c", "col_x", "col_y", "col_z");
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "a", "b", "c", "1", "2", "3");
+  }
+
+  @Test
+  public void testSqlLine_insertSelect() throws Exception {
+    BeamSqlLine.testMain(
+        new String[] {
+          "-e",
+          "CREATE TABLE table_test (col_a VARCHAR, col_b VARCHAR) TYPE 'test';",
+          "-e",
+          "INSERT INTO table_test SELECT '3', 'hello';",
+          "-e",
+          "SELECT * FROM table_test;"
+        },
+        new PrintStream(byteArrayOutputStream));
+
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "3", "hello");
+  }
+
+  @Test
+  public void testSqlLine_GroupBy() throws Exception {
+    BeamSqlLine.testMain(
+        new String[] {
+          "-e",
+          "CREATE TABLE table_test (col_a VARCHAR, col_b VARCHAR) TYPE 'test';",
+          "-e",
+          "INSERT INTO table_test SELECT '3', 'foo';",
+          "-e",
+          "INSERT INTO table_test SELECT '3', 'bar';",
+          "-e",
+          "INSERT INTO table_test SELECT '4', 'foo';",
+          "-e",
+          "SELECT col_a, count(*) FROM table_test GROUP BY col_a;"
+        },
+        new PrintStream(byteArrayOutputStream));
+
+    // col_a 3 has count(*) = 2
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "3", "2");
+    // col_a 4 has count(*) = 1
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "4", "1");
+  }
+
+  @Test
+  public void testSqlLine_fixedWindow() throws Exception {
+    BeamSqlLine.testMain(
+        new String[] {
+          "-e",
+          "CREATE TABLE table_test (col_a VARCHAR, col_b TIMESTAMP) TYPE 'test';",
+          "-e",
+          "INSERT INTO table_test SELECT '3', TIMESTAMP '2018-07-01 21:26:06';",
+          "-e",
+          "INSERT INTO table_test SELECT '3', TIMESTAMP '2018-07-01 21:26:07';",
+          "-e",
+          "SELECT TUMBLE_START(col_b, INTERVAL '1' SECOND), count(*) FROM table_test GROUP BY TUMBLE(col_b, INTERVAL '1' SECOND);"
+        },
+        new PrintStream(byteArrayOutputStream));
+
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "2018-07-01 21:26:06", "1");
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "2018-07-01 21:26:07", "1");
+  }
+
+  @Test
+  public void testSqlLine_slidingWindow() throws Exception {
+    BeamSqlLine.testMain(
+        new String[] {
+          "-e",
+          "CREATE TABLE table_test (col_a VARCHAR, col_b TIMESTAMP) TYPE 'test';",
+          "-e",
+          "INSERT INTO table_test SELECT '3', TIMESTAMP '2018-07-01 21:26:06';",
+          "-e",
+          "INSERT INTO table_test SELECT '4', TIMESTAMP '2018-07-01 21:26:07';",
+          "-e",
+          "INSERT INTO table_test SELECT '6', TIMESTAMP '2018-07-01 21:26:08';",
+          "-e",
+          "INSERT INTO table_test SELECT '7', TIMESTAMP '2018-07-01 21:26:09';",
+          "-e",
+          "SELECT HOP_END(col_b, INTERVAL '1' SECOND, INTERVAL '2' SECOND), count(*) FROM table_test GROUP BY HOP(col_b, INTERVAL '1' SECOND, INTERVAL '2' SECOND);"
+        },
+        new PrintStream(byteArrayOutputStream));
+
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "2018-07-01 21:26:07", "1");
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "2018-07-01 21:26:08", "2");
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "2018-07-01 21:26:09", "2");
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "2018-07-01 21:26:10", "2");
+    verifyStringOutputTrue(byteArrayOutputStream.toString(), "2018-07-01 21:26:11", "1");
+  }
+
+  private void verifyStringOutputTrue(String queryOutput, String... strs) {
+    for (String str : strs) {
+      Assert.assertTrue(queryOutput.contains(str));
+    }
+  }
+
+  private void verifyStringOutputFalse(String queryOutput, String... strs) {
+    for (String str : strs) {
+      Assert.assertFalse(queryOutput.contains(str));
+    }
   }
 }
