77b074d38b9eafec8dc2530d1e85ee1f6a986a30
Katarzyna Kucharczyk <katarzyna.kucharczyk@polidea.com>
2018-07-20 12:07:13 +0200

    Added verbose to buildPerformanceTest. Activated env virtualenv

--- .test-infra/jenkins/CommonJobProperties.groovy
+++ .test-infra/jenkins/CommonJobProperties.groovy
@@ -315,17 +315,26 @@ class CommonJobProperties {
         // create new VirtualEnv, inherit already existing packages
         shell('virtualenv .env --system-site-packages')
 
+        shell('pip show numpy')
+        shell('which python')
+
+        shell('source .env/bin/activate')
+        shell('pip show numpy')
+        shell('which python')
+
         // update setuptools and pip
-        shell('.env/bin/pip install --upgrade setuptools pip')
+        shell('pip install --upgrade setuptools pip')
 
         // Clone appropriate perfkit branch
         shell('git clone https://github.com/GoogleCloudPlatform/PerfKitBenchmarker.git')
         // Install Perfkit benchmark requirements.
-        shell('.env/bin/pip install -r PerfKitBenchmarker/requirements.txt')
+        shell('pip install -r PerfKitBenchmarker/requirements.txt')
         // Install job requirements for Python SDK.
-        shell('.env/bin/pip install -e ' + CommonJobProperties.checkoutDir + '/sdks/python/[gcp,test]')
+        shell('pip install -e ' + CommonJobProperties.checkoutDir + '/sdks/python/[gcp,test]')
         // Launch performance test.
-        shell(".env/bin/python PerfKitBenchmarker/pkb.py $pkbArgs")
+
+        shell('pip show numpy')
+        shell("python PerfKitBenchmarker/pkb.py $pkbArgs")
     }
   }
 
