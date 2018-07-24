a12b3ec2801f8601658f599a222425658c38bd04
jxlewis <jxlewis@godaddy.com>
2018-07-24 14:57:38 -0700

    made it so that aws related config comes from AwsOptions

---
+++ sdks/java/io/amazon-web-services/src/main/java/org/apache/beam/sdk/io/aws/sqs/SqsConfiguration.java
@@ -0,0 +1,41 @@
+package org.apache.beam.sdk.io.aws.sqs;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.Serializable;
+import org.apache.beam.sdk.io.aws.options.AwsModule;
+import org.apache.beam.sdk.io.aws.options.AwsOptions;
+
+class SqsConfiguration implements Serializable {
+
+  private String awsRegion;
+  private String awsCredentialsProviderString;
+
+  public SqsConfiguration(AwsOptions awsOptions) {
+    ObjectMapper om = new ObjectMapper();
+    om.registerModule(new AwsModule());
+    try {
+      this.awsCredentialsProviderString =
+          om.writeValueAsString(awsOptions.getAwsCredentialsProvider());
+    } catch (JsonProcessingException e) {
+      this.awsCredentialsProviderString = null;
+    }
+    this.awsRegion = awsOptions.getAwsRegion();
+  }
+
+  public AWSCredentialsProvider getAwsCredentialsProvider() {
+    ObjectMapper om = new ObjectMapper();
+    om.registerModule(new AwsModule());
+    try {
+      return om.readValue(awsCredentialsProviderString, AWSCredentialsProvider.class);
+    } catch (IOException e) {
+      return null;
+    }
+  }
+
+  public String getAwsRegion() {
+    return awsRegion;
+  }
+}
--- sdks/java/io/amazon-web-services/src/main/java/org/apache/beam/sdk/io/aws/sqs/SqsUnboundedSource.java
+++ sdks/java/io/amazon-web-services/src/main/java/org/apache/beam/sdk/io/aws/sqs/SqsUnboundedSource.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.beam.sdk.io.aws.sqs;
 
 import com.amazonaws.services.sqs.model.Message;
@@ -13,17 +30,19 @@ import org.apache.beam.sdk.options.PipelineOptions;
 
 class SqsUnboundedSource extends UnboundedSource<Message, SqsCheckpointMark> {
 
-  private final Read spec;
+  private final Read read;
+  private final SqsConfiguration sqsConfiguration;
 
-  public SqsUnboundedSource(Read spec) {
-    this.spec = spec;
+  public SqsUnboundedSource(Read read, SqsConfiguration sqsConfiguration) {
+    this.read = read;
+    this.sqsConfiguration = sqsConfiguration;
   }
 
   @Override
   public List<SqsUnboundedSource> split(int desiredNumSplits, PipelineOptions options) {
     List<SqsUnboundedSource> sources = new ArrayList<>();
     for (int i = 0; i < Math.max(1, desiredNumSplits); ++i) {
-      sources.add(new SqsUnboundedSource(spec));
+      sources.add(new SqsUnboundedSource(read, sqsConfiguration));
     }
     return sources;
   }
@@ -42,12 +61,15 @@ class SqsUnboundedSource extends UnboundedSource<Message, SqsCheckpointMark> {
 
   @Override
   public Coder<Message> getOutputCoder() {
-
     return SerializableCoder.of(Message.class);
   }
 
-  public Read getSpec() {
-    return spec;
+  public Read getRead() {
+    return read;
+  }
+
+  public SqsConfiguration getSqsConfiguration() {
+    return sqsConfiguration;
   }
 
   @Override
