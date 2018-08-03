28d9cc1201b536c9267a2ebcb5c92d0e6ee7227e
Alex Amato <ajamato@ajamato-linux0.sea.corp.google.com>
2018-08-03 09:21:19 -0700

    Implementing a subset of the new metrics framework in python. https://s.apache.org/beam-fn-api-metrics
    The follow types have been implemented
    'beam:metrics:SumInt64'
    'beam:metrics:DistributionInt64'
    'beam:metrics:LatestInt64'
    User counters and the existing Metrics returned on
    ProcessBundle[Progress]Response are now being returned
    as monitoring_infos in ProcessBundle[Progress]Response.
    
    Note: The deprecated metrics are still being returned in
    the ProcessBundle[Progress]Response.
    
    Addtionally, a parameter has been added to MetricResults
    allowing to return metrics via use_monitoring_infos.
    
    fn_api_runner_test.test_metrics has been updated to
    verifies that the queried metrics are equivalent, whether
    or not use_monitoring_infos has been specified.
    
    Additionally, MetricResults has been updated to also return
    non user metrics, by setting user_metrics_only=True.
    This has been using in a new test:
    fn_api_runner_test.test_non_user_metrics

--- sdks/python/apache_beam/metrics/cells.py
+++ sdks/python/apache_beam/metrics/cells.py
@@ -161,6 +161,14 @@ class CounterCell(Counter, MetricCell):
     with self._lock:
       return self.value
 
+  def to_runner_api_monitoring_info(self):
+    """Returns a Metric with this value for use in a MonitoringInfo."""
+    return beam_fn_api_pb2.Metric(
+      counter_data=beam_fn_api_pb2.CounterData(
+          int64_value=self.get_cumulative()
+      )
+    )
+
 
 class DistributionCell(Distribution, MetricCell):
   """For internal use only; no backwards-compatibility guarantees.
@@ -375,6 +383,14 @@ class GaugeData(object):
                        float(proto.timestamp.nanos) / 10**9)
     return GaugeData(proto.value, timestamp=gauge_timestamp)
 
+  def to_runner_api_monitoring_info(self):
+    """Returns a Metric with this value for use in a MonitoringInfo."""
+    return beam_fn_api_pb2.Metric(
+        counter_data=beam_fn_api_pb2.CounterData(
+            int64_value=self.value
+        )
+    )
+
 
 class DistributionData(object):
   """For internal use only; no backwards-compatibility guarantees.
@@ -439,6 +455,13 @@ class DistributionData(object):
   @staticmethod
   def from_runner_api(proto):
     return DistributionData(proto.sum, proto.count, proto.min, proto.max)
+
+  def to_runner_api_monitoring_info(self):
+    """Returns a Metric with this value for use in a MonitoringInfo."""
+    return beam_fn_api_pb2.Metric(
+        distribution_data=beam_fn_api_pb2.DistributionData(
+            int_distribution_data=beam_fn_api_pb2.IntDistributionData(
+                count=self.count, sum=self.sum, min=self.min, max=self.max)))
 
 
 class MetricAggregator(object):
