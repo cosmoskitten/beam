b43eca388ac14e11ef617305f088c787ce8ac673
Yifan Zou <yifanzou@yifanzou-linuxworkstation.sea.corp.google.com>
2018-08-08 14:25:50 -0700

    [BEAM-5109] create python nightly snapshots

---
+++ sdks/python/scripts/run_snapshot_publish.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+#    Licensed to the Apache Software Foundation (ASF) under one or more
+#    contributor license agreements.  See the NOTICE file distributed with
+#    this work for additional information regarding copyright ownership.
+#    The ASF licenses this file to You under the Apache License, Version 2.0
+#    (the "License"); you may not use this file except in compliance with
+#    the License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+
+VERSION=$(awk '/__version__/{print $3}' $WORKSPACE/src/sdks/python/apache_beam/version.py)
+VERSION=$(echo VERSION | cut -c 2- | rev | cut -c 2- | rev)
+cd $WORKSPACE/src/sdks/python/build
+time=$(date +"%Y-%m-%dT%H:%M:%S")
+SNAPSHOT="apache-beam-$VERSION-$time.zip"
+
+# rename the file to be apache-beam-{VERSION}-{datetime}.zip
+for file in "apache-beam-$VERSION[0:9].zip"; do
+  mv $file $SNAPSHOT
+done
+
+#generate sha512 hash
+sha512sum $SNAPSHOT > "$SNAPSHOT.sha512"
+
+ls
+#generate gpg key
+gpg --armor --detach-sig $SNAPSHOT
+
+ls
+
