7d11ab1c1a8d96deb199f4adc4ad230b9f657c33
Ankit Jhalaria <ajhalaria@godaddy.com>
2018-08-23 15:04:32 -0700

    Fix accidental rename of AwsModuleTest to AwsModuleAmazonSNSMock

--- sdks/java/io/amazon-web-services/src/test/java/org/apache/beam/sdk/io/aws/options/AwsModule.java
+++ sdks/java/io/amazon-web-services/src/test/java/org/apache/beam/sdk/io/aws/options/AwsModule.java
@@ -47,7 +47,7 @@ import org.junit.runners.JUnit4;
 
 /** Tests {@link AwsModule}. */
 @RunWith(JUnit4.class)
-public class AwsModuleAmazonSNSMock {
+public class AwsModule {
 
   private final ObjectMapper objectMapper = new ObjectMapper().registerModule(new AwsModule());
 
