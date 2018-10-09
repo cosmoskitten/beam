4b55495377827da2115cb83bed03b727d7a0ae57
Maximilian Michels <mxm@apache.org>
2018-10-09 17:57:56 +0200

    [BEAM-3727] Allow sources to shutdown when checkpointing is disabled
    This auto-configures shutdown of the sources depending on whether checkpointing
    is enabled or disabled. When checkpointing is disabled, sources are allowed to
    shutdown, otherwise they have to stay up.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkExecutionEnvironments.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkExecutionEnvironments.java
@@ -203,6 +203,10 @@ public class FlinkExecutionEnvironments {
             .getCheckpointConfig()
             .setMinPauseBetweenCheckpoints(minPauseBetweenCheckpoints);
       }
+    } else {
+      // checkpointing is disabled, we can allow shutting down sources when they're done
+      // https://issues.apache.org/jira/browse/FLINK-2491
+      options.setShutdownSourcesOnFinalWatermark(true);
     }
 
     applyLatencyTrackingInterval(flinkStreamEnv.getConfig(), options);
