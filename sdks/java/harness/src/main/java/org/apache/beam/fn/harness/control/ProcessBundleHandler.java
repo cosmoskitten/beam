a680914bd1da8cb000d2ae7d4d6977b00d844711
Alex Amato <ajamato@ajamato-linux0.sea.corp.google.com>
2018-10-15 11:10:45 -0700

    Added some comments to learn

--- sdks/java/harness/src/main/java/org/apache/beam/fn/harness/control/ProcessBundleHandler.java
+++ sdks/java/harness/src/main/java/org/apache/beam/fn/harness/control/ProcessBundleHandler.java
@@ -300,6 +300,8 @@ public class ProcessBundleHandler {
       }
     }
 
+    // TODO do we add metrics to this progress response?
+
     return BeamFnApi.InstructionResponse.newBuilder().setProcessBundle(response);
   }
 
