7596e930e319d9ecbad75b8c845793aceddd8887
David Moravek <david.moravek@firma.seznam.cz>
2018-10-19 16:43:47 +0200

    [BEAM-5790] Euphoria: Remove Dataset abstraction.

--- sdks/java/extensions/euphoria/src/test/java/org/apache/beam/sdk/extensions/euphoria/core/translate/BeamMetricsTranslationTest.java
+++ sdks/java/extensions/euphoria/src/test/java/org/apache/beam/sdk/extensions/euphoria/core/translate/BeamMetricsTranslationTest.java
@@ -23,7 +23,6 @@ import static org.junit.Assert.assertThat;
 
 import java.util.stream.Stream;
 import org.apache.beam.sdk.PipelineResult;
-import org.apache.beam.sdk.extensions.euphoria.core.client.dataset.Dataset;
 import org.apache.beam.sdk.extensions.euphoria.core.client.io.Collector;
 import org.apache.beam.sdk.extensions.euphoria.core.client.operator.MapElements;
 import org.apache.beam.sdk.extensions.euphoria.core.client.operator.ReduceByKey;
@@ -72,9 +71,9 @@ public class BeamMetricsTranslationTest {
     final String counterName1 = "counter1";
     final String operatorName1 = "count_elements_and_save_even_numbers";
 
-    final Dataset<KV<Integer, Integer>> kvInput =
+    final PCollection<KV<Integer, Integer>> kvInput =
         ReduceByKey.named(operatorName1)
-            .of(Dataset.of(input))
+            .of(input)
             .keyBy(e -> e)
             .reduceBy(
                 (Stream<Integer> list, Collector<Integer> coll) ->
@@ -92,7 +91,7 @@ public class BeamMetricsTranslationTest {
     final String operatorName2 = "map_to_integer";
     final String operatorName3 = "map_elements";
 
-    final Dataset<Integer> mapElementsOutput =
+    final PCollection<Integer> mapElementsOutput =
         MapElements.named(operatorName2)
             .of(kvInput) // kvInput = [<2,2>, <4,4>]
             .using(
@@ -104,7 +103,7 @@ public class BeamMetricsTranslationTest {
                 })
             .output();
 
-    final Dataset<Integer> output =
+    final PCollection<Integer> output =
         MapElements.named(operatorName3)
             .of(mapElementsOutput) // mapElementsOutput = [2,4]
             .using(
@@ -115,7 +114,7 @@ public class BeamMetricsTranslationTest {
                 })
             .output();
 
-    PAssert.that(output.getPCollection()).containsInAnyOrder(2, 4);
+    PAssert.that(output).containsInAnyOrder(2, 4);
 
     final PipelineResult result = testPipeline.run();
     result.waitUntilFinish();
