e457e04e4b1650563edd57ac43a9829e51e5ea4e
David Hrbacek <david.hrbacek@firma.seznam.cz>
2018-10-25 09:32:13 +0200

    LROB-86: Revert GroupCombineFunctions

--- runners/spark/src/main/java/org/apache/beam/runners/spark/translation/GroupCombineFunctions.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/translation/GroupCombineFunctions.java
@@ -100,9 +100,9 @@ public class GroupCombineFunctions {
     //---- InputT: I
     JavaRDD<byte[]> inputRDDBytes = rdd.map(CoderHelpers.toByteFunction(wviCoder));
 
-//    if (inputRDDBytes.isEmpty()) {
-//      return Optional.absent();
-//    }
+    if (inputRDDBytes.isEmpty()) {
+      return Optional.absent();
+    }
 
     /*Itr<WV<A>>*/
     /*Itr<WV<A>>>*/
