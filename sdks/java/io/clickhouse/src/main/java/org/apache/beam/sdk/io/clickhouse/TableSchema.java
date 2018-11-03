77038aabe1ee90071c88193b03524adbf7760049
Gleb Kanterov <gleb@spotify.com>
2018-11-03 18:33:39 +0100

    ClickHouseIO: basic parsing of table schema

---
+++ sdks/java/io/clickhouse/src/main/java/org/apache/beam/sdk/io/clickhouse/TableSchema.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.io.clickhouse;
+
+import com.google.auto.value.AutoValue;
+import java.io.Serializable;
+import java.util.List;
+import org.apache.beam.sdk.annotations.Experimental;
+
+/** A descriptor for ClickHouse table schema. */
+@Experimental(Experimental.Kind.SCHEMAS)
+public abstract class TableSchema implements Serializable {
+
+  abstract List<Column> columns();
+
+  /** A column in ClickHouse table. */
+  public abstract static class Column implements Serializable {
+    abstract String name();
+
+    abstract ColumnType columnType();
+  }
+
+  /** An enumeration of possible types in ClickHouse. */
+  public enum TypeName {
+    DATE,
+    DATETIME,
+    FLOAT32,
+    FLOAT64,
+    INT16,
+    INT32,
+    INT64,
+    INT8,
+    STRING,
+    UINT16,
+    UINT32,
+    UINT64,
+    UINT8
+  }
+
+  /** A descriptor for a column type. */
+  @AutoValue
+  public abstract static class ColumnType implements Serializable {
+    public static final ColumnType DATE = ColumnType.of(TypeName.DATE);
+    public static final ColumnType DATETIME = ColumnType.of(TypeName.DATETIME);
+    public static final ColumnType FLOAT32 = ColumnType.of(TypeName.FLOAT32);
+    public static final ColumnType FLOAT64 = ColumnType.of(TypeName.FLOAT64);
+    public static final ColumnType INT16 = ColumnType.of(TypeName.INT16);
+    public static final ColumnType INT32 = ColumnType.of(TypeName.INT32);
+    public static final ColumnType INT64 = ColumnType.of(TypeName.INT64);
+    public static final ColumnType INT8 = ColumnType.of(TypeName.INT8);
+    public static final ColumnType STRING = ColumnType.of(TypeName.STRING);
+    public static final ColumnType UINT16 = ColumnType.of(TypeName.UINT16);
+    public static final ColumnType UINT32 = ColumnType.of(TypeName.UINT32);
+    public static final ColumnType UINT64 = ColumnType.of(TypeName.UINT64);
+    public static final ColumnType UINT8 = ColumnType.of(TypeName.UINT8);
+
+    public abstract TypeName typeName();
+
+    public static ColumnType of(TypeName typeName) {
+      return new AutoValue_TableSchema_ColumnType(typeName);
+    }
+
+    public static ColumnType parse(String str) {
+      return ColumnType.of(TypeName.valueOf(str.toUpperCase()));
+    }
+  }
+}
