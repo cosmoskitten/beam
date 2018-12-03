d5991678ef473107fc06580299d9aa4ae98960f4
Ryan Williams <ryan.blake.williams@gmail.com>
2018-12-03 01:33:52 +0000

    lint

--- runners/flink/src/main/java/org/apache/beam/runners/flink/metrics/FileReporter.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/metrics/FileReporter.java
@@ -40,16 +40,20 @@ public class FileReporter extends AbstractReporter {
 
   @Override
   public void open(MetricConfig config) {
-    file = config.getString("file", null);
-    log.info("Opening file: {}", file);
-    if (file == null) {
-      throw new IllegalStateException("FileReporter metrics config needs 'file' key");
-    }
-    try {
-      FileOutputStream fos = new FileOutputStream(file);
-      ps = new PrintStream(fos);
-    } catch (FileNotFoundException e) {
-      throw new IllegalStateException("FileReporter couldn't open file", e);
+    synchronized (this) {
+      if (file == null) {
+        file = config.getString("file", null);
+        log.info("Opening file: {}", file);
+        if (file == null) {
+          throw new IllegalStateException("FileReporter metrics config needs 'file' key");
+        }
+        try {
+          FileOutputStream fos = new FileOutputStream(file);
+          ps = new PrintStream(fos);
+        } catch (FileNotFoundException e) {
+          throw new IllegalStateException("FileReporter couldn't open file", e);
+        }
+      }
     }
   }
 
@@ -58,7 +62,7 @@ public class FileReporter extends AbstractReporter {
     final String name = group.getMetricIdentifier(metricName, this);
     super.notifyOfRemovedMetric(metric, metricName, group);
     synchronized (this) {
-      ps.printf("%s: %s\n", name, Metrics.toString(metric));
+      ps.printf("%s: %s%n", name, Metrics.toString(metric));
     }
   }
 
