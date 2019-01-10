7d49a99c8ca4d55863b58d8fe28d71c7490b0bf2
Łukasz Gajowy <lukasz.gajowy@gmail.com>
2019-01-10 16:12:58 +0100

    [BEAM-6290] Use NamedTestResult instead of LoadTestResult (more generic)

--- sdks/java/testing/load-tests/src/main/java/org/apache/beam/sdk/loadtests/LoadTest.java
+++ sdks/java/testing/load-tests/src/main/java/org/apache/beam/sdk/loadtests/LoadTest.java
@@ -19,10 +19,13 @@ package org.apache.beam.sdk.loadtests;
 
 import static org.apache.beam.sdk.io.synthetic.SyntheticOptions.fromJsonString;
 
+import com.google.cloud.Timestamp;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMap;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.Read;
@@ -31,6 +34,9 @@ import org.apache.beam.sdk.io.synthetic.SyntheticSourceOptions;
 import org.apache.beam.sdk.io.synthetic.SyntheticStep;
 import org.apache.beam.sdk.io.synthetic.SyntheticUnboundedSource;
 import org.apache.beam.sdk.loadtests.metrics.TimeMonitor;
+import org.apache.beam.sdk.testutils.NamedTestResult;
+import org.apache.beam.sdk.testutils.TestResult;
+import org.apache.beam.sdk.testutils.metrics.MetricsReader;
 import org.apache.beam.sdk.testutils.publishing.BigQueryResultsPublisher;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
@@ -81,36 +87,46 @@ abstract class LoadTest<OptionsT extends LoadTestOptions> {
    * Runs the load test, collects and publishes test results to various data store and/or console.
    */
   public PipelineResult run() throws IOException {
-    long testStartTime = System.currentTimeMillis();
+    Timestamp timestamp = Timestamp.now();
 
     loadTest();
 
     PipelineResult result = pipeline.run();
     result.waitUntilFinish();
 
-    LoadTestResult testResult = LoadTestResult.create(result, metricsNamespace, testStartTime);
+    String testId = UUID.randomUUID().toString();
+    List metrics = readMetrics(timestamp, result, testId);
 
-    ConsoleResultPublisher.publish(testResult);
+    ConsoleResultPublisher.publish(metrics, testId, timestamp.toString());
 
     if (options.getPublishToBigQuery()) {
-      publishResultToBigQuery(testResult);
+      publishResultsToBigQuery(metrics);
     }
     return result;
   }
 
-  private void publishResultToBigQuery(LoadTestResult testResult) {
+  private List<NamedTestResult> readMetrics(Timestamp timestamp, PipelineResult result, String testId) {
+    MetricsReader reader = new MetricsReader(result, metricsNamespace);
+
+    NamedTestResult runtime = NamedTestResult
+        .create(testId, timestamp.toString(), "runtime_sec",
+            (reader.getEndTimeMetric("runtime") - reader.getStartTimeMetric("runtime")) / 1000D);
+
+    NamedTestResult totalBytes = NamedTestResult
+        .create(testId, timestamp.toString(), "total_bytes_count",
+            reader.getCounterMetric("totalBytes.count"));
+
+    return Arrays.asList(runtime, totalBytes);
+  }
+
+  private void publishResultsToBigQuery(List<TestResult> testResults) {
     String dataset = options.getBigQueryDataset();
     String table = options.getBigQueryTable();
     checkBigQueryOptions(dataset, table);
 
-    ImmutableMap<String, String> schema =
-        ImmutableMap.<String, String>builder()
-            .put("timestamp", "timestamp")
-            .put("runtime", "float")
-            .put("total_bytes_count", "integer")
-            .build();
-
-    BigQueryResultsPublisher.create(dataset, schema).publish(testResult, table);
+    BigQueryResultsPublisher
+        .create(dataset, NamedTestResult.getSchema())
+        .publish(testResults, table);
   }
 
   private static void checkBigQueryOptions(String dataset, String table) {
