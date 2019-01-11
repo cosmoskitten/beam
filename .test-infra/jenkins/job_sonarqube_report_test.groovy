bdf806188ab9daae2045da553c7deac7660b45e8
Mikhail Gryzykhin <migryz@google.com>
2019-01-10 16:28:24 -0800

    disable jenkins analyzer

--- .test-infra/jenkins/job_sonarqube_report_test.groovy
+++ .test-infra/jenkins/job_sonarqube_report_test.groovy
@@ -41,17 +41,18 @@ job('beam_sonarqube_report_test') {
     archiveJunit('**/build/test-results/**/*.xml')
   }
   
-  publishers {
-    sonar {
-      branch('AddCoverage')
-    }
-  }
+  // publishers {
+  //   sonar {
+  //     branch('AddCoverage')
+  //   }
+  // }
 
   steps {
     gradle {
       rootBuildScriptDir(commonJobProperties.checkoutDir)
       tasks(":beam-runners-google-cloud-dataflow-java-fn-api-worker:jacocoTestReport")
       tasks(":beam-runners-google-cloud-dataflow-java-fn-api-worker:test")
+      tasks(":beam-runners-google-cloud-dataflow-java-fn-api-worker:sonarqube")
     }
   }
 }
