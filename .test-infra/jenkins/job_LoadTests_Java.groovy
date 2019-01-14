ae79538f16c4c1ceb1424c8ac351d4021060dd1f
Łukasz Gajowy <lukasz.gajowy@gmail.com>
2019-01-14 14:32:27 +0100

    [BEAM-6351] Add Dataflow smoke load test

--- .test-infra/jenkins/job_LoadTests_Java.groovy
+++ .test-infra/jenkins/job_LoadTests_Java.groovy
@@ -71,6 +71,20 @@ def smokeTestConfigurations = [
                         fanout           : 10,
                         iterations       : 1,
                 ]
+        ],
+        [
+                title        : 'GroupByKey load test Dataflow',
+                itClass      : 'org.apache.beam.sdk.loadtests.GroupByKeyLoadTest',
+                runner       : CommonTestProperties.Runner.DATAFLOW,
+                jobProperties: [
+                        publishToBigQuery: true,
+                        bigQueryDataset  : 'load_test_SMOKE',
+                        bigQueryTable    : 'dataflow_gbk',
+                        sourceOptions    : '{"numRecords":100000,"splitPointFrequencyRecords":1}',
+                        stepOptions      : '{"outputRecordsPerInputRecord":1,"preservesInputKeyDistribution":true}',
+                        fanout           : 10,
+                        iterations       : 1,
+                ]
         ]
 ]
 
