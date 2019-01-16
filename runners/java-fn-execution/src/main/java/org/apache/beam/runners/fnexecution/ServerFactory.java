d35d680798437740dccc2db86c561d6be80b212c
Alex Amato <ajamato@google.com>
2019-01-16 10:02:53 -0800

    rebased

--- runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/ServerFactory.java
+++ runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/ServerFactory.java
@@ -142,7 +142,6 @@ public abstract class ServerFactory {
       return createServer(services, (InetSocketAddress) socketAddress);
     }
 
-
     private static Server createServer(List<BindableService> services, InetSocketAddress socket)
         throws IOException {
       NettyServerBuilder builder =
