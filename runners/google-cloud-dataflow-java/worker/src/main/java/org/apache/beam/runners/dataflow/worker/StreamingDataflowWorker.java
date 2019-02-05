557ba7b98fd69b9677967c53050731b04a24b9d4
David Rieber <drieber@google.com>
2019-02-05 13:52:24 -0800

    Temporary, needs more work.

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/StreamingDataflowWorker.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/StreamingDataflowWorker.java
@@ -1527,9 +1527,9 @@ public class StreamingDataflowWorker {
         mapTask.setStageName(computationConfig.getStageName());
         mapTask.setInstructions(computationConfig.getInstructions());
         // TODO:XXX Pass computationConfig.getTransformUserNameToStateFamily()
-        // addComputation(
-        //        computationConfig.getComputationId(), mapTask, computationConfig.getTransformUserNameToStateFamily());
-        addComputation(computationConfig.getComputationId(), mapTask, ImmutableMap.of());
+        addComputation(
+                computationConfig.getComputationId(), mapTask, computationConfig.getTransformUserNameToStateFamily());
+        // addComputation(computationConfig.getComputationId(), mapTask, ImmutableMap.of());
       }
     }
 
