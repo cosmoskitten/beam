8d3b6e9d3322879e9be06ed755471e5b26439cd4
Heejong Lee <heejong@gmail.com>
2019-02-08 14:18:59 -0800

    [BEAM-6638] Python ExternalTransform output mismatched

--- sdks/python/apache_beam/transforms/external.py
+++ sdks/python/apache_beam/transforms/external.py
@@ -203,6 +203,15 @@ class ExternalTransform(ptransform.PTransform):
                    for tag, pcoll in proto.outputs.items()})
       context.transforms.put_proto(id, new_proto)
 
+    self._expanded_transform = beam_runner_api_pb2.PTransform(
+        unique_name=full_label,
+        spec=self._expanded_transform.spec,
+        subtransforms=self._expanded_transform.subtransforms,
+        inputs=self._expanded_transform.inputs,
+        outputs={
+            tag: pcoll_renames.get(pcoll, pcoll)
+            for tag, pcoll in self._expanded_transform.outputs.items()})
+
     return self._expanded_transform
 
 
