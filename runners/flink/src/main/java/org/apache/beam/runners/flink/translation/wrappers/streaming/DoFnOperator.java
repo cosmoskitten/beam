3f90d20f0f344656435de5887f07c691885e37ff
Maximilian Michels <mxm@apache.org>
2019-02-13 16:27:22 +0100

    [BEAM-6650] Convert FlinkKeyGroupStateInternals to using KeyedStateFunction
    The previous implementation was not used anymore which caused elements emitted
    during finalization of the bundle to be excluded from the checkpoint. This
    resulted in lost elements.
    
    The new implementation uses keyed state which is automatically checkpointed. The
    KeyedStateFunction is applied on the state backend to extract and flush all the
    elements after a restore.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/DoFnOperator.java
@@ -173,8 +173,6 @@ public class DoFnOperator<InputT, OutputT> extends AbstractStreamOperator<Window
 
   protected transient FlinkTimerInternals timerInternals;
 
-  private transient StateInternals nonKeyedStateInternals;
-
   private transient long pushedBackWatermark;
 
   private transient PushedBackElementsHandler<WindowedValue<InputT>> pushedBackElementsHandler;
@@ -300,16 +298,6 @@ public class DoFnOperator<InputT, OutputT> extends AbstractStreamOperator<Window
 
     sideInputReader = NullSideInputReader.of(sideInputs);
 
-    // maybe init by initializeState
-    if (nonKeyedStateInternals == null) {
-      if (keyCoder != null) {
-        nonKeyedStateInternals =
-            new FlinkKeyGroupStateInternals<>(keyCoder, getKeyedStateBackend());
-      } else {
-        nonKeyedStateInternals = new FlinkSplitStateInternals<>(getOperatorStateBackend());
-      }
-    }
-
     if (!sideInputs.isEmpty()) {
 
       FlinkBroadcastStateInternals sideInputStateInternals =
@@ -327,7 +315,14 @@ public class DoFnOperator<InputT, OutputT> extends AbstractStreamOperator<Window
       setPushedBackWatermark(Long.MAX_VALUE);
     }
 
-    outputManager = outputManagerFactory.create(output, nonKeyedStateInternals);
+    final StateInternals outputManagerStateInternals;
+    if (keyCoder != null) {
+      outputManagerStateInternals =
+          new FlinkKeyGroupStateInternals<>(keyCoder, getKeyedStateBackend());
+    } else {
+      outputManagerStateInternals = new FlinkSplitStateInternals<>(getOperatorStateBackend());
+    }
+    outputManager = outputManagerFactory.create(output, outputManagerStateInternals);
 
     // StatefulPardo or WindowDoFn
     if (keyCoder != null) {
@@ -441,7 +436,7 @@ public class DoFnOperator<InputT, OutputT> extends AbstractStreamOperator<Window
     }
 
     // sanity check: these should have been flushed out by +Inf watermarks
-    if (!sideInputs.isEmpty() && nonKeyedStateInternals != null) {
+    if (!sideInputs.isEmpty()) {
 
       List<WindowedValue<InputT>> pushedBackElements =
           pushedBackElementsHandler.getElements().collect(Collectors.toList());
