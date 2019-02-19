08c8c0ff13d69a1f941aa3b925bcaf2c1264838e
Ryan Williams <ryan.blake.williams@gmail.com>
2019-02-19 06:57:29 +0000

    MN doc: URN wrapper

--- sdks/java/core/src/main/java/org/apache/beam/sdk/metrics/MetricName.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/metrics/MetricName.java
@@ -26,9 +26,14 @@ import org.apache.beam.sdk.annotations.Experimental.Kind;
 import org.apache.beam.vendor.guava.v20_0.com.google.common.base.Strings;
 
 /**
- * The name of a metric consists of a {@link #getNamespace} and a {@link #getName}. The {@link
- * #getNamespace} allows grouping related metrics together and also prevents collisions between
- * multiple metrics with the same name.
+ * Wrapper for {@link org.apache.beam.model.fnexecution.v1.BeamFnApi.MonitoringInfo} URN.
+ *
+ * <p>"User" metrics (URN {@link MetricUrns#USER_METRIC_URN_PREFIX}) are defined by a "namespace"
+ * and "name", and are the most commonly dealt with by user code, so structured constructors and
+ * accessors are provided in terms of those strings.
+ *
+ * <p>The {@link #namespace} allows grouping related metrics together and also prevents collisions
+ * between multiple metrics with the same name.
  */
 @Experimental(Kind.METRICS)
 @AutoValue
