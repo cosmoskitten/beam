97003a1183ede10111bd030bf19564e53f3ad383
manuzhang <owenzhang1990@gmail.com>
2019-02-26 06:54:11 +0800

    Fix Nexmark settings of Gearpump Runner

--- .test-infra/jenkins/job_PostCommit_Java_Nexmark_Gearpump.groovy
+++ .test-infra/jenkins/job_PostCommit_Java_Nexmark_Gearpump.groovy
@@ -18,6 +18,7 @@
 
 import CommonJobProperties as commonJobProperties
 import CommonTestProperties.Runner
+import CommonTestProperties.TriggeringContext
 import NexmarkBigqueryProperties
 import NexmarkBuilder as Nexmark
 import NoPhraseTriggeringPostCommitBuilder
@@ -32,40 +33,21 @@ NoPhraseTriggeringPostCommitBuilder.postCommitJob('beam_PostCommit_Java_Nexmark_
   commonJobProperties.setTopLevelMainJobProperties(delegate, 'master', 240)
 
   // Gradle goals for this job.
-  steps {
-    shell('echo *** RUN NEXMARK IN STREAMING MODE USING GEARPUMP RUNNER ***')
-    gradle {
-      rootBuildScriptDir(commonJobProperties.checkoutDir)
-      tasks(':beam-sdks-java-nexmark:run')
-      commonJobProperties.setGradleSwitches(delegate)
-      switches('-Pnexmark.runner=":beam-runners-gearpump"' +
-              ' -Pnexmark.args="' +
-              [NexmarkBigqueryProperties.nexmarkBigQueryArgs,
-              '--runner=TestGearpumpRunner',
-              '--streaming=true',
-              '--suite=SMOKE',
-              '--streamTimeout=60' ,
-              '--manageResources=false',
-              '--monitorJobs=true'].join(' '))
-    }
-  }
+  Nexmark.streamingOnlyJob(delegate, Runner.GEARPUMP, JOB_SPECIFIC_OPTIONS, 
+    Nexmark.TriggeringContext.POST_COMMIT)
 }
 
-PhraseTriggeringPostCommitBuilder.postCommitJob('beam_PostCommit_Java_Nexmark_Gearpump', 'Run Gearpump Nexmark Tests',
-        'Gearpump Runner Nexmark Tests', this) {
+PhraseTriggeringPostCommitBuilder.postCommitJob('beam_PostCommit_Java_Nexmark_Gearpump', 
+  'Run Gearpump Runner Nexmark Tests', 'Gearpump Runner Nexmark Tests', this) {
   description('Runs the Nexmark suite on the Gearpump runner against a Pull Request, on demand.')
 
   // Set common parameters.
   commonJobProperties.setTopLevelMainJobProperties(delegate, 'master', 240)
 
-  def final JOB_SPECIFIC_OPTIONS = [
-          'suite' : 'STRESS',
-          'numWorkers' : 4,
-          'maxNumWorkers' : 4,
-          'autoscalingAlgorithm' : 'NONE',
-          'nexmarkParallel' : 16,
-          'enforceEncodability' : true,
-          'enforceImmutability' : true
-  ]
   Nexmark.streamingOnlyJob(delegate, Runner.GEARPUMP, JOB_SPECIFIC_OPTIONS, Nexmark.TriggeringContext.PR)
 }
+
+def final JOB_SPECIFIC_OPTIONS = [
+        'suite' : 'SMOKE',
+        'streamTimeout' : 60
+]
