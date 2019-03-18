319f6545f3aa6eacf802cf836f791a1c1698b619
Kyle Weaver <kcweaver@google.com>
2019-03-18 15:44:38 -0700

    share non-Flink-specific pipeline helper utils

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/utils/FlinkPipelineTranslatorUtils.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/utils/FlinkPipelineTranslatorUtils.java
@@ -19,33 +19,14 @@ package org.apache.beam.runners.flink.translation.utils;
 
 import java.io.IOException;
 import org.apache.beam.model.pipeline.v1.RunnerApi;
-import org.apache.beam.runners.core.construction.RehydratedComponents;
-import org.apache.beam.runners.core.construction.WindowingStrategyTranslation;
 import org.apache.beam.runners.core.construction.graph.PipelineNode;
 import org.apache.beam.runners.fnexecution.wire.WireCoders;
 import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.util.WindowedValue;
-import org.apache.beam.sdk.values.WindowingStrategy;
-import org.apache.beam.vendor.grpc.v1p13p1.com.google.protobuf.InvalidProtocolBufferException;
-import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.BiMap;
-import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.ImmutableBiMap;
-import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.Sets;
 
 /** Utilities for pipeline translation. */
 public final class FlinkPipelineTranslatorUtils {
   private FlinkPipelineTranslatorUtils() {}
-
-  /** Creates a mapping from PCollection id to output tag integer. */
-  public static BiMap<String, Integer> createOutputMap(Iterable<String> localOutputs) {
-    ImmutableBiMap.Builder<String, Integer> builder = ImmutableBiMap.builder();
-    int outputIndex = 0;
-    // sort localOutputs for stable indexing
-    for (String tag : Sets.newTreeSet(localOutputs)) {
-      builder.put(tag, outputIndex);
-      outputIndex++;
-    }
-    return builder.build();
-  }
 
   /** Creates a coder for a given PCollection id from the Proto definition. */
   public static <T> Coder<WindowedValue<T>> instantiateCoder(
@@ -56,25 +37,6 @@ public final class FlinkPipelineTranslatorUtils {
       return WireCoders.instantiateRunnerWireCoder(collectionNode, components);
     } catch (IOException e) {
       throw new RuntimeException("Could not instantiate Coder", e);
-    }
-  }
-
-  public static WindowingStrategy getWindowingStrategy(
-      String pCollectionId, RunnerApi.Components components) {
-    RunnerApi.WindowingStrategy windowingStrategyProto =
-        components.getWindowingStrategiesOrThrow(
-            components.getPcollectionsOrThrow(pCollectionId).getWindowingStrategyId());
-
-    final WindowingStrategy<?, ?> windowingStrategy;
-    try {
-      return WindowingStrategyTranslation.fromProto(
-          windowingStrategyProto, RehydratedComponents.forComponents(components));
-    } catch (InvalidProtocolBufferException e) {
-      throw new IllegalStateException(
-          String.format(
-              "Unable to hydrate windowing strategy %s for %s.",
-              windowingStrategyProto, pCollectionId),
-          e);
     }
   }
 }
