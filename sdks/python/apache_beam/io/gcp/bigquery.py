7c137e8c93b53dc4eac729097760833313637e5c
Pablo Estrada <pabloem@apache.org>
2019-03-25 14:58:48 -0700

    Adding support for auto-creating buckets for BigQuery file loads

--- sdks/python/apache_beam/io/gcp/bigquery.py
+++ sdks/python/apache_beam/io/gcp/bigquery.py
@@ -674,7 +674,8 @@ class BigQueryWriteFn(DoFn):
         table_reference.datasetId,
         table_reference.tableId,
         table_schema,
-        self.create_disposition, self.write_disposition)
+        self.create_disposition, self.write_disposition,
+        kms_key=self.kms_key)
     self._observed_tables.add(str_table_reference)
 
   def process(self, element, unused_create_fn_output=None):
@@ -995,6 +996,7 @@ bigquery_v2_messages.TableSchema):
                   max_files_per_bundle=self.max_files_per_bundle,
                   custom_gcs_temp_location=self.custom_gcs_temp_location,
                   test_client=self.test_client,
+                  kms_key=self.kms_key,
                   validate=self._validate))
 
   def display_data(self):
