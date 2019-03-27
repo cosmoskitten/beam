3621e4620b2d24cb5c94800c37a7a9e034af6e21
Michal Walenia <michal.walenia@polidea.com>
2019-03-27 09:48:07 +0100

    [BEAM-6627] Add metric reporting to JdbcIOIT

--- .test-infra/jenkins/job_PerformanceTests_JDBC.groovy
+++ .test-infra/jenkins/job_PerformanceTests_JDBC.groovy
@@ -39,7 +39,9 @@ job(jobName) {
             tempRoot       : 'gs://temp-storage-for-perf-tests',
             project        : 'apache-beam-testing',
             postgresPort   : '5432',
-            numberOfRecords: '5000000'
+            numberOfRecords: '5000000',
+            bigQueryDataset: 'beam_performance',
+            bigQueryTable  : 'test_jdbc_it',
     ]
 
     String namespace = commonJobProperties.getKubernetesNamespace(jobName)
