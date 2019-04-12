82e8ae6c6dbc8a5ab4447016152e278b9359adc4
Maximilian Michels <mxm@apache.org>
2019-04-12 16:43:54 +0200

    [BEAM-7029] Add KafkaIO.Write as an external transform
    This adds KafkaIO.Write as an external transform and includes a Python
    wrapper (WriteToKafka) for convenience.

--- sdks/python/apache_beam/io/external/kafka.py
+++ sdks/python/apache_beam/io/external/kafka.py
@@ -102,13 +102,13 @@ class ReadFromKafka(ptransform.PTransform):
 
     args = {
         'consumer_config':
-            ReadFromKafka._encode_map(self.consumer_config),
+            _encode_map(self.consumer_config),
         'topics':
-            ReadFromKafka._encode_list(self.topics),
+            _encode_list(self.topics),
         'key_deserializer':
-            ReadFromKafka._encode_str(self.key_deserializer),
+            _encode_str(self.key_deserializer),
         'value_deserializer':
-            ReadFromKafka._encode_str(self.value_deserializer),
+            _encode_str(self.value_deserializer),
     }
 
     payload = ExternalConfigurationPayload(configuration=args)
@@ -118,35 +118,112 @@ class ReadFromKafka(ptransform.PTransform):
             payload.SerializeToString(),
             self.expansion_service))
 
-  @staticmethod
-  def _encode_map(dict_obj):
-    kv_list = [(key.encode('utf-8'), val.encode('utf-8'))
-               for key, val in dict_obj.items()]
-    coder = IterableCoder(TupleCoder(
-        [LengthPrefixCoder(BytesCoder()), LengthPrefixCoder(BytesCoder())]))
-    coder_urns = ['beam:coder:iterable:v1',
-                  'beam:coder:kv:v1',
-                  'beam:coder:bytes:v1',
-                  'beam:coder:bytes:v1']
-    return ConfigValue(
-        coder_urn=coder_urns,
-        payload=coder.encode(kv_list))
 
-  @staticmethod
-  def _encode_list(list_obj):
-    encoded_list = [val.encode('utf-8') for val in list_obj]
-    coder = IterableCoder(LengthPrefixCoder(BytesCoder()))
-    coder_urns = ['beam:coder:iterable:v1',
-                  'beam:coder:bytes:v1']
-    return ConfigValue(
-        coder_urn=coder_urns,
-        payload=coder.encode(encoded_list))
+class WriteToKafka(ptransform.PTransform):
+  """
+    An external PTransform which writes KV data to a specified Kafka topic.
+    If no Kafka Serializer for key/value is provided, then key/value are
+    assumed to be byte arrays.
 
-  @staticmethod
-  def _encode_str(str_obj):
-    encoded_str = str_obj.encode('utf-8')
-    coder = LengthPrefixCoder(BytesCoder())
-    coder_urns = ['beam:coder:bytes:v1']
-    return ConfigValue(
-        coder_urn=coder_urns,
-        payload=coder.encode(encoded_str))
+    Note: To use this transform, you need to start the Java expansion service.
+    Please refer to the portability documentation on how to do that. The
+    expansion service address has to be provided when instantiating this
+    transform. During pipeline translation this transform will be replaced by
+    the Java SDK's KafkaIO.
+
+    If you start Flink's job server, the expansion service will be started on
+    port 8097. This is also the configured default for this transform. For a
+    different address, please set the expansion_service parameter.
+
+    For more information see:
+    - https://beam.apache.org/documentation/runners/flink/
+    - https://beam.apache.org/roadmap/portability/
+  """
+
+  # Default serializer which passes raw bytes to Kafka
+  byte_array_serializer = 'org.apache.kafka.common.serialization.' \
+                          'ByteArraySerializer'
+
+  def __init__(self, producer_config,
+               topic,
+               key_serializer=byte_array_serializer,
+               value_serializer=byte_array_serializer,
+               expansion_service='localhost:8097'):
+    """
+    Initializes a write operation to Kafka.
+
+    :param consumer_config: A dictionary containing the producer configuration.
+    :param topic: A Kafka topic name.
+    :param key_deserializer: A fully-qualified Java class name of a Kafka
+                             Serializer for the topic's key, e.g.
+                             'org.apache.kafka.common.
+                             serialization.LongSerializer'.
+                             Default: 'org.apache.kafka.common.
+                             serialization.ByteArraySerializer'.
+    :param value_deserializer: A fully-qualified Java class name of a Kafka
+                               Serializer for the topic's value, e.g.
+                               'org.apache.kafka.common.
+                               serialization.LongSerializer'.
+                               Default: 'org.apache.kafka.common.
+                               serialization.ByteArraySerializer'.
+    :param expansion_service: The address (host:port) of the ExpansionService.
+    """
+    super(WriteToKafka, self).__init__()
+    self._urn = 'beam:external:java:kafka:write:v1'
+    self.producer_config = producer_config
+    self.topic = topic
+    self.key_serializer = key_serializer
+    self.value_serializer = value_serializer
+    self.expansion_service = expansion_service
+
+  def expand(self, pvalue):
+    args = {
+        'producer_config':
+            _encode_map(self.producer_config),
+        'topic':
+            _encode_str(self.topic),
+        'key_serializer':
+            _encode_str(self.key_serializer),
+        'value_serializer':
+            _encode_str(self.value_serializer),
+    }
+
+    payload = ExternalConfigurationPayload(configuration=args)
+    return pvalue.apply(
+        ExternalTransform(
+            self._urn,
+            payload.SerializeToString(),
+            self.expansion_service))
+
+
+def _encode_map(dict_obj):
+  kv_list = [(key.encode('utf-8'), val.encode('utf-8'))
+             for key, val in dict_obj.items()]
+  coder = IterableCoder(TupleCoder(
+      [LengthPrefixCoder(BytesCoder()), LengthPrefixCoder(BytesCoder())]))
+  coder_urns = ['beam:coder:iterable:v1',
+                'beam:coder:kv:v1',
+                'beam:coder:bytes:v1',
+                'beam:coder:bytes:v1']
+  return ConfigValue(
+      coder_urn=coder_urns,
+      payload=coder.encode(kv_list))
+
+
+def _encode_list(list_obj):
+  encoded_list = [val.encode('utf-8') for val in list_obj]
+  coder = IterableCoder(LengthPrefixCoder(BytesCoder()))
+  coder_urns = ['beam:coder:iterable:v1',
+                'beam:coder:bytes:v1']
+  return ConfigValue(
+      coder_urn=coder_urns,
+      payload=coder.encode(encoded_list))
+
+
+def _encode_str(str_obj):
+  encoded_str = str_obj.encode('utf-8')
+  coder = LengthPrefixCoder(BytesCoder())
+  coder_urns = ['beam:coder:bytes:v1']
+  return ConfigValue(
+      coder_urn=coder_urns,
+      payload=coder.encode(encoded_str))
