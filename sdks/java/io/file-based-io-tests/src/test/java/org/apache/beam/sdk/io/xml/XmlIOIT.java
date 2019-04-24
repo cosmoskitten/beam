a40059fe92daecfcc5e14d5eb30ab750e68d0843
Michal Walenia <michal.walenia@polidea.com>
2019-04-24 14:26:05 +0200

    [BEAM-6627] Add byte and element count metric to XmlIOIT

--- sdks/java/io/file-based-io-tests/src/test/java/org/apache/beam/sdk/io/xml/XmlIOIT.java
+++ sdks/java/io/file-based-io-tests/src/test/java/org/apache/beam/sdk/io/xml/XmlIOIT.java
@@ -42,6 +42,8 @@ import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.testutils.NamedTestResult;
+import org.apache.beam.sdk.testutils.metrics.ByteMonitor;
+import org.apache.beam.sdk.testutils.metrics.CountMonitor;
 import org.apache.beam.sdk.testutils.metrics.IOITMetrics;
 import org.apache.beam.sdk.testutils.metrics.MetricsReader;
 import org.apache.beam.sdk.testutils.metrics.TimeMonitor;
@@ -129,6 +131,11 @@ public class XmlIOIT {
                 "Gather write start time",
                 ParDo.of(new TimeMonitor<>(XMLIOIT_NAMESPACE, "writeStart")))
             .apply(
+                "Gather byte count", ParDo.of(new ByteMonitor<>(XMLIOIT_NAMESPACE, "totalBytes")))
+            .apply(
+                "Gather element count",
+                ParDo.of(new CountMonitor<>(XMLIOIT_NAMESPACE, "elementCount")))
+            .apply(
                 "Write xml files",
                 FileIO.<Bird>write()
                     .via(XmlIO.sink(Bird.class).withRootElement("birds").withCharset(charset))
@@ -211,6 +218,19 @@ public class XmlIOIT {
           double runTime = (readEnd - writeStart) / 1e3;
           return NamedTestResult.create(uuid, timestamp, "run_time", runTime);
         });
+
+    suppliers.add(
+        reader -> {
+          double totalBytes = reader.getCounterMetric("totalBytes");
+          return NamedTestResult.create(uuid, timestamp, "total_bytes", totalBytes);
+        });
+
+    suppliers.add(
+        reader -> {
+          double totalBytes = reader.getCounterMetric("elementCount");
+          return NamedTestResult.create(uuid, timestamp, "element_count", totalBytes);
+        });
+
     return suppliers;
   }
 
