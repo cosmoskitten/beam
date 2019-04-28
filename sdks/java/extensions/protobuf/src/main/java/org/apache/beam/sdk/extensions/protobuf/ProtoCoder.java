0402c5b4ad95cd1a3f6aed01423c4485c9b4cf0d
Alex Van Boxel <alex@vanboxel.be>
2019-04-28 13:16:23 +0200

    [BEAM-5967] Add handling of DynamicMessage in ProtoCoder
    The ProtoCoder was unable to handle DynamicMessage as it was unable to
    get a message specific parser. The Coder is expanded to handle
    DynamicMessage as a special case. It stores the complete descriptor set
    when (de)serializing.
    
    Design decision: Although DynamicMessage could in theory have a
    different schema per message in a single stream this doesn't make sense.
    The common use-case for using DynamicMessages is when the schema is not
    known at compile type, but is known at pipeline build time (example,
    like pulled from a schema registry).
    
    With this restriction we only need to store the schema (or descriptor)
    once when the pipeline is serialized and send to the workers.

--- sdks/java/extensions/protobuf/src/main/java/org/apache/beam/sdk/extensions/protobuf/ProtoCoder.java
+++ sdks/java/extensions/protobuf/src/main/java/org/apache/beam/sdk/extensions/protobuf/ProtoCoder.java
@@ -19,11 +19,15 @@ package org.apache.beam.sdk.extensions.protobuf;
 
 import static org.apache.beam.vendor.guava.v20_0.com.google.common.base.Preconditions.checkArgument;
 
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
 import com.google.protobuf.ExtensionRegistry;
 import com.google.protobuf.Message;
 import com.google.protobuf.Parser;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -107,6 +111,8 @@ import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.Sets;
  */
 public class ProtoCoder<T extends Message> extends CustomCoder<T> {
 
+  public static final long serialVersionUID = -5043999806040629525L;
+
   /** Returns a {@link ProtoCoder} for the given Protocol Buffers {@link Message}. */
   public static <T extends Message> ProtoCoder<T> of(Class<T> protoMessageClass) {
     return new ProtoCoder<>(protoMessageClass, ImmutableSet.of());
@@ -120,6 +126,14 @@ public class ProtoCoder<T extends Message> extends CustomCoder<T> {
     @SuppressWarnings("unchecked")
     Class<T> protoMessageClass = (Class<T>) protoMessageType.getRawType();
     return of(protoMessageClass);
+  }
+
+  /**
+   * Returns a {@link ProtoCoder} for the Protocol Buffers {@link DynamicMessage} for the given
+   * {@link Descriptors.Descriptor}.
+   */
+  public static ProtoCoder<DynamicMessage> of(Descriptors.Descriptor protoMessageDescriptor) {
+    return new ProtoCoder<>(protoMessageDescriptor, ImmutableSet.of());
   }
 
   /**
@@ -269,21 +283,61 @@ public class ProtoCoder<T extends Message> extends CustomCoder<T> {
   private transient ExtensionRegistry memoizedExtensionRegistry;
   private transient Parser<T> memoizedParser;
 
+  // Descriptor used by DynamicMessage.
+  private transient Descriptors.Descriptor protoMessageDescriptor;
+
   /** Private constructor. */
   private ProtoCoder(Class<T> protoMessageClass, Set<Class<?>> extensionHostClasses) {
     this.protoMessageClass = protoMessageClass;
     this.extensionHostClasses = extensionHostClasses;
+    this.protoMessageDescriptor = null;
+  }
+
+  private ProtoCoder(
+      Descriptors.Descriptor protoMessageDescriptor, Set<Class<?>> extensionHostClasses) {
+    @SuppressWarnings("unchecked")
+    Class<T> protoMessageClass = (Class<T>) DynamicMessage.class;
+    this.protoMessageClass = protoMessageClass;
+    this.extensionHostClasses = extensionHostClasses;
+    this.protoMessageDescriptor = protoMessageDescriptor;
+  }
+
+  private void writeObject(ObjectOutputStream oos) throws IOException {
+    oos.defaultWriteObject();
+    if (DynamicMessage.class.equals(this.protoMessageClass)) {
+      if (this.protoMessageDescriptor == null) {
+        throw new RuntimeException("DynamicMessages require provider a Descriptor to the coder.");
+      }
+      ProtoDescriptorSerializer.writeObject(oos, this.protoMessageDescriptor);
+    }
+  }
+
+  private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
+    ois.defaultReadObject();
+    if (DynamicMessage.class.equals(this.protoMessageClass)) {
+      this.protoMessageDescriptor = ProtoDescriptorSerializer.readObject(ois);
+    }
   }
 
   /** Get the memoized {@link Parser}, possibly initializing it lazily. */
   private Parser<T> getParser() {
     if (memoizedParser == null) {
       try {
-        @SuppressWarnings("unchecked")
-        T protoMessageInstance = (T) protoMessageClass.getMethod("getDefaultInstance").invoke(null);
-        @SuppressWarnings("unchecked")
-        Parser<T> tParser = (Parser<T>) protoMessageInstance.getParserForType();
-        memoizedParser = tParser;
+        if (DynamicMessage.class.equals(protoMessageClass)) {
+          @SuppressWarnings("unchecked")
+          T protoMessageInstance =
+              (T) DynamicMessage.newBuilder(this.protoMessageDescriptor).build();
+          @SuppressWarnings("unchecked")
+          Parser<T> tParser = (Parser<T>) protoMessageInstance.getParserForType();
+          memoizedParser = tParser;
+        } else {
+          @SuppressWarnings("unchecked")
+          T protoMessageInstance =
+              (T) protoMessageClass.getMethod("getDefaultInstance").invoke(null);
+          @SuppressWarnings("unchecked")
+          Parser<T> tParser = (Parser<T>) protoMessageInstance.getParserForType();
+          memoizedParser = tParser;
+        }
       } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
         throw new IllegalArgumentException(e);
       }
