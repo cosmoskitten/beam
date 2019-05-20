2e4981704b9d3216e7404e56df5e27cd9c3d0e04
Yifan Zou <yifanzou@google.com>
2019-05-19 22:21:12 -0700

    fix dependency check

--- .test-infra/jenkins/dependency_check/generate_report.sh
+++ .test-infra/jenkins/dependency_check/generate_report.sh
@@ -39,7 +39,7 @@ REPORT_DESCRIPTION="
 
 
 # Virtualenv for the rest of the script to run setup
-/usr/bin/virtualenv dependency/check
+virtualenv dependency/check
 . dependency/check/bin/activate
 pip install --upgrade google-cloud-bigquery
 pip install --upgrade google-cloud-bigtable
