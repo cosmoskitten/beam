7fb51bc0e7a56de4dd35a295a86ef52bfdc36b2c
frederik2 <frederik.bode@ml6.eu>
2019-05-21 10:33:56 +0200

    fixup: [BEAM-6623] add DataflowValidatesRunner tests for python 3.6 and 3.7

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -255,6 +255,29 @@ class BeamModulePlugin implements Plugin<Project> {
     String pipelineOptions = System.getProperty('pipelineOptions', '')
   }
 
+  // Reads and contains all necessary performance test parameters for the
+  // ValidatesRunner
+  class PythonValidatesRunnerConfiguration {
+    // Fully qualified name of the test to run.
+    String tests = System.getProperty('tests')
+
+    // Attribute tag that can filter the test set.
+    String attribute = System.getProperty('attr')
+
+    // Extra test options pass to nose.
+    String[] extraTestOptions = [
+      "--nocapture",
+      "--processes=8",
+      "--process-timeout=4500"
+    ]
+
+    // Name of Cloud KMS encryption key to use in some tests.
+    String kmsKeyName = System.getProperty('kmsKeyName')
+
+    // Pipeline options to be used for pipeline invocation.
+    String pipelineOptions = System.getProperty('pipelineOptions', '')
+  }
+
   // A class defining the set of configurable properties accepted by containerImageName.
   class ContainerImageNameConfiguration {
     String root = null // Sets the docker repository root (optional).
@@ -1806,6 +1829,42 @@ class BeamModulePlugin implements Plugin<Project> {
             project.exec {
               executable 'sh'
               args '-c', ". ${project.ext.envdir}/bin/activate && ${runScriptsDir}/run_integration_test.sh ${cmdArgs}"
+            }
+          }
+        }
+      }
+
+      // Method to create the ValidatesRunnerTasks.
+      project.ext.enablePythonDataflowValidatesRunner = {
+
+        // Use the implicit it parameter of the closure to handle zero argument or one argument map calls.
+        // See: http://groovy-lang.org/closures.html#implicit-it
+        def config = it ? it as PythonValidatesRunnerConfiguration : new PythonValidatesRunnerConfiguration()
+
+        project.task('validatesRunnerBatchTests') {
+          dependsOn 'installGcpTest'
+          dependsOn 'sdist'
+
+          def dataflowWorkerJar = project(":runners:google-cloud-dataflow-java:worker:shadowJar").shadowJar.archivePath
+
+          doLast {
+            def argMap = [:]
+
+            // Build test options that configures test environment and framework
+            def testOptions = []
+            if (config.tests)
+              testOptions += "--tests=$config.tests"
+            if (config.attribute)
+              testOptions += "--attr=$config.attribute"
+            testOptions.addAll(config.extraTestOptions)
+            argMap["test_opts"] = testOptions
+            argMap["sdk_location"] = ${project.buildDir}/apache-beam.tar.gz
+            argMap["worker_jar"] = dataflowWorkerJar
+            def cmdArgs = project.mapToArgString(argMap)
+
+            exec {
+              executable 'sh'
+              args '-c', ". ${project.ext.envdir}/bin/activate && ${runScriptsDir}/run_integration_test.sh $cmdArgs"
             }
           }
         }
