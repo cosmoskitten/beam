d7b78014fcfe66db1aa678ecbdc9a5ff47a160fd
Shoaib <shoaib.zafaf@venturedive.com>
2019-05-21 17:37:08 +0500

    [BEAM-7021]: added comments on the python ToString transformation classes

--- sdks/python/apache_beam/transforms/combiners.py
+++ sdks/python/apache_beam/transforms/combiners.py
@@ -862,8 +862,16 @@ class PhasedCombineFnExecutor(object):
 
 
 class ToString(object):
+  """PTransform for converting a PCollection element, KV or PCollection Iterable to string."""
 
   class Kvs(ptransform.PTransform):
+    """
+    Transforms each element of the PCollection to a string on the key followed by the specific
+    delimiter and the value.
+
+    Args:
+      delimiter: Delimiter used to separate the key and the value to string. Default is the comma ","
+    """
 
     def __init__(self, delimiter = None, **kwargs):
       self.delimiter = delimiter or ","
@@ -872,14 +880,20 @@ class ToString(object):
       input_type = KV[Any, Any]
       output_type = List[str]
       return (
-              pcoll
-              | ('%s:KeyVaueToString' % self.label >> (core.Map(lambda x: "{}{}{}".format(x[0], self.delimiter, x[1]) ))
-                 .with_input_types(input_type)
-                 .with_output_types(output_type)
-                 )
+          pcoll
+          | ('%s:KeyVaueToString' % self.label >> (core.Map(lambda x: "{}{}{}".format(x[0], self.delimiter, x[1])))
+             .with_input_types(input_type)
+             .with_output_types(output_type)
+             )
       )
 
   class Element(ptransform.PTransform):
+    """
+    Transforms each element of the PCollection to a string.
+
+    Args:
+      delimiter: Delimiter used to separate the elements to string. Default is the comma ","
+    """
 
     def __init__(self, delimiter = None, **kwargs):
       self.delimiter = delimiter or ","
@@ -888,14 +902,20 @@ class ToString(object):
       input_type = T
       output_type = List[str]
       return (
-              pcoll
-              | ('%s:ElementToString' % self.label >> (core.Map(lambda x: str(x) ))
-                 .with_input_types(input_type)
-                 .with_output_types(output_type)
-                 )
+          pcoll
+          | ('%s:ElementToString' % self.label >> (core.Map(lambda x: str(x) ))
+             .with_input_types(input_type)
+             .with_output_types(output_type)
+             )
       )
 
   class Iterables(ptransform.PTransform):
+    """
+    Transforms each item in the iterable of the input of PCollection to a string. There is no trailing delimiter.
+
+    Args:
+      delimiter: Delimiter used to separate the iterable elements to string. Default is the comma ","
+    """
 
     def __init__(self, delimiter = None, **kwargs):
       self.delimiter = delimiter or ","
@@ -904,9 +924,9 @@ class ToString(object):
       input_type = Iterable[Any]
       output_type = List[str]
       return (
-              pcoll
-              | ('%s:IterablesToString' % self.label >> (core.Map(lambda x: self.delimiter.join(str(_x) for _x in x) ))
-                 .with_input_types(input_type)
-                 .with_output_types(output_type)
-                 )
+          pcoll
+          | ('%s:IterablesToString' % self.label >> (core.Map(lambda x: self.delimiter.join(str(_x) for _x in x)))
+             .with_input_types(input_type)
+             .with_output_types(output_type)
+             )
       )
