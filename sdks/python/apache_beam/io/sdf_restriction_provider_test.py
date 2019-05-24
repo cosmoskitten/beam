46e28fbde480ad521c92abf02f202567e84337cc
Boyuan Zhang <boyuanz@google.com>
2019-05-23 17:16:08 -0700

    adding tests

---
+++ sdks/python/apache_beam/io/sdf_restriction_provider_test.py
@@ -0,0 +1,9 @@
+"""Unit tests for the SDFRestrictionProvider module."""
+
+from __future__ import absolute_import
+
+import unittest
+from apache_beam.io.concat_source_test import RangeSource
+
+
+class SDFBoundedSourceRestirctionProviderTest(unittest.TestCase):
