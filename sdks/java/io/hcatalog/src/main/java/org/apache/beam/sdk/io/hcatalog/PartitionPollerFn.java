757fdb334b8e22d72e453d134e59199434ddaef3
Ankit Jhalaria <ajhalaria@godaddy.com>
2019-06-03 17:08:38 -0700

    cleaner interfaces for public consumption

--- sdks/java/io/hcatalog/src/main/java/org/apache/beam/sdk/io/hcatalog/PartitionPollerFn.java
+++ sdks/java/io/hcatalog/src/main/java/org/apache/beam/sdk/io/hcatalog/PartitionPollerFn.java
@@ -17,30 +17,26 @@
  */
 package org.apache.beam.sdk.io.hcatalog;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import org.apache.beam.sdk.io.hcatalog.HCatalogIO.Read;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.DoFn.UnboundedPerElement;
 import org.apache.beam.sdk.transforms.SerializableComparator;
-import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.transforms.splittabledofn.RestrictionTracker;
 import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.ImmutableList;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.IMetaStoreClient;
 import org.apache.hadoop.hive.metastore.api.Partition;
 import org.apache.hive.hcatalog.common.HCatUtil;
-import org.joda.time.Duration;
-import org.joda.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /** Unbounded poller to listen for new partitions. */
 @UnboundedPerElement
-class PartitionPollerFn
-    extends DoFn<PartitionPollerFn.ReadRequest, HCatRecordReaderFn.PartitionWrapper> {
+class PartitionPollerFn extends DoFn<Read, HCatRecordReaderFn.PartitionWrapper> {
 
   private static final Logger LOG = LoggerFactory.getLogger(PartitionPollerFn.class);
   private transient IMetaStoreClient metaStoreClient;
@@ -48,32 +44,6 @@ class PartitionPollerFn
   private String database;
   private String table;
   final Boolean shouldTreatSourceAsBounded;
-
-  /** Wrapper class for reads. */
-  public static class ReadRequest implements Serializable {
-
-    final Duration pollInterval;
-    final HCatalogIO.Read readSpec;
-    final SerializableComparator<Partition> comparator;
-    final SerializableFunction<String, Instant> watermarkerFn;
-    final ImmutableList<String> partitionCols;
-    final String watermarkPartitionColumn;
-
-    public ReadRequest(
-        Duration pollInterval,
-        HCatalogIO.Read readSpec,
-        SerializableComparator<Partition> comparator,
-        SerializableFunction<String, Instant> watermarkerFn,
-        ImmutableList partitionCols,
-        String watermarkPartitionColumn) {
-      this.pollInterval = pollInterval;
-      this.readSpec = readSpec;
-      this.comparator = comparator;
-      this.partitionCols = partitionCols;
-      this.watermarkPartitionColumn = watermarkPartitionColumn;
-      this.watermarkerFn = watermarkerFn;
-    }
-  }
 
   public PartitionPollerFn(
       final Map<String, String> properties,
@@ -91,12 +61,12 @@ class PartitionPollerFn
   @SuppressWarnings("unused")
   public ProcessContinuation processElement(
       final ProcessContext c, RestrictionTracker<PartitionRange, Partition> tracker) {
-    final ReadRequest readRequest = c.element();
+    final Read readRequest = c.element();
     final PartitionRange range = tracker.currentRestriction();
     final ImmutableList<Partition> allAvailablePartitions = range.getPartitions();
 
     List<Partition> forSort = new ArrayList<>(allAvailablePartitions);
-    Collections.sort(forSort, readRequest.comparator);
+    Collections.sort(forSort, readRequest.getPartitionComparator());
 
     int trueStart;
     if (tracker.currentRestriction().getLastCompletedPartition() != null) {
@@ -112,10 +82,10 @@ class PartitionPollerFn
         c.output(
             new HCatRecordReaderFn.PartitionWrapper(
                 forSort.get(i),
-                readRequest.readSpec,
-                readRequest.watermarkPartitionColumn,
-                readRequest.partitionCols,
-                readRequest.watermarkerFn));
+                readRequest,
+                readRequest.getWatermarkPartitionColumn(),
+                readRequest.getPartitionCols(),
+                readRequest.getWatermarkTimestampConverter()));
       }
     }
 
@@ -123,8 +93,8 @@ class PartitionPollerFn
       LOG.info("Will terminate reading since the source is set to behave as bounded.");
       return ProcessContinuation.stop();
     } else {
-      LOG.info("{} - will resume polling in {} ms.", readRequest, readRequest.pollInterval);
-      return ProcessContinuation.resume().withResumeDelay(readRequest.pollInterval);
+      LOG.info("{} - will resume polling in {} ms.", readRequest, readRequest.getPollingInterval());
+      return ProcessContinuation.resume().withResumeDelay(readRequest.getPollingInterval());
     }
   }
 
@@ -148,10 +118,10 @@ class PartitionPollerFn
 
   @GetInitialRestriction
   @SuppressWarnings("unused")
-  public PartitionRange getInitialRestriction(ReadRequest request) throws Exception {
+  public PartitionRange getInitialRestriction(Read request) throws Exception {
     final ImmutableList<Partition> partitions =
-        getPartitions(request.readSpec.getDatabase(), request.readSpec.getTable());
-    return new PartitionRange(partitions, request.comparator, null);
+        getPartitions(request.getDatabase(), request.getTable());
+    return new PartitionRange(partitions, request.getPartitionComparator(), null);
   }
 
   @NewTracker
