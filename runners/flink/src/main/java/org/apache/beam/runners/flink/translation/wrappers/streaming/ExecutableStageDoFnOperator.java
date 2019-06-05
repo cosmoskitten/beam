ce5a9a6b1cd3dcbc9557902d0bd4ce9cc92ef61a
Jan Lukavsky <je.ik@seznam.cz>
2019-06-05 10:07:53 +0200

    [core] add StepContext to StatefulDoFnRunner

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/ExecutableStageDoFnOperator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/ExecutableStageDoFnOperator.java
@@ -51,6 +51,7 @@ import org.apache.beam.runners.core.StateNamespace;
 import org.apache.beam.runners.core.StateNamespaces;
 import org.apache.beam.runners.core.StateTag;
 import org.apache.beam.runners.core.StateTags;
+import org.apache.beam.runners.core.StepContext;
 import org.apache.beam.runners.core.StatefulDoFnRunner;
 import org.apache.beam.runners.core.TimerInternals;
 import org.apache.beam.runners.core.construction.Timer;
@@ -451,7 +452,7 @@ public class ExecutableStageDoFnOperator<InputT, OutputT> extends DoFnOperator<I
 
   @Override
   protected DoFnRunner<InputT, OutputT> createWrappingDoFnRunner(
-      DoFnRunner<InputT, OutputT> wrappedRunner) {
+      DoFnRunner<InputT, OutputT> wrappedRunner, StepContext stepContext) {
     sdkHarnessRunner =
         new SdkHarnessDoFnRunner<>(
             wrappedRunner,
@@ -465,7 +466,7 @@ public class ExecutableStageDoFnOperator<InputT, OutputT> extends DoFnOperator<I
             this::setTimer,
             () -> FlinkKeyUtils.decodeKey(getCurrentKey(), keyCoder));
 
-    return ensureStateCleanup(sdkHarnessRunner);
+    return ensureStateDoFnRunner(sdkHarnessRunner, stepContext);
   }
 
   @Override
@@ -707,8 +708,8 @@ public class ExecutableStageDoFnOperator<InputT, OutputT> extends DoFnOperator<I
     }
   }
 
-  private DoFnRunner<InputT, OutputT> ensureStateCleanup(
-      SdkHarnessDoFnRunner<InputT, OutputT> sdkHarnessRunner) {
+  private DoFnRunner<InputT, OutputT> ensureStateDoFnRunner(
+      SdkHarnessDoFnRunner<InputT, OutputT> sdkHarnessRunner, StepContext stepContext) {
     if (keyCoder == null) {
       // There won't be any state to clean up
       // (stateful functions have to be keyed)
@@ -735,7 +736,7 @@ public class ExecutableStageDoFnOperator<InputT, OutputT> extends DoFnOperator<I
         new StateCleaner(userStates, windowCoder, () -> stateBackend.getCurrentKey());
 
     return new StatefulDoFnRunner<InputT, OutputT, BoundedWindow>(
-        sdkHarnessRunner, windowingStrategy, cleanupTimer, stateCleaner) {
+        sdkHarnessRunner, stepContext, windowingStrategy, cleanupTimer, stateCleaner) {
       @Override
       public void finishBundle() {
         // Before cleaning up state, first finish bundle for all underlying DoFnRunners
