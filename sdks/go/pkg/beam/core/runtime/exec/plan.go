de4fc2b975d269f84ca780f34cb296f1c5f4c5fb
Robert Bradshaw <robertwb@gmail.com>
2019-06-06 10:37:01 +0200

    Lookup split requests on transform id rather than constant string.

--- sdks/go/pkg/beam/core/runtime/exec/plan.go
+++ sdks/go/pkg/beam/core/runtime/exec/plan.go
@@ -87,6 +87,11 @@ func (p *Plan) ID() string {
 	return p.id
 }
 
+// ID returns the plan identifier.
+func (p *Plan) Source() *DataSource {
+	return p.source
+}
+
 // Execute executes the plan with the given data context and bundle id. Units
 // are brought up on the first execution. If a bundle fails, the plan cannot
 // be reused for further bundles. Does not panic. Blocking.
