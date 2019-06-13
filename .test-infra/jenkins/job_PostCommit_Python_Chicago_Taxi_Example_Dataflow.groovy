f709fa47b16dec031ac81c07bb8be517f323e6cd
Michal Walenia <michal.walenia@polidea.com>
2019-06-13 13:39:37 +0200

    CTE without cron job

--- .test-infra/jenkins/job_PostCommit_Python_Chicago_Taxi_Example_Dataflow.groovy
+++ .test-infra/jenkins/job_PostCommit_Python_Chicago_Taxi_Example_Dataflow.groovy
@@ -19,8 +19,6 @@
 import CommonJobProperties as commonJobProperties
 import PostcommitJobBuilder
 import CronJobBuilder
-import CommonTestProperties
-
 
 
 // This job runs the Chicao Taxi Example script on Dataflow
@@ -28,24 +26,6 @@ PostcommitJobBuilder.postCommitJob('beam_PostCommit_Python_Chicago_Taxi_Dataflow
         'Run Chicago Taxi on Dataflow', 'Google Cloud Dataflow Runner Chicago Taxi Example', this) {
 
     description('Runs the Chicago Taxi Example on the Dataflow runner.')
-    // Publish all test results to Jenkins
-    publishers {
-        archiveJunit('**/build/test-results/**/*.xml')
-    }
-
-    // Gradle goals for this job.
-    steps {
-        gradle {
-            rootBuildScriptDir(commonJobProperties.checkoutDir)
-            tasks(':sdks:python:dataflowChicagoTaxiExample')
-            switches('-PgcsRoot=gs://')
-            switches('-Prunner=DataflowRunner')
-        }
-    }
-}
-
-CronJobBuilder.cronJob('beam_PostCommit_Python_Chicago_Taxi_Dataflow', 'H 12 * * *', this) {
-    description('Runs the Chicago Taxi on the Dataflow runner.')
     // Publish all test results to Jenkins
     publishers {
         archiveJunit('**/build/test-results/**/*.xml')
