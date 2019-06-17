714987782017470c55c987e8751e8ea136ce2fca
Juta <juta.staes@gmail.com>
2019-06-17 12:32:45 +0200

    [BEAM-7326] add documentation bigquery data types

--- sdks/python/apache_beam/io/gcp/bigquery.py
+++ sdks/python/apache_beam/io/gcp/bigquery.py
@@ -224,6 +224,9 @@ high-precision decimal numbers (precision of 38 digits, scale of 9 digits).
 The GEOGRAPHY data type works with Well-Known Text (See
 https://en.wikipedia.org/wiki/Well-known_text) format for reading and writing
 to BigQuery.
+The BYTES data type requires that bytes are encoded using base64 encoding when
+writing to BigQuery. When bytes are read from BigQuery they are returned as
+base64 encoded bytes.
 """
 
 from __future__ import absolute_import
