b9381b715464dafb841d5d5bb5b0440d8f36c28b
Heejong Lee <heejong@gmail.com>
2019-07-01 12:07:09 -0700

    [BEAM-7667] report GCS throttling time to Dataflow autoscaler

--- runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/BatchModeExecutionContext.java
+++ runners/google-cloud-dataflow-java/worker/src/main/java/org/apache/beam/runners/dataflow/worker/BatchModeExecutionContext.java
@@ -66,6 +66,8 @@ public class BatchModeExecutionContext
   // TODO: Move throttle time Metric to a dedicated namespace.
   protected static final String DATASTORE_THROTTLE_TIME_NAMESPACE =
       "org.apache.beam.sdk.io.gcp.datastore.DatastoreV1$DatastoreWriterFn";
+  protected static final String HTTP_CLIENT_API_THROTTLE_TIME_NAMESPACE =
+      "org.apache.beam.sdk.extensions.gcp.util.RetryHttpRequestInitializer$LoggingHttpBackOffHandler";
 
   private BatchModeExecutionContext(
       CounterFactory counterFactory,
@@ -499,14 +501,24 @@ public class BatchModeExecutionContext
     Long totalThrottleTime = 0L;
     for (MetricsContainerImpl container : containerRegistry.getContainers()) {
       // TODO: Update Datastore to use generic throttling-msecs metric.
-      CounterCell throttleTime =
+      CounterCell dataStoreThrottlingTime =
           container.tryGetCounter(
               MetricName.named(
                   BatchModeExecutionContext.DATASTORE_THROTTLE_TIME_NAMESPACE,
                   "cumulativeThrottlingSeconds"));
-      if (throttleTime != null) {
-        totalThrottleTime += throttleTime.getCumulative();
+      if (dataStoreThrottlingTime != null) {
+        totalThrottleTime += dataStoreThrottlingTime.getCumulative();
       }
+
+      CounterCell httpClientApiThrottlingTime =
+          container.tryGetCounter(
+              MetricName.named(
+                  BatchModeExecutionContext.HTTP_CLIENT_API_THROTTLE_TIME_NAMESPACE,
+                  "cumulativeThrottlingSeconds"));
+      if (httpClientApiThrottlingTime != null) {
+        totalThrottleTime += httpClientApiThrottlingTime.getCumulative();
+      }
+
       CounterCell throttlingMsecs =
           container.tryGetCounter(DataflowSystemMetrics.THROTTLING_MSECS_METRIC_NAME);
       if (throttlingMsecs != null) {
