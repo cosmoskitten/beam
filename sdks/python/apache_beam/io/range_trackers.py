e2296e2461158d51c3a78cbc7cf60ed4d799595b
Boyuan Zhang <boyuanz@google.com>
2019-07-02 14:58:25 -0700

    Revert "Add position_to_fraction(pos, start, stop) to RangeTracker API"
    This reverts commit fa581919

--- sdks/python/apache_beam/io/range_trackers.py
+++ sdks/python/apache_beam/io/range_trackers.py
@@ -155,14 +155,13 @@ class OffsetRangeTracker(iobase.RangeTracker):
       # the range of the OffsetRangeTracker. Hence fraction could be > 1.
       # self.last_record_start is initialized to -1, hence fraction may be < 0.
       # Bounding the to range [0, 1].
-      fraction = self.position_to_fraction(self._last_record_start,
+      return self.position_to_fraction(self._last_record_start,
                                        self.start_position(),
                                        self.stop_position())
-      return max(0.0, min(1.0, fraction))
 
   def position_to_fraction(self, pos, start, stop):
-    fraction = (1.0 * (pos - start) / (stop - start)) if start != stop else 0.0
-    return fraction
+    fraction = 1.0 * (pos - start) / (stop - start) if start != stop else 0.0
+    return max(0.0, min(1.0, fraction))
 
   def position_at_fraction(self, fraction):
     if self.stop_position() == OffsetRangeTracker.OFFSET_INFINITY:
