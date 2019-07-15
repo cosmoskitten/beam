558dc489c0d871e17f7ab94bb6cb80fdd7040413
Juta <juta.staes@gmail.com>
2019-07-15 10:20:34 +0200

    [DO NOT MERGE][BEAM-7463] test bq matcher issues

--- sdks/python/apache_beam/io/gcp/tests/bigquery_matcher.py
+++ sdks/python/apache_beam/io/gcp/tests/bigquery_matcher.py
@@ -84,6 +84,7 @@ class BigqueryMatcher(BaseMatcher):
     response = self._query_with_retry(bigquery_client)
     logging.info('Read from given query (%s), total rows %d',
                  self.query, len(response))
+    logging.info(response)
 
     # Compute checksum
     self.checksum = compute_hash(response)
@@ -141,6 +142,7 @@ class BigqueryFullResultMatcher(BaseMatcher):
     response = self._get_query_result(bigquery_client)
     logging.info('Read from given query (%s), total rows %d',
                  self.query, len(response))
+    logging.info(response)
 
     self.actual_data = response
 
