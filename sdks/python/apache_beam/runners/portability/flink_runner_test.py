149933f2c08eb3152022a462f2310fca6044dccd
Boyuan Zhang <boyuanz@google.com>
2019-07-16 17:16:53 -0700

    Skip UT when running on flink.

--- sdks/python/apache_beam/runners/portability/flink_runner_test.py
+++ sdks/python/apache_beam/runners/portability/flink_runner_test.py
@@ -262,6 +262,12 @@ if __name__ == '__main__':
     def test_sdf_synthetic_source(self):
       raise unittest.SkipTest("BEAM-2939")
 
+    def test_sdf_wrapper_overrides_read(self):
+      raise unittest.SkipTest("BEAM-2939")
+
+    def test_sdf_wrapper_overrides_read_from_text(self):
+      raise unittest.SkipTest("BEAM-2939")
+
     def test_callbacks_with_exception(self):
       raise unittest.SkipTest("BEAM-6868")
 
