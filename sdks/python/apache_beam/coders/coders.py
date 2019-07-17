2b309f97f0875c31fe4ae3e03eee4ad1f640a1a8
Chad Dombrova <chadrik@gmail.com>
2019-07-17 00:48:15 -0700

    Create a more user friendly external transform API
    Standardize and reduce boilerplate

--- sdks/python/apache_beam/coders/coders.py
+++ sdks/python/apache_beam/coders/coders.py
@@ -386,8 +386,14 @@ class FastCoder(Coder):
 class BytesCoder(FastCoder):
   """Byte string coder."""
 
+  def __init__(self, encode_utf8=False):
+    """
+    :param encode_utf8: encode the string value as utf-8.
+    """
+    self.encode_utf8 = encode_utf8
+
   def _create_impl(self):
-    return coder_impl.BytesCoderImpl()
+    return coder_impl.BytesCoderImpl(encode_utf8=self.encode_utf8)
 
   def is_deterministic(self):
     return True
