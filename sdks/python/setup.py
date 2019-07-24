2b7385f9616c9e8fb5ae83e6f4f4480a45780106
Alexey Strokach <strokach@google.com>
2019-07-23 17:51:55 -0700

    Use dateutil for more flexible parsing of RFC-3339 / ISO-8601 formatted strings

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -122,6 +122,7 @@ REQUIRED_PACKAGES = [
     ('pyarrow>=0.11.1,<0.14.0; python_version >= "3.0" or '
      'platform_system != "Windows"'),
     'pydot>=1.2.0,<1.3',
+    'python-dateutil>=2.8.0,<2.9.0',
     'pytz>=2018.3',
     # [BEAM-5628] Beam VCF IO is not supported in Python 3.
     'pyvcf>=0.6.8,<0.7.0; python_version < "3.0"',
