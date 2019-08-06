26d89529bd02e7e417ca7383b7fb7cd52901335f
Alexey Strokach <strokach@google.com>
2019-08-06 14:55:59 -0700

    Add a PCollectionCache ABC and several file-based implementations
    This should facilitate interactivity and simplify the debugging of Beam
    pipelines.

--- sdks/python/apache_beam/runners/interactive/caching/__init__.py
+++ sdks/python/apache_beam/runners/interactive/caching/__init__.py
@@ -14,3 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from __future__ import absolute_import
+
+from apache_beam.runners.interactive.caching.pcollectioncache import PCollectionCache
+from apache_beam.runners.interactive.caching.filebasedcache import FileBasedCache
