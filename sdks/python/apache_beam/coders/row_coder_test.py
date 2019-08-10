17bfdbb6a75916f05a6d3b3cd214b2109eae88c4
Brian Hulette <bhulette@google.com>
2019-08-09 17:19:41 -0700

    !fixup Add tests to verify RowCoder's handling of bad inputs
    should throw an error when:
    - it receives a null for a non-nullable field
    - it receives an integer that's too large for an INT field (currently
      this test is skipped)

--- sdks/python/apache_beam/coders/row_coder_test.py
+++ sdks/python/apache_beam/coders/row_coder_test.py
@@ -19,6 +19,7 @@ from __future__ import absolute_import
 import logging
 import typing
 import unittest
+from itertools import chain
 
 import numpy as np
 from past.builtins import unicode
@@ -83,6 +84,44 @@ class CodersTest(unittest.TestCase):
 
     for test_case in self.TEST_CASES:
       self.assertEqual(test_case, coder.decode(coder.encode(test_case)))
+
+  @unittest.skip(
+      "Need to decide whether to defer to the stream writer for these checks "
+      "or add explicit checks"
+  )
+  def test_overflows(self):
+    IntTester = typing.NamedTuple('IntTester', [
+        #('i8', typing.Optional[np.int8]),
+        #('i16', typing.Optional[np.int16]),
+        ('i32', typing.Optional[np.int32]),
+        ('i64', typing.Optional[np.int64]),
+    ])
+
+    c = RowCoder.from_type_hint(IntTester, None)
+
+    no_overflow = chain(
+        (IntTester(i32=i, i64=None) for i in (-2**31, 2**31-1)),
+        (IntTester(i32=None, i64=i) for i in (-2**63, 2**63-1)),
+    )
+
+    # Encode max/min ints to make sure they don't throw any error
+    for case in no_overflow:
+      c.encode(case)
+
+    overflow = chain(
+        (IntTester(i32=i, i64=None) for i in (-2**31-1, 2**31)),
+        (IntTester(i32=None, i64=i) for i in (-2**63-1, 2**63)),
+    )
+
+    # Encode max+1/min-1 ints to make sure they DO throw an error
+    for case in overflow:
+      self.assertRaises(OverflowError, lambda: c.encode(case))
+
+  def test_none_in_non_nullable_field_throws(self):
+    Test = typing.NamedTuple('Test', [('foo', str)])
+
+    c = RowCoder.from_type_hint(Test, None)
+    self.assertRaises(ValueError, lambda: c.encode(Test(foo=None)))
 
 
 if __name__ == "__main__":
