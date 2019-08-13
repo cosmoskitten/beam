dae2658b7e61c1b97e42171ed5feb4d34ac40049
robinyqiu <robinyqiu@gmail.com>
2019-08-12 21:19:17 -0700

    Remove HyperLogLogPlusPlusWrapper and its coder

--- sdks/java/extensions/zetasketch/src/main/java/org/apache/beam/sdk/extensions/zetasketch/HllCountMergePartialFn.java
+++ sdks/java/extensions/zetasketch/src/main/java/org/apache/beam/sdk/extensions/zetasketch/HllCountMergePartialFn.java
@@ -18,15 +18,9 @@
 package org.apache.beam.sdk.extensions.zetasketch;
 
 import com.google.zetasketch.HyperLogLogPlusPlus;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import javax.annotation.Nullable;
-import org.apache.beam.sdk.coders.AtomicCoder;
 import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.coders.CoderRegistry;
 import org.apache.beam.sdk.coders.NullableCoder;
-import org.apache.beam.sdk.extensions.zetasketch.HllCountMergePartialFn.HyperLogLogPlusPlusWrapper;
 import org.apache.beam.sdk.transforms.Combine;
 
 /**
@@ -34,8 +28,7 @@ import org.apache.beam.sdk.transforms.Combine;
  *
  * @param <T> type of the HLL++ sketch to be merged
  */
-class HllCountMergePartialFn<T>
-    extends Combine.CombineFn<byte[], HyperLogLogPlusPlusWrapper<T>, byte[]> {
+class HllCountMergePartialFn<T> extends Combine.CombineFn<byte[], HyperLogLogPlusPlus<T>, byte[]> {
 
   // Call HllCountMergePartialFn.create() to instantiate
   private HllCountMergePartialFn() {}
@@ -44,109 +37,55 @@ class HllCountMergePartialFn<T>
     return new HllCountMergePartialFn();
   }
 
-  /**
-   * Accumulator for the {@link HllCount.MergePartial} combiner. Cannot use {@link
-   * HyperLogLogPlusPlus} directly because we need an "identity element" for the {@code merge}
-   * operation, and we are not able to create such "identity element" of type {@link
-   * HyperLogLogPlusPlus} without knowing the sketch precision and value type.
-   */
-  static final class HyperLogLogPlusPlusWrapper<T> {
-    @Nullable private HyperLogLogPlusPlus<T> hll;
-
-    private HyperLogLogPlusPlusWrapper(@Nullable HyperLogLogPlusPlus<T> hll) {
-      this.hll = hll;
-    }
-  }
-
-  /**
-   * Coder for the {@link HyperLogLogPlusPlusWrapper} class with generic type parameter {@code T}.
-   */
-  private static final class HyperLogLogPlusPlusWrapperCoder<T>
-      extends AtomicCoder<HyperLogLogPlusPlusWrapper<T>> {
-
-    private static final HyperLogLogPlusPlusWrapperCoder<?> INSTANCE =
-        new HyperLogLogPlusPlusWrapperCoder<>();
-
-    private final Coder<HyperLogLogPlusPlus<T>> nullableHllCoder =
-        NullableCoder.of(HyperLogLogPlusPlusCoder.of());
-
-    // Generic singleton factory pattern; the coder works for all HyperLogLogPlusPlusWrapper objects
-    // at runtime regardless of type T
-    @SuppressWarnings("unchecked")
-    static <T> HyperLogLogPlusPlusWrapperCoder<T> of() {
-      return (HyperLogLogPlusPlusWrapperCoder<T>) INSTANCE;
-    }
-
-    @Override
-    public void encode(HyperLogLogPlusPlusWrapper<T> value, OutputStream outStream)
-        throws IOException {
-      nullableHllCoder.encode(value.hll, outStream);
-    }
-
-    @Override
-    public HyperLogLogPlusPlusWrapper<T> decode(InputStream inStream) throws IOException {
-      return new HyperLogLogPlusPlusWrapper<T>(nullableHllCoder.decode(inStream));
-    }
-
-    @Override
-    public void verifyDeterministic() throws NonDeterministicException {
-      nullableHllCoder.verifyDeterministic();
-    }
-  }
-
   @Override
-  public Coder<HyperLogLogPlusPlusWrapper<T>> getAccumulatorCoder(
+  public Coder<HyperLogLogPlusPlus<T>> getAccumulatorCoder(
       CoderRegistry registry, Coder<byte[]> inputCoder) {
-    return HyperLogLogPlusPlusWrapperCoder.of();
+    return NullableCoder.of(HyperLogLogPlusPlusCoder.of());
   }
 
   @Override
-  public HyperLogLogPlusPlusWrapper<T> createAccumulator() {
-    return new HyperLogLogPlusPlusWrapper<>(null);
+  public HyperLogLogPlusPlus<T> createAccumulator() {
+    return null;
   }
 
   @Override
-  public HyperLogLogPlusPlusWrapper<T> addInput(
-      HyperLogLogPlusPlusWrapper<T> accumulator, byte[] input) {
-    if (accumulator.hll == null) {
+  public HyperLogLogPlusPlus<T> addInput(HyperLogLogPlusPlus<T> accumulator, byte[] input) {
+    if (accumulator == null) {
       @SuppressWarnings("unchecked")
-      HyperLogLogPlusPlus<T> hll = (HyperLogLogPlusPlus<T>) HyperLogLogPlusPlus.forProto(input);
-      accumulator.hll = hll;
+      HyperLogLogPlusPlus<T> result = (HyperLogLogPlusPlus<T>) HyperLogLogPlusPlus.forProto(input);
+      return result;
     } else {
-      accumulator.hll.merge(input);
+      accumulator.merge(input);
+      return accumulator;
     }
-    return accumulator;
   }
 
   @Override
-  public HyperLogLogPlusPlusWrapper<T> mergeAccumulators(
-      Iterable<HyperLogLogPlusPlusWrapper<T>> accumulators) {
-    HyperLogLogPlusPlusWrapper<T> merged = createAccumulator();
-    for (HyperLogLogPlusPlusWrapper<T> accumulator : accumulators) {
-      if (accumulator.hll == null) {
+  public HyperLogLogPlusPlus<T> mergeAccumulators(Iterable<HyperLogLogPlusPlus<T>> accumulators) {
+    HyperLogLogPlusPlus<T> merged = createAccumulator();
+    for (HyperLogLogPlusPlus<T> accumulator : accumulators) {
+      if (accumulator == null) {
         continue;
       }
-      if (merged.hll == null) {
+      if (merged == null) {
         @SuppressWarnings("unchecked")
-        HyperLogLogPlusPlus<T> hll =
-            (HyperLogLogPlusPlus<T>)
-                // Cannot set merged.hll to accumulator.hll directly because we shouldn't mutate
-                // accumulator
-                HyperLogLogPlusPlus.forProto(accumulator.hll.serializeToProto());
-        merged.hll = hll;
+        HyperLogLogPlusPlus<T> clonedAccumulator =
+            (HyperLogLogPlusPlus<T>) HyperLogLogPlusPlus.forProto(accumulator.serializeToProto());
+        // Cannot set merged to accumulator directly because we shouldn't mutate accumulator
+        merged = clonedAccumulator;
       } else {
-        merged.hll.merge(accumulator.hll);
+        merged.merge(accumulator);
       }
     }
     return merged;
   }
 
   @Override
-  public byte[] extractOutput(HyperLogLogPlusPlusWrapper<T> accumulator) {
-    if (accumulator.hll == null) {
+  public byte[] extractOutput(HyperLogLogPlusPlus<T> accumulator) {
+    if (accumulator == null) {
       throw new IllegalStateException(
           "HllCountMergePartialFn.extractOutput() should not be called on a dummy accumulator.");
     }
-    return accumulator.hll.serializeToByteArray();
+    return accumulator.serializeToByteArray();
   }
 }
