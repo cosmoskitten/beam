5de6378803dda87885f5cc33e5a8d82f4da30d96
Mikhail Gryzykhin <migryz@google.com>
2019-08-20 10:27:20 -0700

    Adjust test name

--- sdks/python/apache_beam/runners/dataflow/dataflow_exercise_streaming_metrics_pipeline_test.py
+++ sdks/python/apache_beam/runners/dataflow/dataflow_exercise_streaming_metrics_pipeline_test.py
@@ -114,7 +114,7 @@ class ExerciseStreamingMetricsPipelineTest(unittest.TestCase):
     return dataflow_exercise_streaming_metrics_pipeline.run(argv)
 
   @attr('IT', 'ValidatesContainer')
-  def test_fnapi_streaming_pipeline_returns_expected_user_metrics(self):
+  def test_streaming_pipeline_returns_expected_user_metrics_fnapi_it(self):
     self._inject_words(self.input_topic, MESSAGES_TO_PUBLISH)
     result = self.run_pipeline()
 
