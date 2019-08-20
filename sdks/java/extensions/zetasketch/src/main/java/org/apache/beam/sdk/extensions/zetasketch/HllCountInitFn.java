c5ddcf6695614dbeb136a36c8750c3b458ee26bb
Yueyang Qiu <robinyqiu@gmail.com>
2019-08-20 12:41:58 -0700

    Make comments clear

--- sdks/java/extensions/zetasketch/src/main/java/org/apache/beam/sdk/extensions/zetasketch/HllCountInitFn.java
+++ sdks/java/extensions/zetasketch/src/main/java/org/apache/beam/sdk/extensions/zetasketch/HllCountInitFn.java
@@ -34,9 +34,10 @@ import org.apache.beam.sdk.transforms.Combine;
 abstract class HllCountInitFn<InputT, HllT>
     extends Combine.CombineFn<InputT, HyperLogLogPlusPlus<HllT>, byte[]> {
 
-  // Would make this a final field. However, that not only requires adding an extra type enum to
-  // HllCount.Init.Builder to cache the type information, but also makes it hard to make the Builder
-  // generic with input type T (requires lots of type casting when constructing the transform).
+  // Ideally, this would be a final field set at construction time via the builder. However, that
+  // not only requires adding an extra type enum to HllCount.Init.Builder to cache the type
+  // information, but also makes it hard to make the Builder generic with input type T (requires
+  // lots of type casting when constructing the transform).
   private int precision = HllCount.DEFAULT_PRECISION;
 
   int getPrecision() {
