db66205dd4ff8ce2c25c41459458e5271ae09c46
Kamil Wasilewski <kamil.wasilewski@polidea.com>
2019-08-22 12:40:46 +0200

    [BEAM-7872] Explicitly do not publish any metrics until it works

--- .test-infra/jenkins/job_LoadTests_coGBK_Flink_Python.groovy
+++ .test-infra/jenkins/job_LoadTests_coGBK_Flink_Python.groovy
@@ -36,7 +36,7 @@ def scenarios = { datasetName -> [
                         project              : 'apache-beam-testing',
                         job_name             : 'load-tests-python-flink-batch-cogbk-1-' + now,
                         temp_location        : 'gs://temp-storage-for-perf-tests/loadtests',
-                        publish_to_big_query : true,
+                        publish_to_big_query : false,
                         metrics_dataset      : datasetName,
                         metrics_table        : "python_flink_batch_cogbk_1",
                         input_options        : '\'{' +
@@ -66,7 +66,7 @@ def scenarios = { datasetName -> [
                         project              : 'apache-beam-testing',
                         job_name             : 'load-tests-python-flink-batch-cogbk-2-' + now,
                         temp_location        : 'gs://temp-storage-for-perf-tests/loadtests',
-                        publish_to_big_query : true,
+                        publish_to_big_query : false,
                         metrics_dataset      : datasetName,
                         metrics_table        : 'python_flink_batch_cogbk_2',
                         input_options        : '\'{' +
@@ -96,7 +96,7 @@ def scenarios = { datasetName -> [
                         project              : 'apache-beam-testing',
                         job_name             : 'load-tests-python-flink-batch-cogbk-3-' + now,
                         temp_location        : 'gs://temp-storage-for-perf-tests/loadtests',
-                        publish_to_big_query : true,
+                        publish_to_big_query : false,
                         metrics_dataset      : datasetName,
                         metrics_table        : "python_flink_batch_cogbk_3",
                         input_options        : '\'{' +
@@ -126,7 +126,7 @@ def scenarios = { datasetName -> [
                         project              : 'apache-beam-testing',
                         job_name             : 'load-tests-python-flink-batch-cogbk-4-' + now,
                         temp_location        : 'gs://temp-storage-for-perf-tests/loadtests',
-                        publish_to_big_query : true,
+                        publish_to_big_query : false,
                         metrics_dataset      : datasetName,
                         metrics_table        : 'python_flink_batch_cogbk_4',
                         input_options        : '\'{' +
