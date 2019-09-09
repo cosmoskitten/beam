ee70151c5821b82caaedf97b99665a810517bf0f
Hannah Jiang <hannahjiang@google.com>
2019-09-09 15:20:53 -0700

    [BEAM-8165] read version to set tag for default docker image

--- sdks/go/pkg/beam/options/jobopts/options.go
+++ sdks/go/pkg/beam/options/jobopts/options.go
@@ -21,6 +21,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 	"time"
@@ -108,7 +109,13 @@ func GetEnvironmentUrn(ctx context.Context) string {
 // Convenience function.
 func GetEnvironmentConfig(ctx context.Context) string {
 	if *EnvironmentConfig == "" {
-		*EnvironmentConfig = os.ExpandEnv("$apachebeam/go_sdk:latest")
+		version_file := "version.txt"
+		sdk_version, err := ioutil.ReadFile(version_file)
+		if err != nil {
+			log.Fatalf("Failed to read 'version.txt' file: %v", err)
+		}
+		*EnvironmentConfig = os.ExpandEnv(fmt.Sprintf("apachebeam/go_sdk:%s",
+			string(sdk_version)))
 		log.Infof(ctx, "No environment config specified. Using default config: '%v'", *EnvironmentConfig)
 	}
 	return *EnvironmentConfig
