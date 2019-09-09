0bc6d7d21092ee337dcf81004492c83a4096e178
Chad Dombrova <chadrik@gmail.com>
2019-09-09 12:53:19 -0700

    Ignore an issue regarding asymmetrical property getter/setter

--- sdks/python/apache_beam/transforms/ptransform.py
+++ sdks/python/apache_beam/transforms/ptransform.py
@@ -342,7 +342,7 @@ class PTransform(WithTypeHints, HasDisplayData):
   def __init__(self, label=None):
     # type: (Optional[str]) -> None
     super(PTransform, self).__init__()
-    self.label = label
+    self.label = label  # type: ignore # https://github.com/python/mypy/issues/3004
 
   @property
   def label(self):
