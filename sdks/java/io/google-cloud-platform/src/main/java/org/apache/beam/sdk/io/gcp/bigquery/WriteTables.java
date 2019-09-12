569e29d792802feb294274e87b05652543c620b2
Brian Bradley <lispwisp@gmail.com>
2019-09-12 01:52:51 -0700

    Don't set CREATE_NEVER when writing to destination
    If writing directly to the destination, then the table is created on the first write, but only when the first pane actually contains elements that are destined for that table. If the first pane does not contain such elements, the CreateDisposition is currently inadvertently set to CreateNever, which produces production issues for me:
    ```
    java.lang.RuntimeException: Failed to create job with prefix beam_load_iottimeseriesdatapipeline_399f52a8d4c249c387948d3472b516b4_a9bbce63e9d84f698e587084f1402bd1_00001_00345, reached max retries: 1000, last failed job: {
      "configuration" : {
        "jobType" : "LOAD",
        "load" : {
          "clustering" : {
            "fields" : [ "device_id" ]
          },
          "createDisposition" : "CREATE_NEVER",
          "destinationTable" : {
            "datasetId" : "device_metrics_raw",
            "projectId" : "awair-stg",
            "tableId" : "diskused_awairgateway"
          },
          "ignoreUnknownValues" : false,
          "sourceFormat" : "NEWLINE_DELIMITED_JSON",
          "timePartitioning" : {
            "field" : "timestamp",
            "requirePartitionFilter" : true,
            "type" : "DAY"
          },
          "writeDisposition" : "WRITE_APPEND"
        }
      },```
    
    which shows up despite explicitly setting:
    ```
    .to(
            new BigqueryDestinationFunc(project, datasetName)
          ).withClustering(
          ).optimizedWrites(
          ).withWriteDisposition(
            WriteDisposition.WRITE_APPEND
          ).withCreateDisposition(
            CreateDisposition.CREATE_IF_NEEDED
          ).withMethod(
            BigQueryIO.Write.Method.FILE_LOADS
          ).withTriggeringFrequency(
            Duration.standardMinutes(5)
          )
    ```

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/WriteTables.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/WriteTables.java
@@ -199,7 +199,6 @@ class WriteTables<DestinationT>
         // If writing directly to the destination, then the table is created on the first write
         // and we should change the disposition for subsequent writes.
         writeDisposition = WriteDisposition.WRITE_APPEND;
-        createDisposition = CreateDisposition.CREATE_NEVER;
       } else if (tempTable) {
         // In this case, we are writing to a temp table and always need to create it.
         // WRITE_TRUNCATE is set so that we properly handle retries of this pane.
