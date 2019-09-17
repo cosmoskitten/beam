35d4cf905d472f38f7bdb0b817c62ad7c90cb5d7
Chamikara Jayalath <chamikara@apache.org>
2019-09-17 16:34:56 -0700

    Several small changes needed to support cross-language transforms on DataflowRunner

--- sdks/python/apache_beam/transforms/core.py
+++ sdks/python/apache_beam/transforms/core.py
@@ -362,10 +362,16 @@ class RunnerAPIPTransformHolder(PTransform):
       else:
         env1 = id_to_proto_map[env_id]
         env2 = context.environments[env_id]
-        assert env1.SerializeToString() == env2.SerializeToString(), (
+        assert env1.urn == env2.proto.urn, (
             'Expected environments with the same ID to be equal but received '
+            'environments with different URNs '
             '%r and %r',
-            env1, env2)
+            env1.urn, env2.proto.urn)
+        assert env1.payload == env2.proto.payload, (
+            'Expected environments with the same ID to be equal but received '
+            'environments with different payloads '
+            '%r and %r',
+            env1.payload, env2.proto.payload)
     return self._proto
 
   def get_restriction_coder(self):
@@ -1110,6 +1116,7 @@ class ParDo(PTransformWithSideInputs):
     # TODO(robertwb): Change all uses of the dofn attribute to use fn instead.
     self.dofn = self.fn
     self.output_tags = set()
+    self.main_output_tag = None
 
     if not isinstance(self.fn, DoFn):
       raise TypeError('ParDo must be called with a DoFn instance.')
