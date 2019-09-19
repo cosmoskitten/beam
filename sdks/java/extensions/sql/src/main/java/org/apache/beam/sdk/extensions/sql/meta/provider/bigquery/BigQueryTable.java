c05fc64c0bf753537a072eceaa91af11dcd0dd83
Kirill Kozlov <kirillkozlov@google.com>
2019-09-19 16:18:15 -0700

    [BEAM-8275] Beam SQL should support BigQuery in DIRECT_READ mode

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/meta/provider/bigquery/BigQueryTable.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/meta/provider/bigquery/BigQueryTable.java
@@ -56,7 +56,8 @@ class BigQueryTable extends BaseBeamTable implements Serializable {
   private static final Logger LOGGER = LoggerFactory.getLogger(BigQueryTable.class);
   @VisibleForTesting final Method method;
 
-  BigQueryTable(Table table, BigQueryUtils.ConversionOptions options) {
+  BigQueryTable(Table table, BigQueryUtils.ConversionOptions options)
+      throws InvalidPropertyException {
     super(table.getSchema());
     this.conversionOptions = options;
     this.bqLocation = table.getLocation();
