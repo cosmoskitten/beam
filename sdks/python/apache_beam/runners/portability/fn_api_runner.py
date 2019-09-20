d8e43da3fb6407badf49da8d723cacc0e3b73895
Chad Dombrova <chadrik@gmail.com>
2019-09-20 09:30:55 -0700

    Brute force some scenarios where inference falls down

--- sdks/python/apache_beam/runners/portability/fn_api_runner.py
+++ sdks/python/apache_beam/runners/portability/fn_api_runner.py
@@ -47,6 +47,7 @@ from typing import Tuple
 from typing import Type
 from typing import TypeVar
 from typing import Union
+from typing import cast
 from typing import overload
 
 import grpc
@@ -1576,7 +1577,9 @@ class WorkerHandlerManager(object):
 
     # assume all environments except EMBEDDED_PYTHON use gRPC.
     if environment.urn == python_urns.EMBEDDED_PYTHON:
-      pass # no need for a gRPC server
+      # no need for a gRPC server, but we need to silence a typing error from
+      # WorkerHandler.create() below
+      self._grpc_server = cast('GrpcServer', None)
     elif self._grpc_server is None:
       self._grpc_server = GrpcServer(self._state, self._job_provision_info,
                                      max_total_workers)
