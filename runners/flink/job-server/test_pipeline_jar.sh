20dd851b7516265b080f478b658cf314a53b23a5
Kyle Weaver <kcweaver@google.com>
2019-09-27 14:37:36 -0700

    [BEAM-8321] fix Flink portable jar test

--- runners/flink/job-server/test_pipeline_jar.sh
+++ runners/flink/job-server/test_pipeline_jar.sh
@@ -43,6 +43,16 @@ case $key in
         shift # past argument
         shift # past value
         ;;
+    --python_container_image)
+        PYTHON_CONTAINER_IMAGE="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    --python_container_tag)
+        PYTHON_CONTAINER_TAG="$2"
+        shift # past argument
+        shift # past value
+        ;;
     *)    # unknown option
         echo "Unknown option: $1"
         exit 1
@@ -57,10 +67,8 @@ cd $(git rev-parse --show-toplevel)
 command -v docker
 docker -v
 
-CONTAINER=$USER-docker-apache.bintray.io/beam/python$PYTHON_VERSION
-TAG=latest
 # Verify container has already been built
-docker images $CONTAINER:$TAG | grep $TAG
+docker images $PYTHON_CONTAINER_IMAGE:$PYTHON_CONTAINER_TAG | grep $PYTHON_CONTAINER_TAG
 
 # Set up Python environment
 virtualenv -p python$PYTHON_VERSION $ENV_DIR
@@ -102,7 +110,7 @@ OUTPUT_JAR=flink-test-$(date +%Y%m%d-%H%M%S).jar
   --parallelism 1 \
   --sdk_worker_parallelism 1 \
   --environment_type DOCKER \
-  --environment_config=$CONTAINER:$TAG \
+  --environment_config=$PYTHON_CONTAINER_IMAGE:$PYTHON_CONTAINER_TAG \
 ) || TEST_EXIT_CODE=$? # don't fail fast here; clean up before exiting
 
 if [[ "$TEST_EXIT_CODE" -eq 0 ]]; then
