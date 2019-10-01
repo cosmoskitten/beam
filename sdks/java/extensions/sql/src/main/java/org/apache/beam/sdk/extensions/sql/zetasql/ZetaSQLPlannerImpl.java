b5404cd7c560fced447d9f0405a10620eb4ba6c3
Andrew Pilloud <apilloud@google.com>
2019-10-01 13:31:06 -0700

    [BEAM-8334] Expose Language Options for testing

--- sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/zetasql/ZetaSQLPlannerImpl.java
+++ sdks/java/extensions/sql/src/main/java/org/apache/beam/sdk/extensions/sql/zetasql/ZetaSQLPlannerImpl.java
@@ -18,6 +18,7 @@
 package org.apache.beam.sdk.extensions.sql.zetasql;
 
 import com.google.zetasql.Value;
+import com.google.zetasql.LanguageOptions;
 import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedQueryStmt;
 import com.google.zetasql.resolvedast.ResolvedNodes.ResolvedStatement;
 import java.io.Reader;
@@ -181,5 +182,9 @@ public class ZetaSQLPlannerImpl implements Planner {
   @Override
   public RelTraitSet getEmptyTraitSet() {
     throw new RuntimeException("getEmptyTraitSet() is not implemented.");
+  }
+
+  public static LanguageOptions getLanguageOptions() {
+    return SqlAnalyzer.initAnalyzerOptions().getLanguageOptions();
   }
 }
