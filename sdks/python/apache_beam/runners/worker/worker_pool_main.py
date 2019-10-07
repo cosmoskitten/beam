b1c7636b6cfb562241eb191b81c290004b7dc27a
Luke Cwik <lcwik@google.com>
2019-10-07 13:46:42 -0700

    fixup!

--- sdks/python/apache_beam/runners/worker/worker_pool_main.py
+++ sdks/python/apache_beam/runners/worker/worker_pool_main.py
@@ -60,7 +60,7 @@ class BeamFnExternalWorkerPoolServicer(
   @classmethod
   def start(cls, worker_threads=1, use_process=False, port=0,
             state_cache_size=0, container_executable=None):
-    worker_server = grpc.server(CollapsingThreadPoolExecutor(max_workers=10))
+    worker_server = grpc.server(CollapsingThreadPoolExecutor())
     worker_address = 'localhost:%s' % worker_server.add_insecure_port(
         '[::]:%s' % port)
     worker_pool = cls(worker_threads,
